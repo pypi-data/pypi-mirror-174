85068abae219bb53a311726ee43eb116e6e6bff0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:57 +0000

    Add package netport/0.3.0/netport-0.3.0-py3-none-any.whl

---
+++ packages/netport/netport-0.3.0-py3-none-any.whl/netport/common.py
@@ -0,0 +1,9 @@
+R_PORT = "PORT"
+R_PATH = "PATH"
+R_PROCESS = "PROCESS"
+
+# TODO: remove the use of environment variables for redis parameters
+# TODO: For some reason there is always 2 tests that are being skipped because the parametrise gives
+#  empty parameters
+# TODO: Return the same response for each request, making every response in the same format
+# TODO: Add build and publish CI on when a new tag is created
