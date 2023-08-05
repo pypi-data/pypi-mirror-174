91d25a7206bc98a8f5f3f0b03e9c91e6c654d376
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:18:32 +0000

    Add package hpcflow-new2/0.2.0a9/hpcflow_new2-0.2.0a9.tar.gz

---
+++ packages/hpcflow-new2/hpcflow_new2-0.2.0a9.tar.gz/hpcflow_new2-0.2.0a9/hpcflow/sdk/core/errors.py
@@ -0,0 +1,82 @@
+class InputValueDuplicateSequenceAddress(ValueError):
+    pass
+
+
+class TaskTemplateMultipleSchemaObjectives(ValueError):
+    pass
+
+
+class TaskTemplateUnexpectedInput(ValueError):
+    pass
+
+
+class TaskTemplateMultipleInputValues(ValueError):
+    pass
+
+
+class InvalidIdentifier(ValueError):
+    pass
+
+
+class MissingInputs(Exception):
+    pass
+
+
+class TaskTemplateInvalidNesting(ValueError):
+    pass
+
+
+class TaskSchemaSpecValidationError(Exception):
+    pass
+
+
+class WorkflowSpecValidationError(Exception):
+    pass
+
+
+class InputSourceValidationError(Exception):
+    pass
+
+
+class EnvironmentSpecValidationError(Exception):
+    pass
+
+
+class ParameterSpecValidationError(Exception):
+    pass
+
+
+class FileSpecValidationError(Exception):
+    pass
+
+
+class DuplicateExecutableError(ValueError):
+    pass
+
+
+class MissingActionsError(ValueError):
+    pass
+
+
+class MissingCompatibleActionEnvironment(Exception):
+    pass
+
+
+class MissingActionEnvironment(Exception):
+    pass
+
+
+class FromSpecMissingObjectError(Exception):
+    pass
+
+
+class TaskSchemaMissingParameterError(Exception):
+    pass
+
+
+class ToJSONLikeChildReferenceError(Exception):
+    pass
+
+
+class InvalidInputSourceTaskReference(Exception):
+    pass
---
+++ packages/hpcflow-new2/hpcflow_new2-0.2.0a9.tar.gz/hpcflow_new2-0.2.0a9/hpcflow/tests/unit/test_spec.py
@@ -0,0 +1,5 @@
+import pytest
+
+
+def test_expected_return_1():
+    pass
