acaa22aa0f197c55d3198c67983fd36f5e9e26c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:19:42 +0000

    Add package shapely/2.0b2/shapely-2.0b2-cp39-cp39-win_amd64.whl

---
+++ packages/shapely/shapely-2.0b2-cp39-cp39-win_amd64.whl/shapely/algorithms/cga.py
@@ -0,0 +1,25 @@
+import shapely
+
+
+def signed_area(ring):
+    """Return the signed area enclosed by a ring in linear time using the
+    algorithm at: https://web.archive.org/web/20080209143651/http://cgafaq.info:80/wiki/Polygon_Area
+    """
+    xs, ys = ring.coords.xy
+    xs.append(xs[1])
+    ys.append(ys[1])
+    return (
+        sum(xs[i] * (ys[i + 1] - ys[i - 1]) for i in range(1, len(ring.coords))) / 2.0
+    )
+
+
+def is_ccw_impl(name=None):
+    """Predicate implementation"""
+
+    def is_ccw_op(ring):
+        return signed_area(ring) >= 0.0
+
+    if shapely.geos_version >= (3, 7, 0):
+        return shapely.is_ccw
+    else:
+        return is_ccw_op
