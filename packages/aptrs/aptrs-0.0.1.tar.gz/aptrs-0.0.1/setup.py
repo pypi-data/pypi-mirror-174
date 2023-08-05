c86a95710104c1408e7b18be8eaa5dbee6e3dd8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:25 +0000

    Add package aptrs/0.0.1/aptrs-0.0.1.tar.gz

---
+++ packages/aptrs/aptrs-0.0.1.tar.gz/aptrs-0.0.1/setup.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+from setuptools import (
+    find_packages,
+    setup,
+)
+
+from pathlib import Path
+
+
+def read(rel_path):
+    init = Path(__file__).resolve().parent / rel_path
+    return init.read_text('utf-8', 'ignore')
+
+
+
+description = (
+    'Automated Penetration Testing Reporting System')
+
+setup(
+    name='aptrs',
+    version='0.0.1',
+    description=description,
+    author='AnoF',
+    author_email='kalalsourav20@gmail.com',
+    packages=find_packages(include=[
+        '*', 'aptrs.*',
+    ]),
+    include_package_data=True,
+    python_requires='>=3.8<=3.9',
+    entry_points={
+        'console_scripts': [
+            'aptrs = APTRS.__main__:main',
+        ],
+    },
+    url='https://github.com/Anof-cyber/APTRS',
+    long_description=read('README.md'),
+    long_description_content_type='text/markdown',
+    install_requires=Path('requirements.txt').read_text().splitlines(),
+)
