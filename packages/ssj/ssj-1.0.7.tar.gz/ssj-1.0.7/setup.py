808f0c1628a748117f68564c74518a19465f8e6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:10:06 +0000

    Add package ssj/1.0.7/ssj-1.0.7.tar.gz

---
+++ packages/ssj/ssj-1.0.7.tar.gz/ssj-1.0.7/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(name = 'ssj',
+version = '1.0.7',
+author = 'Jonathan N. Nagel',
+author_email = 'jinnascimento81@gmail.com',
+install_requires = ['dropbox', 'tinydb', 'nested_dict', 'cryptography'],
+packages = ['ssj'],
+description = 'Servidor ou gestor de dados em JSON.',
+license = 'MIT',
+keywords = 'ssj')
