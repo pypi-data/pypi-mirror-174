6b8191b78cabc029f7e81d15b27b8cf2913e3f0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:12:23 +0000

    Add package odoo11-addon-packaging-uom/11.0.1.0.1/odoo11_addon_packaging_uom-11.0.1.0.1-py2.py3-none-any.whl

---
+++ packages/odoo11-addon-packaging-uom/odoo11_addon_packaging_uom-11.0.1.0.1-py2.py3-none-any.whl/odoo/addons/packaging_uom/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2015-2017 ACSONE SA/NV (<http://acsone.eu>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Packaging UOM",
+    "version": "11.0.1.0.1",
+    "author": 'ACSONE SA/NV, '
+              'Odoo Community Association (OCA)',
+    "category": "Warehouse",
+    "website": "https://github.com/OCA/stock-logistics-warehouse",
+    'summary': "Use uom in package",
+    "depends": [
+        "product",
+    ],
+    "data": [
+        "views/product_packaging_views.xml",
+    ],
+    "license": "AGPL-3",
+    "installable": True,
+}
