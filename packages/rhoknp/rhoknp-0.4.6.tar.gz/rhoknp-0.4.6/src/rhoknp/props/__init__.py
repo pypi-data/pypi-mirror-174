454670202160cb10f608a667c63dd1d8eaacdb1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:12:19 +0000

    Add package rhoknp/0.4.6/rhoknp-0.4.6.tar.gz

---
+++ packages/rhoknp/rhoknp-0.4.6.tar.gz/rhoknp-0.4.6/src/rhoknp/props/__init__.py
@@ -0,0 +1,12 @@
+from .dependency import DepType
+from .feature import FeatureDict
+from .named_entity import NamedEntity, NamedEntityCategory
+from .semantics import SemanticsDict
+
+__all__ = [
+    "DepType",
+    "FeatureDict",
+    "SemanticsDict",
+    "NamedEntity",
+    "NamedEntityCategory",
+]
