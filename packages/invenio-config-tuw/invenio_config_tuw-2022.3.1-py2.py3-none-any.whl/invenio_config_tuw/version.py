5f10b580487ba7cf4d94e75159e5d2605fc2af9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:10:31 +0000

    Add package invenio-config-tuw/2022.3.1/invenio_config_tuw-2022.3.1-py2.py3-none-any.whl

---
+++ packages/invenio-config-tuw/invenio_config_tuw-2022.3.1-py2.py3-none-any.whl/invenio_config_tuw/version.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2020-2022 TU Wien.
+#
+# Invenio-Config-TUW is free software; you can redistribute it and/or modify
+# it under the terms of the MIT License; see LICENSE file for more details.
+
+"""Version information for Invenio-Config-TUW.
+
+This file is imported by ``invenio_config_tuw.__init__``,
+and parsed by ``setup.py``.
+"""
+
+__version__ = "2022.3.1"
