924d8ddb1baf545d53e6e4b5c17e2fcb512e2445
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:44 +0000

    Add package novelai-api/0.10.1/novelai_api-0.10.1-py3-none-any.whl

---
+++ packages/novelai-api/novelai_api-0.10.1-py3-none-any.whl/novelai_api/SchemaValidator.py
@@ -0,0 +1,35 @@
+from jsonschema import validate, RefResolver
+from json import loads
+from os import listdir
+from os.path import splitext, dirname, abspath, join
+
+from typing import Any, Dict
+
+
+class SchemaValidator:
+    _schemas: Dict[str, Dict[str, Any]]
+    _resolver: RefResolver
+
+    def __init__(self):
+        if not hasattr(self, "_schemas"):
+            schemas = {}
+
+            lib_root = abspath(dirname(__file__))
+            schema_dir = join(lib_root, "schemas")
+
+            for filename in listdir(schema_dir):
+                with open(join(schema_dir, filename)) as f:
+                    schema_key = splitext(filename)[0]
+
+                    schemas[schema_key] = loads(f.read())
+
+            SchemaValidator._schemas = schemas
+            SchemaValidator._resolver = RefResolver("", "", store = schemas)
+
+    @classmethod
+    def validate(cls, name: str, obj: Any):
+        validate(obj, cls._schemas[name], resolver = cls._resolver)
+
+
+# initialize the schemas. A bit dirty, but the simplest
+SchemaValidator()
