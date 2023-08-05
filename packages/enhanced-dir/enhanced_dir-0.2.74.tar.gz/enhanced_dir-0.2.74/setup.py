880a03c3bf29860ac42488db5d347b75e91dad27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:10 +0000

    Add package enhanced-dir/0.2.74/enhanced_dir-0.2.74.tar.gz

---
+++ packages/enhanced-dir/enhanced_dir-0.2.74.tar.gz/enhanced_dir-0.2.74/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='enhanced_dir',
+    version='0.2.74',
+    description='An enhanced version of dir, which gives more details',
+    py_modules=['enhanced_dir'],
+    install_requires=['matplotlib', 'seaborn'],
+    package_dir={'': 'src'},
+)
