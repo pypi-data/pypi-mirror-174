3432f7b4611a3c93eb02b4d56c204be08a7435ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:12:55 +0000

    Add package dystopia/0.0.1/dystopia-0.0.1.tar.gz

---
+++ packages/dystopia/dystopia-0.0.1.tar.gz/dystopia-0.0.1/CHANGELOG.md
@@ -0,0 +1,7 @@
+CHANGELOG 
+==========
+
+**as dystopia**
+
+0.0.1 (29/10/2022)
+- initial release.
---
+++ packages/dystopia/dystopia-0.0.1.tar.gz/dystopia-0.0.1/dystopia/__init__.py
@@ -0,0 +1,22 @@
+'''
+
+Dystopia.
+
+'''
+
+# ignore __pycache__ from forming inside the library directory.
+import sys
+sys.dont_write_bytecode = True
+
+# meta data imports from the vandal library.
+from dystopia.misc._meta import (
+    __author__,
+    __copyright__,
+    __credits__,
+    __license__,
+    __version__,
+    __documentation__,
+    __contact__,
+    __donate__,
+    __APPversion__,
+)
