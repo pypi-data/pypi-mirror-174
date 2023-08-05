1d0cef1aeca75ac85ae3ceccf3bfacd251cdd349
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:53 +0000

    Add package tvpy/0.0.14/tvpy-0.0.14-py3-none-any.whl

---
+++ packages/tvpy/tvpy-0.0.14-py3-none-any.whl/tvpy/main.py
@@ -0,0 +1,41 @@
+from fire import Fire
+
+
+def tv_json():
+    from tvpy.tv_json import tv_json
+    Fire(tv_json)
+
+
+def tv_down():
+    from tvpy.tv_down import tv_down
+    Fire(tv_down)
+
+
+def tv_subs():
+    from tvpy.tv_subs import tv_subs
+    Fire(tv_subs)
+
+
+def tv_renm():
+    from tvpy.tv_renm import tv_renm
+    Fire(tv_renm)
+
+
+def tv_klyn():
+    from tvpy.tv_klyn import tv_klyn
+    Fire(tv_klyn)
+
+
+def tv_info():
+    from tvpy.tv_info import tv_info
+    Fire(tv_info)
+
+
+def tv_html():
+    from tvpy.tv_html import tv_html
+    Fire(tv_html)
+
+
+def tvpy():
+    from tvpy.tvpy import tvpy
+    Fire(tvpy)
