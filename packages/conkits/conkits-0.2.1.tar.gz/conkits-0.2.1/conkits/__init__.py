d07716d28093899626e869140ece27a6e74d7b67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:19:44 +0000

    Add package conkits/0.2.1/conkits-0.2.1.tar.gz

---
+++ packages/conkits/conkits-0.2.1.tar.gz/conkits-0.2.1/conkits/__init__.py
@@ -0,0 +1,10 @@
+from .conio import Conio
+from .csi256colors import Colors256
+from .csi import Fore, Back, Style, Cursor
+from .printtools import DynamicPrint, Choice
+from .help import conkits_help
+
+
+name = 'conkits'
+__version__ = '0.2.1'
+
