cfac05a03ad5962ec68c2bbfd247d1bc1bdf2c88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:11:44 +0000

    Add package skidbilly/1.0/skidbilly-1.0.tar.gz

---
+++ packages/skidbilly/skidbilly-1.0.tar.gz/skidbilly-1.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+
+
+setup(
+    name="skidbilly",
+    version='1.0',
+    license='Eclipse Public License 2.0',
+    authors=["Bernward"],
+    author_email="<contact@bern.codes>",
+    description="reuploaded",
+    long_description='reuploaded',
+    keywords=['cli', 'fade', 'colors', 'terminal', 'tui'],
+    package_dir={"": "src"},
+    package_data={'': ['**/*']}
+)
