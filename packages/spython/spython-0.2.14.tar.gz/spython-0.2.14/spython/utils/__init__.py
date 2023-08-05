15f47b42a0e5830391b0f5348a50ac94bfa24dfb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:12:26 +0000

    Add package spython/0.2.14/spython-0.2.14.tar.gz

---
+++ packages/spython/spython-0.2.14.tar.gz/spython-0.2.14/spython/utils/__init__.py
@@ -0,0 +1,16 @@
+from .fileio import mkdir_p, read_file, read_json, write_file, write_json
+from .misc import ScopedEnvVar
+from .terminal import (
+    check_install,
+    format_container_name,
+    get_installdir,
+    get_singularity_version,
+    get_singularity_version_info,
+    get_userhome,
+    get_username,
+    remove_uri,
+    run_command,
+    split_uri,
+    stream_command,
+    which,
+)
