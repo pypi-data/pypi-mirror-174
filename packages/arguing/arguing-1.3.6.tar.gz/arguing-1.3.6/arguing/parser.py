13886a9b80af7570a64bc873bde4cbae16fce92e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:14 +0000

    Add package arguing/1.3.6/arguing-1.3.6.tar.gz

---
+++ packages/arguing/arguing-1.3.6.tar.gz/arguing-1.3.6/arguing/parser.py
@@ -0,0 +1,40 @@
+import sys
+
+argv = sys.argv
+argument_dict = {}
+
+
+def set(argument, type=str, default=None, mandatory=False, help=None):
+    argument_dict[argument] = {
+        'default': default,
+        'type': type,
+        'help': help,
+        'mandatory': mandatory
+    }
+
+    if mandatory and not check(argument):
+        exit(f'{argument}: {help} ({type.__name__}).')
+
+    return get(argument)
+
+
+def get(argument):
+    if argument in argv and argument != argv[-1]:
+        value_index = argv.index(argument) + 1
+        argument_value = argv[value_index]
+
+    elif argument in argument_dict:
+        argument_value = argument_dict[argument]['default']
+
+    else:
+        return None
+
+    if argument in argument_dict:
+        argument_value = argument_dict[argument]['type'](argument_value)
+
+    return argument_value
+
+
+def check(argument):
+    return (argument in argv
+            and len(argv) > argv.index(argument) + 1)
