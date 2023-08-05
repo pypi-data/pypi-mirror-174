fabc7577c14e858dbc3b613ef310712e1b6e744e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:18:21 +0000

    Add package cambridge/3.3.0/cambridge-3.3.0.tar.gz

---
+++ packages/cambridge/cambridge-3.3.0.tar.gz/cambridge-3.3.0/cambridge/errors.py
@@ -0,0 +1,36 @@
+"""
+This script sets up self-defined errors.
+"""
+
+import sys
+
+from .log import logger
+
+
+class ParsedNoneError(Exception):
+    """Used when bs4 returned None whereas there's target content existing within the document"""
+
+    def __init__(self):
+        self.message = "Nothing parsed out"
+
+    def __str__(self):
+        return self.message
+
+
+class NoResultError(Exception):
+    """Used when bs4 returned None because Cambridge dict has no result"""
+
+    def __init__(self):
+        self.message = "No result found in Cambridge Dictionary"
+
+    def __str__(self):
+        return self.message
+
+
+def call_on_error(error, url, attempt, op):
+    attempt += 1
+    logger.debug(f"{op} HTML from {url} {attempt} times")
+    if attempt == 3:
+        logger.error(f"Maximum {op} retries reached. \n{error}")
+        sys.exit()
+    return attempt
