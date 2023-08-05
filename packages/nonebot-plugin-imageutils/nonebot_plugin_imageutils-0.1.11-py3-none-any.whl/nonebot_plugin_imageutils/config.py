acf32b468c2364ecff18e8d61de36d1c6d9240e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:18:29 +0000

    Add package nonebot-plugin-imageutils/0.1.11/nonebot_plugin_imageutils-0.1.11-py3-none-any.whl

---
+++ packages/nonebot-plugin-imageutils/nonebot_plugin_imageutils-0.1.11-py3-none-any.whl/nonebot_plugin_imageutils/config.py
@@ -0,0 +1,24 @@
+from pathlib import Path
+from typing import List, Optional
+from pydantic import BaseModel, Extra
+
+
+class Config(BaseModel, extra=Extra.ignore):
+    custom_font_path: Optional[Path] = None
+    default_fallback_fonts: List[str] = [
+        "Arial",
+        "Tahoma",
+        "Helvetica Neue",
+        "Segoe UI",
+        "PingFang SC",
+        "Hiragino Sans GB",
+        "Microsoft YaHei",
+        "Source Han Sans SC",
+        "Noto Sans SC",
+        "Noto Sans CJK JP",
+        "WenQuanYi Micro Hei",
+        "Apple Color Emoji",
+        "Noto Color Emoji",
+        "Segoe UI Emoji",
+        "Segoe UI Symbol",
+    ]
