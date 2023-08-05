6e8d0efe081b4c8815ed3cc115bb5124d83a8abd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:27 +0000

    Add package pdf2ebook/1.2.0/pdf2ebook-1.2.0.tar.gz

---
+++ packages/pdf2ebook/pdf2ebook-1.2.0.tar.gz/pdf2ebook-1.2.0/setup.py
@@ -0,0 +1,28 @@
+from setuptools import (
+    find_packages,
+    setup
+)
+
+INSTALL_REQUIRES = (
+    'pdf2text',
+    'ebooklib',
+    'bs4'
+)
+
+setup(
+    name='pdf2ebook',
+    version='1.2.0',
+    python_requires='>=3.6',
+    description='PDF to ebook',
+    long_description='PDF to ebook',
+    author='Robert Lucey',
+    url='https://github.com/RobertLucey/pdf2ebook',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    install_requires=INSTALL_REQUIRES,
+    entry_points={
+        'console_scripts': [
+            'pdf2epub = pdf2ebook.bin.convert:main'
+        ]
+    }
+)
