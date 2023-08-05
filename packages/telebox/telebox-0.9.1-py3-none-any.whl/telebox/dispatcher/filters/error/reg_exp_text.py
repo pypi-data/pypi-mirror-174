ab5cda4afc3d76b603edee5bc712c33b02830ac1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:11:13 +0000

    Add package telebox/0.9.1/telebox-0.9.1-py3-none-any.whl

---
+++ packages/telebox/telebox-0.9.1-py3-none-any.whl/telebox/dispatcher/filters/error/reg_exp_text.py
@@ -0,0 +1,23 @@
+from typing import Pattern
+
+from telebox.dispatcher.filters.error_filter import AbstractErrorFilter
+from telebox.dispatcher.typing import Event
+
+
+class ErrorRegExpTextFilter(AbstractErrorFilter):
+
+    def __init__(self, *patterns: Pattern, full_match: bool = True):
+        if not patterns:
+            raise ValueError("No patterns!")
+
+        self._patterns = set(patterns)
+        self._full_match = full_match
+
+    def get_value(self, error: Exception, event: Event) -> str:
+        return str(error)
+
+    def check_value(self, value: str) -> bool:
+        if self._full_match:
+            return any(i.fullmatch(value) is not None for i in self._patterns)
+        else:
+            return any(i.match(value) is not None for i in self._patterns)
