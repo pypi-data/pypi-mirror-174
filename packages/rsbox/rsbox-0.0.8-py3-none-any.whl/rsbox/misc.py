613af76d9f218f155932a72e3a341dfbc0814245
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:10:35 +0000

    Add package rsbox/0.0.8/rsbox-0.0.8-py3-none-any.whl

---
+++ packages/rsbox/rsbox-0.0.8-py3-none-any.whl/rsbox/misc.py
@@ -0,0 +1,41 @@
+"""
+File: misc.py 
+-------------- 
+Miscellaneous utils. 
+""" 
+
+import pickle
+from datetime import datetime
+from pytz import timezone
+
+
+def timestamp():
+    """
+    Simple function that retrieves the current date and time
+    and returns a properly formatted string (i.e., a timestamp).  
+    """
+    now = datetime.now()
+    date_time = now.strftime("%-I-%M-%p-%b-%d-%Y")
+    return str(date_time)
+
+
+def pickle(filepath, object_to_pickle):
+	"""
+	Pickles object_to_pickle saved at 
+	filepath. filepath is a 'str' and
+	should end in '.pkl'.  
+	"""
+	out_file = open(filepath, "wb")
+	pickle.dump(object_to_pickle, out_file)
+	out_file.close()
+
+
+def unpickle(filepath):
+	"""
+	Takes in a path to a pickled 
+	.pkl file (type: 'str') and
+	returns the unpickled object. 
+	"""
+	in_file = open(filepath, 'rb')
+	loaded_object = pickle.load(in_file) 
+	return loaded_object
