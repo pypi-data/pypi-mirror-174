1b2c1386ae2501932026377d8f2939d52b1e9ed6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:19:27 +0000

    Add package ghasedakpack/0.1.13/ghasedakpack-0.1.13.tar.gz

---
+++ packages/ghasedakpack/ghasedakpack-0.1.13.tar.gz/ghasedakpack-0.1.13/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+# with open("readme.md", "r") as fh:
+#     long_description = fh.read()
+
+setuptools.setup(
+    name="ghasedakpack",
+    version="0.1.13",
+    author="Ghasedak Group",
+    author_email="",
+    description="Ghasedak sms webservice api wrapper for python.",
+#     long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="http://github.com/ghasedakapi/ghasedak-python",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
