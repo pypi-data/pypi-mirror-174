f46666f78cbf775ce01692336da3e2b163c6a9e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:09:55 +0000

    Add package ballpark-fixed/1.4.1/ballpark-fixed-1.4.1.tar.gz

---
+++ packages/ballpark-fixed/ballpark-fixed-1.4.1.tar.gz/ballpark-fixed-1.4.1/setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages, setup
+
+setup(name='ballpark-fixed',
+    description='Better human-readable numbers. Forked and fixed from Stijn',
+    long_description=open('README.rst').read(),
+    author='Endlessz',
+    author_email='lckrep@protonmail.com',
+    url='https://github.com/Endlesszombiez/python-ballpark',
+    version='1.4.1',
+    license='ISC',
+    packages=find_packages(),
+    keywords='human numbers format notation scientific engineering',
+    install_requires=[],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: ISC License (ISCL)',
+        'Operating System :: OS Independent',
+        'Topic :: Scientific/Engineering :: Information Analysis',
+        'Topic :: Utilities',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.10'
+        ],
+    )
