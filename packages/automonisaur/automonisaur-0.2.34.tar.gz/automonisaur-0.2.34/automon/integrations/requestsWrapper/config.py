5760ce106266afd54a85a9f1f30ecb849669db70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:56 +0000

    Add package automonisaur/0.2.34/automonisaur-0.2.34.tar.gz

---
+++ packages/automonisaur/automonisaur-0.2.34.tar.gz/automonisaur-0.2.34/automon/integrations/requestsWrapper/config.py
@@ -0,0 +1,10 @@
+import requests
+
+from automon.log import Logging
+
+log = Logging(name='RequestsConfig', level=Logging.DEBUG)
+
+
+class RequestsConfig(object):
+    def __repr__(self):
+        return f'{NotImplemented}'
