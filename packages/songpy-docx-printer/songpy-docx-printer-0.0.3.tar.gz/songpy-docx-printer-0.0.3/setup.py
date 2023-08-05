2f4cb37be3e74688557f181d47e61262d6589c23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:22 +0000

    Add package songpy-docx-printer/0.0.3/songpy-docx-printer-0.0.3.tar.gz

---
+++ packages/songpy-docx-printer/songpy-docx-printer-0.0.3.tar.gz/songpy-docx-printer-0.0.3/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+
+# with open('README.rst') as f:
+#     readme = f.read()
+
+# with open('LICENSE') as f:
+#     license = f.read()
+
+setup(
+    name='songpy-docx-printer',
+    version='0.0.3',
+    description='Support package for package songpy, for printing formatted songbooks to docx files',
+#    long_description=readme,
+    author='Karol Ważny',
+#    author_email='me@kennethreitz.com',
+#    url='https://github.com/kennethreitz/samplemod',
+#    license=license,
+    packages=find_packages(exclude=('tests', 'docs')),
+    package_data={'resources': ["*"]}
+)
