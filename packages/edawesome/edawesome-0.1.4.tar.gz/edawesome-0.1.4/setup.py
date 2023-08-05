25362f91a4c923bc05cc5c81cc2f54ecb01d93bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:12:37 +0000

    Add package edawesome/0.1.4/edawesome-0.1.4.tar.gz

---
+++ packages/edawesome/edawesome-0.1.4.tar.gz/edawesome-0.1.4/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['edawesome']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['ipython>=8.5.0,<9.0.0',
+ 'kaggle>=1.5.12,<2.0.0',
+ 'scipy>=1.9.3,<2.0.0',
+ 'seaborn>=0.12.1,<0.13.0',
+ 'transitions>=0.9.0,<0.10.0']
+
+setup_kwargs = {
+    'name': 'edawesome',
+    'version': '0.1.4',
+    'description': '',
+    'long_description': '',
+    'author': 'Timofei Ryko',
+    'author_email': 'timofei.ryko@gmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
