e9c4204fb12f198c72cfcf5196ed98c4301920d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:22:17 +0000

    Add package autogluon.timeseries/0.5.3b20221029/autogluon.timeseries-0.5.3b20221029-py3-none-any.whl

---
+++ packages/autogluon.timeseries/autogluon.timeseries-0.5.3b20221029-py3-none-any.whl/autogluon/timeseries/models/__init__.py
@@ -0,0 +1,33 @@
+from .autogluon_tabular import AutoGluonTabularModel
+from .gluonts import (
+    DeepARModel,
+    DeepARMXNetModel,
+    MQCNNMXNetModel,
+    MQRNNMXNetModel,
+    SimpleFeedForwardModel,
+    SimpleFeedForwardMXNetModel,
+    TemporalFusionTransformerMXNetModel,
+    TransformerMXNetModel,
+)
+from .sktime import SktimeARIMAModel, SktimeAutoARIMAModel, SktimeAutoETSModel, SktimeTBATSModel, SktimeThetaModel
+from .statsmodels import ARIMAModel, ETSModel, ThetaModel
+
+__all__ = [
+    "DeepARModel",
+    "DeepARMXNetModel",
+    "MQCNNMXNetModel",
+    "MQRNNMXNetModel",
+    "SimpleFeedForwardModel",
+    "SimpleFeedForwardMXNetModel",
+    "TemporalFusionTransformerMXNetModel",
+    "TransformerMXNetModel",
+    "SktimeARIMAModel",
+    "SktimeAutoARIMAModel",
+    "SktimeAutoETSModel",
+    "SktimeTBATSModel",
+    "SktimeThetaModel",
+    "ARIMAModel",
+    "ETSModel",
+    "ThetaModel",
+    "AutoGluonTabularModel",
+]
