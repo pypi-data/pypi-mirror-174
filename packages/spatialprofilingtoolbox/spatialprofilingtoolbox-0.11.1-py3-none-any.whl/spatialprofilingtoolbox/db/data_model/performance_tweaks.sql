edf02dc95d808be82091e9b407c2c21b289b15d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:08 +0000

    Add package spatialprofilingtoolbox/0.11.1/spatialprofilingtoolbox-0.11.1-py3-none-any.whl

---
+++ packages/spatialprofilingtoolbox/spatialprofilingtoolbox-0.11.1-py3-none-any.whl/spatialprofilingtoolbox/db/data_model/performance_tweaks.sql
@@ -0,0 +1,17 @@
+
+ALTER TABLE diagnosis
+ADD UNIQUE (subject, condition, result, assessor, date) ;
+
+ALTER TABLE histology_assessment_process
+ADD UNIQUE (slide, assay, result, assessor, assessment_date) ;
+
+ALTER TABLE cell_phenotype_criterion
+ADD UNIQUE (cell_phenotype, marker, polarity, study) ;
+
+ALTER TABLE feature_specifier
+ADD UNIQUE (feature_specification, specifier, ordinality) ;
+
+ALTER TABLE two_cohort_feature_association_test
+ADD UNIQUE (selection_criterion_1, selection_criterion_2, test, p_value, feature_tested) ;
+
+CREATE EXTENSION IF NOT EXISTS tablefunc;
