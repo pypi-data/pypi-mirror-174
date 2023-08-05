7d41b15cf7c9412b192707733437470d9a5393ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:19:57 +0000

    Add package T3SF/1.1rc1/T3SF-1.1rc1.tar.gz

---
+++ packages/T3SF/T3SF-1.1rc1.tar.gz/T3SF-1.1rc1/setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="T3SF",
+    version="1.1rc1",
+    author="BASE4 Security",
+    author_email="jlanfranconi@base4sec.com",
+    description="Technical Tabletop Exercises Simulation Framework",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Base4Security/T3SF",
+    project_urls={
+        "Bug Tracker": "https://github.com/Base4Security/T3SF/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Topic :: Software Development :: Libraries :: Application Frameworks",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    install_requires=[
+        'asyncio',
+        'configparser'
+    ],
+)
