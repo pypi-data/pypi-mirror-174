3f08e544ff2e0f10acff5648594b436c3ab0daac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:05:50 +0000

    Add package devl/0.1.0/devl-0.1.0-py3-none-any.whl

---
+++ packages/devl/devl-0.1.0-py3-none-any.whl/devl/main.py
@@ -0,0 +1,13 @@
+def print_signature() -> None:
+    print("""-----BEGIN PGP SIGNED MESSAGE-----
+Hash: SHA512
+
+This is a signature package.
+-----BEGIN PGP SIGNATURE-----
+
+iHUEARYKAB0WIQTu3enDu4rKxAP4uLZq1HqAflJe8QUCYvq4owAKCRBq1HqAflJe
+8c8CAP9CsgWD9Vf3CzEYlonVm6r+gM0LZ/sQmyS8FqZbxBbjBwD/Z+QEh6T2yYas
+c8MEh1X4vzd98C6G2An7RkE9PP1SxAo=
+=AVDl
+-----END PGP SIGNATURE-----
+""")
