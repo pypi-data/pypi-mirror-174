e7581b49f57a5be408ed07b21c1e3a1e639e96ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:10:22 +0000

    Add package flurryflake/0.1.27/flurryflake-0.1.27-py3-none-any.whl

---
+++ packages/flurryflake/flurryflake-0.1.27-py3-none-any.whl/flurryflake/filters/config.py
@@ -0,0 +1,19 @@
+import configparser
+import os
+import sys
+
+configp = configparser.ConfigParser()
+config_path = '/etc/flake.ini'
+configp.read(config_path)
+
+# check if the path is to a valid file
+if not os.path.isfile(config_path):
+    print("Invalid configuration path provided.")
+    sys.exit()
+
+def initFromConfig(param):
+    for section in configp.sections():
+        if configp.has_option(section, param):
+            return configp[section][param]
+    print("Error initializing " + str(param) + "from config. Parameter not found.")
+    sys.exit()
