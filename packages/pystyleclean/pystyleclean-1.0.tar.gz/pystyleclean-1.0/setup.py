8d024fddf901c0b54ef4ffb545afe4a5534c83a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:55 +0000

    Add package pystyleclean/1.0/pystyleclean-1.0.tar.gz

---
+++ packages/pystyleclean/pystyleclean-1.0.tar.gz/pystyleclean-1.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name="pystyleclean",
+    version='1.0',
+    license='Eclipse Public License 2.0',
+    authors=["billythegoat356", "loTus01", "BlueRed"],
+    author_email="<hideakiatsuyo69@gmail.com>",
+    description="by billythegoat356, loTus01 and BlueRed",
+    long_description='Documentation: https://github.com/HideakiAtsuyo/pystyleclean',
+    keywords=['cli', 'fade', 'colors', 'terminal', 'tui'],
+    packages=['pystyleclean'],
+    install_requires=[]
+)
