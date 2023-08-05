39e92dcfc4e7f0015d2be6ca11e6982e95e3d73e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:17:21 +0000

    Add package kitbag/0.0.2/kitbag-0.0.2.tar.gz

---
+++ packages/kitbag/kitbag-0.0.2.tar.gz/kitbag-0.0.2/README.md
@@ -0,0 +1,2 @@
+# Kitbag
+**A "kitbag" with all visualizations, colors, logos etc. used in FCN and RTD**
---
+++ packages/kitbag/kitbag-0.0.2.tar.gz/kitbag-0.0.2/kitbag/utils/colors.py
@@ -0,0 +1,8 @@
+red = "#dd2a26"
+yellow = "#fff100"
+green = "#0fac4d"
+blue = "#4285f4"
+grey = "#3C4043"
+
+team_colors = {"FC Nordsjaelland": red, "FC Nordsjaelland U19": blue, "FC Nordsjaelland U17":blue, 
+               "FC Nordsjaelland Women": yellow, "RTD Ghana - Senior": green}
