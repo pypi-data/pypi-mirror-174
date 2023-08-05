16eae89b243cd96b1f418e77ff1fc1dd0c3bcc4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:00 +0000

    Add package cnextlib/0.7.0/cnextlib-0.7.0-py3-none-any.whl

---
+++ packages/cnextlib/cnextlib-0.7.0-py3-none-any.whl/cnextlib/tests/test_loc_func.py
@@ -0,0 +1,26 @@
+import os, sys
+# os.chdir('...')
+# sys.path.append(os.getcwd()+'/../')
+import cnextlib.dataframe as cd
+from cnextlib.dataframe import DataFrameStatusHook
+
+# df = cd.DataFrame('tests/data/machine-simulation/21549286_out.csv')
+# # DataFrameStatusHook.update_active_df_status(get_global_df_list())
+# # print(DataFrameStatusHook.get_active_df())
+
+# df['Copy'] = df['Engine Speed']
+# DataFrameStatusHook.update_active_df_status(get_user_space_df_list())
+# print(DataFrameStatusHook.get_active_df())
+
+# df.loc[0]
+# DataFrameStatusHook.update_active_df_status(get_user_space_df_list())
+# print(DataFrameStatusHook.get_active_df())
+
+# df.loc[4000] = df.loc[0]
+# DataFrameStatusHook.update_active_df_status(get_user_space_df_list())
+# print(DataFrameStatusHook.get_active_df())
+
+# print(df.shape)
+
+# DataFrameStatusHook.update_active_df_status(get_global_df_list())
+# print(DataFrameStatusHook.get_active_df())
