c5c99f19756e88e30cdd56042e828409aa4193e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:15:00 +0000

    Add package foursight/2.2.0.1b65/foursight-2.2.0.1b65.tar.gz

---
+++ packages/foursight/foursight-2.2.0.1b65.tar.gz/foursight-2.2.0.1b65/chalicelib_fourfront/app_utils.py
@@ -0,0 +1,34 @@
+from chalice import Cron
+import os
+from os.path import dirname
+from dcicutils.env_utils import public_url_for_app
+from foursight_core.app_utils import app  # Chalice object
+from foursight_core.app_utils import AppUtils as AppUtils_from_core
+from foursight_core.identity import apply_identity_globally
+from .vars import FOURSIGHT_PREFIX, HOST
+
+
+class AppUtils(AppUtils_from_core):
+
+    # dmichaels/C4-826: Apply identity globally.
+    apply_identity_globally()
+
+    # Overridden from subclass.
+    APP_PACKAGE_NAME = "foursight"
+
+    # Note that this is set in the new (as of August 2022) apply_identity code;
+    # see foursight-core/foursight_core/{app_utils.py,identity.py}.
+    es_host = os.environ.get("ES_HOST")
+    if not es_host:
+        raise Exception("Foursight ES_HOST environment variable not set!")
+    HOST = es_host
+    
+    # overwriting parent class
+    prefix = FOURSIGHT_PREFIX
+    FAVICON = public_url_for_app('fourfront') + '/static/img/favicon-fs.ico'  # favicon acquired from prod
+    host = HOST
+    package_name = 'chalicelib_fourfront'
+    check_setup_dir = dirname(__file__)
+    check_setup_dir_fallback = dirname(__file__)
+    DEFAULT_ENV = os.environ.get("ENV_NAME", "foursight-fourfront-env-uninitialized")
+    html_main_title = 'Foursight'
