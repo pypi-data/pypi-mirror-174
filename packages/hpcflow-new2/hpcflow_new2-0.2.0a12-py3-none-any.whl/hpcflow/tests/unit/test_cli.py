912b738c5b3adf6fb6e45538b8385a83e95fea6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:05:49 +0000

    Add package hpcflow-new2/0.2.0a12/hpcflow_new2-0.2.0a12-py3-none-any.whl

---
+++ packages/hpcflow-new2/hpcflow_new2-0.2.0a12-py3-none-any.whl/hpcflow/tests/unit/test_cli.py
@@ -0,0 +1,12 @@
+# import pytest
+
+# from click.testing import CliRunner
+
+# from hpcflow import __version__
+# from hpcflow.cli.cli import cli
+
+
+# def test_version():
+#     runner = CliRunner()
+#     result = runner.invoke(cli, args="--version")
+#     assert result.output.strip() == f"hpcflow, version {__version__}"
