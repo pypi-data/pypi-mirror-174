b8a491178de27e1026a6c1b2aa038326761a4a1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:14 +0000

    Add package stackzilla/0.1.1/stackzilla-0.1.1-py2.py3-none-any.whl

---
+++ packages/stackzilla/stackzilla-0.1.1-py2.py3-none-any.whl/stackzilla/cli/database.py
@@ -0,0 +1,40 @@
+"""Module for database debugging CLI."""
+import pprint
+
+import click
+
+from stackzilla.blueprint import StackzillaBlueprint
+from stackzilla.database.base import StackzillaDB
+from stackzilla.database.exceptions import ResourceNotFound
+from stackzilla.resource.base import StackzillaResource
+from stackzilla.utils.constants import DISK_BP_PREFIX
+
+from .options import path_option
+
+
+@click.group(name='database')
+def database():
+    """Command group for all database CLI commands."""
+
+@database.command('show-resource')
+@path_option
+def show_resource(path):
+    """Show a resource, and its attributes, as it is in the database."""
+    StackzillaDB.db.open()
+
+    # Import the blueprint from disk
+    db_blueprint = StackzillaBlueprint(python_root=DISK_BP_PREFIX)
+    db_blueprint.load()
+
+    # Load the resource specified by path
+    try:
+        resource: StackzillaResource = db_blueprint.get_resource(path=path)
+        resource = resource()
+        resource.load_from_db()
+    except ResourceNotFound as exc:
+        raise click.ClickException('Resource specified by path not found') from exc
+
+    print(resource.path(remove_prefix=True))
+    print(f'Version: {resource.version()}')
+    print(f'Saved version: {resource.saved_version()}')
+    pprint.pprint(resource.__dict__)
---
+++ packages/stackzilla/stackzilla-0.1.1-py2.py3-none-any.whl/stackzilla/cli/options.py
@@ -0,0 +1,31 @@
+"""Module for common Click options."""
+import click
+
+from stackzilla.cli._callbacks import namespace_callback
+
+
+def namespace_option(function):
+    """Decorator for the namesapce CLI argument."""
+    function = click.option(
+        '--namespace',
+        required=True,
+        expose_value=True,
+        callback=namespace_callback,
+        help='Text name for the new work environment')(function)
+    return function
+
+def path_option(function):
+    """Decorator for any CLI command which takes the path to a blueprint resource."""
+    function = click.option('--path', required=True, help='Python path to the blueprint resource')(function)
+    return function
+
+def key_option(function):
+    """Decorator for the metadata key CLI argument."""
+    function = click.option('--key', required=True, help='Metadata key to index by')(function)
+    return function
+
+
+def value_option(function):
+    """Decorator for the metadata value CLI argument."""
+    function = click.option('--value', required=True, help='Metadata value to set')(function)
+    return function
