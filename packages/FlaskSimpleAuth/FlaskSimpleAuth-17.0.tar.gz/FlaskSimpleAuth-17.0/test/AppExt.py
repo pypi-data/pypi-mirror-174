470dd565785309b245dd333edc5b5606e2ad88e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:15:21 +0000

    Add package FlaskSimpleAuth/17.0/FlaskSimpleAuth-17.0.tar.gz

---
+++ packages/FlaskSimpleAuth/FlaskSimpleAuth-17.0.tar.gz/FlaskSimpleAuth-17.0/test/AppExt.py
@@ -0,0 +1,57 @@
+#
+# ANOTHER TEST APP FOR FlaskSimpleAuth
+#
+
+from Auth import user_in_group, get_user_pass
+
+import logging
+log = logging.getLogger("app-ext")
+
+#
+# APP
+#
+from flask import Flask
+app = Flask("Test")
+
+# note that FSA initialization is delayed…
+from FlaskSimpleAuth import FlaskSimpleAuth
+fsa = FlaskSimpleAuth(app)
+
+#
+# AUTH
+#
+app.config.update(
+    FSA_DEBUG = True,
+    FSA_AUTH = "fake",
+    FSA_TOKEN_CARRIER = "cookie",
+    FSA_GET_USER_PASS = get_user_pass,
+    FSA_USER_IN_GROUP = user_in_group,
+    FSA_CAST = { list: lambda s: s.split(" ") },
+    FSA_OBJECT_PERMS = { "xyz": lambda d, i, m: False }
+)
+
+#
+# ROUTES
+#
+@fsa.route("/stuff", methods=["GET"], authorize="ALL")
+def get_stuff():
+    return "stuff", 200
+
+# bad route decorator used
+@app.route("/bad", methods=["GET"])
+def get_bad():
+    return "bad", 200
+
+from SubApp import subapp
+fsa.register_blueprint(subapp, url_prefix="/b2")
+
+# errors because authorize is not handled
+# app.register_blueprint(subapp, url_prefix="/b3")
+
+import Shared
+from Shared import something
+Shared.init_app(something="AppExt")
+
+@fsa.route("/something", methods=["GET"], authorize="ALL")
+def get_something():
+    return str(something), 200
