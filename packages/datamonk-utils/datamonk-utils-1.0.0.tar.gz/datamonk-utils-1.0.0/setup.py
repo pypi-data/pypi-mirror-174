f59f0aab636e6a5973c832d8f5c3c08fb6ae2400
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:05:29 +0000

    Add package datamonk-utils/1.0.0/datamonk-utils-1.0.0.tar.gz

---
+++ packages/datamonk-utils/datamonk-utils-1.0.0.tar.gz/datamonk-utils-1.0.0/setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['utils']
+
+package_data = \
+{'': ['*'], 'utils': ['data/*', 'documentation/drawIO/*']}
+
+install_requires = \
+['pandas>=1.4.3,<2.0.0',
+ 'python-dotenv>=0.21.0,<0.22.0',
+ 'redmail>=0.4.2,<0.5.0',
+ 'slack-logger>=0.3.1,<0.4.0']
+
+setup_kwargs = {
+    'name': 'datamonk-utils',
+    'version': '1.0.0',
+    'description': '',
+    'long_description': 'None',
+    'author': 'Vit Mrnavek',
+    'author_email': 'vit.mrnavek@datamonk.cz',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.9.0,<3.10.0',
+}
+
+
+setup(**setup_kwargs)
