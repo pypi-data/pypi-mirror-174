aee86b5bfee165bb8aad06f748ece8525658da08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:12:37 +0000

    Add package random-img-api/1.1.0/random_img_api-1.1.0.tar.gz

---
+++ packages/random-img-api/random_img_api-1.1.0.tar.gz/random_img_api-1.1.0/random_img_api/img_api_cli.py
@@ -0,0 +1,17 @@
+import rich_click as click
+
+from random_img_api.src.get_img.get_img import get
+from random_img_api.src.run import run
+
+
+@click.group()
+@click.version_option(version="v1.1.0")
+def cli():
+    """
+    Random Image API command line interface
+    """
+    pass
+
+
+cli.add_command(get)
+cli.add_command(run)
