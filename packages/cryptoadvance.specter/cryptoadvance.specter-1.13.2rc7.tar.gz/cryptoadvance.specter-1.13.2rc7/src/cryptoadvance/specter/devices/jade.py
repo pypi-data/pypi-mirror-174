f0192b25374e39eb70fdcd7ff30bb75b8ca55744
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:51 +0000

    Add package cryptoadvance.specter/1.13.2rc7/cryptoadvance.specter-1.13.2rc7.tar.gz

---
+++ packages/cryptoadvance.specter/cryptoadvance.specter-1.13.2rc7.tar.gz/cryptoadvance.specter-1.13.2rc7/src/cryptoadvance/specter/devices/jade.py
@@ -0,0 +1,33 @@
+from . import DeviceTypes
+from .hwi_device import HWIDevice
+from .hwi.jade import JadeClient
+from .hwi.jade import enumerate as jade_enumerate
+from ..helpers import is_liquid
+
+
+class Jade(HWIDevice):
+    device_type = DeviceTypes.JADE
+    name = "Jade"
+    icon = "img/devices/jade_icon.svg"
+
+    supports_hwi_toggle_passphrase = False
+    supports_hwi_multisig_display_address = True
+    liquid_support = True
+
+    @classmethod
+    def get_client(cls, *args, **kwargs):
+        return JadeClient(*args, **kwargs)
+
+    @classmethod
+    def enumerate(cls, *args, **kwargs):
+        return jade_enumerate(*args, **kwargs)
+
+    def has_key_types(self, wallet_type, network="main"):
+        if wallet_type == "multisig" and is_liquid(network):
+            return False
+        return super().has_key_types(wallet_type, network)
+
+    def no_key_found_reason(self, wallet_type, network="main"):
+        if wallet_type == "multisig" and is_liquid(network):
+            return "Jade does not support multisig wallets on Liquid."
+        return super().no_key_found_reason(wallet_type, network)
