447eb5b7eb6b84c7e6cfe302d427ddc44a694180
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:10 +0000

    Add package aioqzone/0.9.9.dev3/aioqzone-0.9.9.dev3-py3-none-any.whl

---
+++ packages/aioqzone/aioqzone-0.9.9.dev3-py3-none-any.whl/qqqr/exception.py
@@ -0,0 +1,33 @@
+from typing import Callable
+
+
+class TencentLoginError(RuntimeError):
+    """This exception represents an error that occured in Qzone login,
+    with at least an error code."""
+
+    def __init__(self, code: int, msg: str, *args: object, subcode: int = ...) -> None:
+        self.code = code
+        self.msg = msg
+        if subcode != ...:
+            self.subcode = subcode
+        super().__init__(*args)
+
+    def __str__(self) -> str:
+        return f"Code {self.code}: {self.msg}"
+
+
+class UserBreak(KeyboardInterrupt):
+    """This exception should be raised when user cancel the login spontaneously."""
+
+    def __init__(self, *args: object) -> None:
+        super().__init__(*args)
+
+
+class HookError(RuntimeError):
+    """Once we await a hook, we expect that if the hook is broken, it will not mess up our
+    own error handling. It is convenient to wrap an exception raise from hooks with this error.
+    """
+
+    def __init__(self, hook: Callable) -> None:
+        self.hook = hook
+        super().__init__(f"Error in hook: {hook.__qualname__}")
