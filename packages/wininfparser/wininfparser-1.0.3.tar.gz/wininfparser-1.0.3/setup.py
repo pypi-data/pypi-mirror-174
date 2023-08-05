6a232414f7de462ccf1c43076a28c83546a583d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:18:08 +0000

    Add package wininfparser/1.0.3/wininfparser-1.0.3.tar.gz

---
+++ packages/wininfparser/wininfparser-1.0.3.tar.gz/wininfparser-1.0.3/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='wininfparser',
+    version='1.0.3',
+    license='GPLv3',
+    author="Arsen Arutyunyan",
+    author_email='arutar@bk.ru',
+    py_modules=['wininfparser'],
+    url='https://github.com/arutar/wininfparser',
+    keywords=['inf parser','inf file parser','Windows INF parser'],
+    description='INF file parser',
+)
