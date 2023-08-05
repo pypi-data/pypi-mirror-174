1150fa3e840442aabff80ddd32e0f6b016106007
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:58 +0000

    Add package bioc/2.0.post5/bioc-2.0.post5.tar.gz

---
+++ packages/bioc/bioc-2.0.post5.tar.gz/bioc-2.0.post5/src/bioc/__init__.py
@@ -0,0 +1,20 @@
+"""
+BioC data structures and encoder/decoder for Python
+"""
+from .bioc import BioCAnnotation, BioCCollection, BioCDocument, BioCLocation, BioCNode, BioCPassage, \
+    BioCRelation, BioCSentence, BioCDataModel
+from .validator import validate
+from .biocitertools import annotations, relations, sentences
+from .utils import get_text, pretty_print
+from .biocxml import loads, load, dump, dumps
+from .constants import PASSAGE, DOCUMENT, SENTENCE, BioCVersion
+
+
+__all__ = ['BioCAnnotation', 'BioCCollection', 'BioCDocument', 'BioCLocation', 'BioCNode',
+           'BioCPassage', 'BioCRelation', 'BioCSentence', 'BioCVersion',
+           'BioCDataModel',
+           'validate', 'annotations', 'relations', 'sentences', 'get_text', 'pretty_print',
+           'biocxml', 'biocjson',
+           'load', 'loads', 'dump', 'dumps',
+           'PASSAGE', 'DOCUMENT', 'SENTENCE',
+           'pubtator']
