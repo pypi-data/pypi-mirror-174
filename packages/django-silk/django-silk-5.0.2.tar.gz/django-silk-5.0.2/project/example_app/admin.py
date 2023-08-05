3a21fa4190d1ac4906e1551637fa13317b65dc22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:27 +0000

    Add package django-silk/5.0.2/django-silk-5.0.2.tar.gz

---
+++ packages/django-silk/django-silk-5.0.2.tar.gz/django-silk-5.0.2/project/example_app/admin.py
@@ -0,0 +1,35 @@
+from django.contrib import admin
+from django.urls import reverse
+
+from .models import Blind
+
+
+class BlindAdmin(admin.ModelAdmin):
+    list_display = ('desc', 'thumbnail', 'name', 'child_safe')
+    list_editable = ('name', 'child_safe')
+
+    def thumbnail(self, obj):
+        try:
+            img_tag = '<img src="%s" width="200px"/>' % obj.photo.url
+        except ValueError:
+            return ''
+        url = self._blind_url(obj)
+        return f'<a href="{url}">{img_tag}</a>'
+
+    thumbnail.short_description = 'Photo'
+    thumbnail.allow_tags = True
+
+    def _blind_url(self, obj):
+        url = reverse('admin:example_app_blind_change', args=(obj.id, ))
+        return url
+
+    def desc(self, obj):
+        desc = str(obj)
+        url = self._blind_url(obj)
+        return f'<a href="{url}">{desc}</a>'
+
+    desc.short_description = 'Blind'
+    desc.allow_tags = True
+
+
+admin.site.register(Blind, BlindAdmin)
