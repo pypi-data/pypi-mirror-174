0bd60de13af8e22e99c34939409fbb4bbe418967
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:35:21 +0000

    Add package Digo-generator/0.0.2/Digo_generator-0.0.2.tar.gz

---
+++ packages/Digo-generator/Digo_generator-0.0.2.tar.gz/Digo_generator-0.0.2/setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="Digo_generator", # Replace with your own username
+    version="0.0.2",
+    author="Richard Alphonse",
+    description="It is a random text generator.",
+    python_requires='>=3.6'
+)
