d81db3ee33af940dd9aea2bda6908905408cb387
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:00:23 +0000

    Add package pi-heif/0.7.2/pi_heif-0.7.2-cp36-abi3-musllinux_1_1_aarch64.whl

---
+++ packages/pi-heif/pi_heif-0.7.2-cp36-abi3-musllinux_1_1_aarch64.whl/pi_heif/__init__.py
@@ -0,0 +1,51 @@
+"""
+Import all possible stuff that can be used.
+"""
+
+
+from ._lib_info import (
+    have_decoder_for_format,
+    have_encoder_for_format,
+    libheif_info,
+    libheif_version,
+)
+from ._options import options
+from ._version import __version__
+from .as_opener import (
+    AvifImageFile,
+    HeifImageFile,
+    register_avif_opener,
+    register_heif_opener,
+)
+from .constants import heif_filetype_maybe  # DEPRECATED
+from .constants import heif_filetype_no  # DEPRECATED
+from .constants import heif_filetype_yes_supported  # DEPRECATED
+from .constants import heif_filetype_yes_unsupported  # DEPRECATED
+from .constants import (
+    HeifChannel,
+    HeifChroma,
+    HeifColorProfileType,
+    HeifColorspace,
+    HeifCompressionFormat,
+    HeifErrorCode,
+    HeifFiletype,
+)
+from .error import HeifError
+
+# pylint: disable=redefined-builtin
+from .heif import (
+    HeifFile,
+    HeifImage,
+    HeifThumbnail,
+    check,
+    check_heif,
+    from_bytes,
+    from_pillow,
+    is_supported,
+    open,
+    open_heif,
+    read,
+    read_heif,
+)
+from .misc import get_file_mimetype, getxmp, set_orientation
+from .thumbnails import add_thumbnails, thumbnail
