2a2849f8f1cf779a07f0ada217342ce4d104fc12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:15:16 +0000

    Add package mercuryclient/1.7.3/mercuryclient-1.7.3.tar.gz

---
+++ packages/mercuryclient/mercuryclient-1.7.3.tar.gz/mercuryclient-1.7.3/mercuryclient/types/id_verification/enums.py
@@ -0,0 +1,25 @@
+from enum import Enum
+
+
+class IDTypes(Enum):
+    PAN_CARD = "PAN_CARD"
+    DRIVING_LICENSE = "DRIVING_LICENSE"
+    VOTER_ID = "VOTER_ID"
+    PASSPORT = "PASSPORT"
+    AADHAAR_CARD = "AADHAAR_CARD"
+
+
+class Gender(Enum):
+    MALE = "MALE"
+    FEMALE = "FEMALE"
+    TRANSGENDER = "TRANSGENDER"
+
+
+class PassportTypes(Enum):
+    PERSONAL = "PERSONAL"
+    SERVICE = "SERVICE"
+    DIPLOMATIC = "DIPLOMATIC"
+
+
+class Countries(Enum):
+    INDIA = "INDIA"
