7547729d1153f9a51b6168dae4889d9b0564c58a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:15:58 +0000

    Add package mojang/0.2.4/mojang-0.2.4-py3-none-any.whl

---
+++ packages/mojang/mojang-0.2.4-py3-none-any.whl/mojang/__init__.py
@@ -0,0 +1,10 @@
+from .api import API
+from .client import Client
+
+from .exceptions import (
+    MojangError,
+    HTTPException,
+    LoginError,
+    MissingMinecraftLicense,
+    MissingMinecraftProfile,
+)
