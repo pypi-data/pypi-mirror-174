5ab29705d2ff117d75eb0ff9c65b4f53bc72c46e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:08 +0000

    Add package magic-config/0.0.8/magic_config-0.0.8-py3-none-any.whl

---
+++ packages/magic-config/magic_config-0.0.8-py3-none-any.whl/src/__init__.py
@@ -0,0 +1,13 @@
+import sys
+
+if sys.version_info < (3, 10, 8):
+    raise ImportError('Your Python version {0} is not supported by MagicConfig, please install '
+                      'Python 3.10.8+'.format('.'.join(map(str, sys.version_info[:3]))))
+
+from .magic_config import *
+__all__ = (
+    "MagicConfig",
+    "Config"
+)
+
+__version__ = '0.0.8'
