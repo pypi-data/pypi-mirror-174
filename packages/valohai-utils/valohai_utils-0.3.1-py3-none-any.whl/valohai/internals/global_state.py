4e01331a6711bbc2e4c9977e411dddb3f45966a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:14 +0000

    Add package valohai-utils/0.3.1/valohai_utils-0.3.1-py3-none-any.whl

---
+++ packages/valohai-utils/valohai_utils-0.3.1-py3-none-any.whl/valohai/internals/global_state.py
@@ -0,0 +1,23 @@
+from typing import Any, Dict, Optional
+
+from valohai.distributed import Distributed
+from valohai.internals.input_info import InputInfo
+
+loaded: bool = False
+inputs: Dict[str, InputInfo] = {}
+parameters: Dict[str, Any] = {}
+step_name: Optional[str] = None
+image_name: Optional[str] = None
+environment: Optional[str] = None
+distributed = Distributed()
+
+
+def flush_global_state() -> None:
+    global loaded, inputs, parameters, step_name, image_name, distributed, environment
+    loaded = False
+    inputs = {}
+    parameters = {}
+    step_name = None
+    image_name = None
+    environment = None
+    distributed.flush_state()
