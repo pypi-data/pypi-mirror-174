e7eebe190fe55392f53a8c5d698ddeeb956f0b35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:01:41 +0000

    Add package CoegilCli/1.0.10/CoegilCli-1.0.10.tar.gz

---
+++ packages/CoegilCli/CoegilCli-1.0.10.tar.gz/CoegilCli-1.0.10/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='CoegilCli',
+    version='1.0.10',
+    author="Mike Levine",
+    author_email="mike@coegil.com",
+    description="Coegil Command Line Interface",
+    packages=find_packages(),
+    include_package_data=True,
+    url="https://coegil.com",
+    python_requires='>=3.9',
+    install_requires=[
+        'click',
+        'wheel',
+        'boto3',
+        'requests',
+        'coegil-sdk'
+    ],
+    entry_points={
+        'console_scripts': [
+            'coegil=coegil.Cli:cli'
+        ]
+    }
+)
