8c3832b17afee2c579ab74d005319c61b226d365
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:23:58 +0000

    Add package bitdotio/2.0.0b0/bitdotio-2.0.0b0.tar.gz

---
+++ packages/bitdotio/bitdotio-2.0.0b0.tar.gz/bitdotio-2.0.0b0/setup.py
@@ -0,0 +1,63 @@
+# coding: utf-8
+
+"""
+    bit.io REST API
+
+    bit.io API  # noqa: E501
+"""
+
+
+from setuptools import setup, find_packages  # noqa: H301
+
+NAME = "bitdotio"
+VERSION = "2.0.0b"
+# To install the library, run the following
+#
+# python setup.py install
+#
+# prerequisite: setuptools
+# http://pypi.python.org/pypi/setuptools
+
+REQUIRES = ["urllib3 >= 1.15", "six >= 1.10", "certifi", "python-dateutil", "click >= 8.0.1"]
+
+EXTRAS = {
+    "psycopg2": [
+        "psycopg2>=2.8.6",
+    ],
+    "psycopg2-binary": [
+        "psycopg2-binary>=2.8.6",
+    ],
+}
+
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name=NAME,
+    version=VERSION,
+    description="bit.io Python SDK and CLI",
+    author="bit.io",
+    author_email="python@bit.io",
+    url="https://github.com/bitdotioinc/python-bitdotio",
+    keywords=["bit.io", "Database", "bit.io Python SDK"],
+    install_requires=REQUIRES,
+    extras_require=EXTRAS,
+    packages=find_packages(exclude=["test", "tests"]),
+    include_package_data=True,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    project_urls={
+        "Bug Tracker": "https://github.com/bitdotioinc/python-bitdotio/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+    entry_points={
+        'console_scripts': [
+            'bit = bit.bit:main',
+        ]
+    },
+)
