bfce8a636fad720365439cfd09478189cf659d4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:17:46 +0000

    Add package ffuzzer/0.1.4/ffuzzer-0.1.4.tar.gz

---
+++ packages/ffuzzer/ffuzzer-0.1.4.tar.gz/ffuzzer-0.1.4/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ffuzzer',
+    description='ffuzzer is a CLI tool that makes fuzzing format string offsets easy, especially relevant for full RELRO format string challenges, where you want to leak as much info from the binary as you can.',
+    version='0.1.4',
+    license='MIT',
+    author='samuzora',
+    author_email='lucastanyj@gmail.com',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    py_modules=['ffuzzer'],
+    keywords='formatstring pwn',
+    url='https://github.com/samuzora/ffuzzer',
+    install_requires=[
+        'Click',
+        'pwntools'
+        'rich',
+        'regex'
+    ],
+    python_requires='>=3.9',
+    entry_points={
+        'console_scripts': [
+            'ffuzzer=ffuzzer:cli',
+        ],
+    },
+)
