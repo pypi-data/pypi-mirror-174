ebfa0f6007a5bd8949574e61a249929c84ee3470
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:29:43 +0000

    Add package btrace/0.0.1/btrace-0.0.1.tar.gz

---
+++ packages/btrace/btrace-0.0.1.tar.gz/btrace-0.0.1/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+    name="btrace",
+    version="0.0.1",
+    author="btrace",
+    description="A small example package",
+    long_description="Nothing to desc",
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
