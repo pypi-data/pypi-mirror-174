eb1c020157230da24bc10e482845a2c1586ee3a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:50 +0000

    Add package probability-distributions-dhan/0.1/probability-distributions-dhan-0.1.tar.gz

---
+++ packages/probability-distributions-dhan/probability-distributions-dhan-0.1.tar.gz/probability-distributions-dhan-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='probability-distributions-dhan',
+      version='0.1',
+      description='Probability distributions',
+      packages=['probability-distributions-dhan'],
+      zip_safe=False)
