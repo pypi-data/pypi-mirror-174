87c07862e7c79c60b9228bfe03ffac0749e90216
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:09 +0000

    Add package stitch2d/1.2/stitch2d-1.2-py3-none-any.whl

---
+++ packages/stitch2d/stitch2d-1.2-py3-none-any.whl/stitch2d/__init__.py
@@ -0,0 +1,8 @@
+"""Stitches a 2D grid of images into a mosaic"""
+from .mosaic import Mosaic, StructuredMosaic, create_mosaic, build_grid, is_grid
+from .tile import Tile, OpenCVTile, ScikitImageTile
+
+
+__version__ = "1.2"
+__author__ = "Adam Mansur"
+__credits__ = "Smithsonian National Museum of Natural History"
