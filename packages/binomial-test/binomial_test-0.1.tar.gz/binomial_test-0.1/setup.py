ab7e17c11463f72becd209de55597dc9fbf4edba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:16 +0000

    Add package binomial-test/0.1/binomial_test-0.1.tar.gz

---
+++ packages/binomial-test/binomial_test-0.1.tar.gz/binomial_test-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='binomial_test',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['binomial_test'],
+      zip_safe=False)
