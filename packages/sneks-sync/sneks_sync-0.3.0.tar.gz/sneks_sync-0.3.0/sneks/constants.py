c5a68637bb1d78f9f28415e403d23ec1e903c4a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:10:38 +0000

    Add package sneks-sync/0.3.0/sneks_sync-0.3.0.tar.gz

---
+++ packages/sneks-sync/sneks_sync-0.3.0.tar.gz/sneks_sync-0.3.0/sneks/constants.py
@@ -0,0 +1,7 @@
+COILED_ACCOUNT_NAME = "gjoseph92"
+DOCKER_USERNAME = "jabriel"
+PROJECT_NAME = "sneks"
+REQUIRED_PACKAGES = frozenset(
+    ["dask", "distributed", "bokeh", "cloudpickle", "msgpack"]
+)
+OPTIONAL_PACKAGES = frozenset(["numpy", "pandas", "s3fs", "pyarrow, dask-pyspy"])
