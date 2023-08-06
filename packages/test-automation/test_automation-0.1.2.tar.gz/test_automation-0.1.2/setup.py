9534bf49d25c5106697d820623aff04464962b59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:17:25 +0000

    Add package test-automation/0.1.2/test_automation-0.1.2.tar.gz

---
+++ packages/test-automation/test_automation-0.1.2.tar.gz/test_automation-0.1.2/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['test_automation']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['pytest>=7.2.0,<8.0.0', 'requests>=2.28.1,<3.0.0']
+
+setup_kwargs = {
+    'name': 'test-automation',
+    'version': '0.1.2',
+    'description': 'test framework for yogiyo',
+    'long_description': 'None',
+    'author': 'KimKitaeB',
+    'author_email': 'kt.kim@wesang.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
---
+++ packages/test-automation/test_automation-0.1.2.tar.gz/test_automation-0.1.2/test_automation/__init__.py
@@ -0,0 +1,12 @@
+from .api import *
+from .vault import *
+from .ui import *
+
+# API
+# VAULT
+# UI
+
+__all__ = ["login", \
+           "get_path_value", "get_key_value", \
+           "login_ui"]
+
