a897eede0dbc5df848b33effbef5456432214af6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:05:02 +0000

    Add package MariaDB-SQLBuilder/0.5.1/MariaDB-SQLBuilder-0.5.1.tar.gz

---
+++ packages/MariaDB-SQLBuilder/MariaDB-SQLBuilder-0.5.1.tar.gz/MariaDB-SQLBuilder-0.5.1/mariadb_sqlbuilder/builder/deleteBuilder.py
@@ -0,0 +1,32 @@
+from typing import Union
+
+from ..execution.executeFunctions import execute as fExecute
+from .baseBuilder import ConditionsBuilder
+
+
+class DeleteBuilder(ConditionsBuilder):
+
+    def __init__(self, tb):
+        super().__init__(tb)
+        self.sureNotUseConditions = False
+
+    def imSureImNotUseConditions(self, imSure: bool = False):
+        self.sureNotUseConditions = imSure
+        return self
+
+    def execute(self) -> bool:
+        if not self._where_conditions and not self.sureNotUseConditions:
+            raise PermissionError('Delete Builder: You are not sure enough not to use where')
+        cursor = self.tb.connect.getAvailableCursor()
+        result = fExecute(
+            cursor,
+            self.get_sql()
+        )
+        cursor._connection.commit()
+        self.tb.connect.makeCursorAvailable(cursor)
+        return result
+
+    def get_sql(self) -> str:
+        return f"DELETE FROM {self.tb.table} " \
+            f"{self._getWhereSQL()};"
+
