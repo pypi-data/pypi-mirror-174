2ae594d0e43aa7b0c81b2738c2367c07fe4c546b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:20:19 +0000

    Add package pagium/0.0.18/pagium-0.0.18-py3-none-any.whl

---
+++ packages/pagium/pagium-0.0.18-py3-none-any.whl/pagium/__init__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+"""
+Python page object patter realization for selenium library.
+"""
+
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+from pagium.page import Page, PageElement, WebElement
+from pagium.webdriver import Remote, Chrome, Firefox, Safari
+
+
+__all__ = [
+    'By',
+    'Page',
+    'PageElement',
+    'WebElement',
+    'Remote',
+    'Chrome',
+    'Firefox',
+    'Safari',
+    'Keys',
+]
