1797e60833b8e666e4c825242fa7187a22899d3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:22:00 +0000

    Add package mmselfsup/1.0.0rc3/mmselfsup-1.0.0rc3-py3-none-any.whl

---
+++ packages/mmselfsup/mmselfsup-1.0.0rc3-py3-none-any.whl/mmselfsup/models/backbones/__init__.py
@@ -0,0 +1,13 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+from .cae_vit import CAEViT
+from .mae_vit import MAEViT
+from .maskfeat_vit import MaskFeatViT
+from .mocov3_vit import MoCoV3ViT
+from .resnet import ResNet, ResNetSobel, ResNetV1d
+from .resnext import ResNeXt
+from .simmim_swin import SimMIMSwinTransformer
+
+__all__ = [
+    'ResNet', 'ResNetSobel', 'ResNetV1d', 'ResNeXt', 'MAEViT', 'MoCoV3ViT',
+    'SimMIMSwinTransformer', 'CAEViT', 'MaskFeatViT'
+]
