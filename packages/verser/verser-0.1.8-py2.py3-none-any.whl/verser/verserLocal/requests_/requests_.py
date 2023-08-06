ebcc62150fc33f52761fbca8e7e63beeffb6fb03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:01:24 +0000

    Add package verser/0.1.8/verser-0.1.8-py2.py3-none-any.whl

---
+++ packages/verser/verser-0.1.8-py2.py3-none-any.whl/verser/verserLocal/requests_/requests_.py
@@ -0,0 +1,23 @@
+from .ev_request import EVRequest
+
+"""Request Class Instance"""
+ev = EVRequest()
+
+from rich import inspect
+
+
+def get_request_with_cache(url):
+    # ev = EVRequest()
+
+    response = ev.get_request_by_checking_cache(url)
+    # print(url, "URL")
+
+    return response
+
+def get_request_without_cache(url):
+    # ev = EVRequest()
+
+    response = ev.get_request_by_checking_NO_CACHE(url)
+    # print(url, "URL")
+
+    return response
