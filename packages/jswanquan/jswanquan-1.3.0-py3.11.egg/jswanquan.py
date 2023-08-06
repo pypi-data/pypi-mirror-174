6ca8677206d16b31ecc303a768520264efeb7d3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:19:26 +0000

    Add package jswanquan/1.3.0/jswanquan-1.3.0-py3.11.egg

---
+++ packages/jswanquan/jswanquan-1.3.0-py3.11.egg/jswanquan.py
@@ -0,0 +1,13 @@
+"""This is a standard way to include a multiple-line comment in your code."""
+def print_lol(the_list,indent=False,level=0):
+    """这个函数有一个位置参数，名为“the_list”，
+这可以是任何Python列表（包含或不包含嵌套列表），
+所提供列表中的各个数据项会（递归地）打印到屏幕上，而且各占一行"""
+    for each_item in the_list:
+        if isinstance(each_item,list):
+            print_lol(each_item,indent,level+1)
+        else:
+            if indent:
+                for tab_stop in range(level):
+                    print("\t",end='')
+            print(each_item)
