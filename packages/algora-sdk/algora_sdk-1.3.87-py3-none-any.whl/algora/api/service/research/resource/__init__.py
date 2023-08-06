a9b6dc234f8e5015d4e171e0025454cdd5b33081
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:07:26 +0000

    Add package algora-sdk/1.3.87/algora_sdk-1.3.87-py3-none-any.whl

---
+++ packages/algora-sdk/algora_sdk-1.3.87-py3-none-any.whl/algora/api/service/research/resource/__init__.py
@@ -0,0 +1,2 @@
+from algora.api.service.research.resource.asynchronous import async_get_resource, async_delete_resource
+from algora.api.service.research.resource.synchronous import get_resource, delete_resource
