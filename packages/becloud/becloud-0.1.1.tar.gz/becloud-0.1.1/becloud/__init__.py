0abbd43b21c2614d6e865089ba18720097c0acc6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:27:28 +0000

    Add package becloud/0.1.1/becloud-0.1.1.tar.gz

---
+++ packages/becloud/becloud-0.1.1.tar.gz/becloud-0.1.1/becloud/__init__.py
@@ -0,0 +1,13 @@
+# Files
+from . import string
+
+# Folders
+# from . import lookup
+
+
+def printName():
+    print("becloud")
+
+
+def files():
+    print(1)
---
+++ packages/becloud/becloud-0.1.1.tar.gz/becloud-0.1.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='becloud',
+      version='0.1.1',
+      description='Backend Cloud Package',
+      packages=['becloud'],
+      install_requires=[],
+      zip_safe=False,
+      )
