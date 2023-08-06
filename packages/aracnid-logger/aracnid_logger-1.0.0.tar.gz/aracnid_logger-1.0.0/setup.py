9f46f4f9dc2c49702246f76cef16d222e58d8d21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:49:28 +0000

    Add package aracnid-logger/1.0.0/aracnid_logger-1.0.0.tar.gz

---
+++ packages/aracnid-logger/aracnid_logger-1.0.0.tar.gz/aracnid_logger-1.0.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['aracnid_logger']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['slack-bolt>=1.14,<1.15']
+
+setup_kwargs = {
+    'name': 'aracnid-logger',
+    'version': '1.0.0',
+    'description': 'Customization of the Python Logging System',
+    'long_description': '# Aracnid Logger\n\nThis is a simple customization of the Python Logging System.\n\n## Getting Started\n\nThese instructions will get you a copy of the project up and running on your local machine for development and testing purposes.\n\n### Prerequisites\n\nThis package supports the following version of Python. It probably supports older versions, but they have not been tested.\n\n- Python 3.10 or later\n\n### Installing\n\nInstall the latest package using pip.\n\n```\n$ pip install aracnid-logger\n```\n\nEnd with an example of getting some data out of the system or using it for a little demo\n\n## Running the tests\n\nExplain how to run the automated tests for this system\n\n```\n$ python -m pytest\n```\n\n## Usage\n\nTODO\n\n## Authors\n\n- **Jason Romano** - [Aracnid](https://github.com/aracnid)\n\nSee also the list of [contributors](https://github.com/lakeannebrewhouse/aracnid-logger/contributors) who participated in this project.\n\n## License\n\nThis project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details\n',
+    'author': 'Jason Romano',
+    'author_email': 'aracnid@gmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'https://github.com/aracnid/aracnid-logger',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
