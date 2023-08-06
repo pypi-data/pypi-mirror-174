f46e1e17aba8b129201344a55cd779b7f08251a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:24 +0000

    Add package pyHM/0.0.7/pyHM-0.0.7.tar.gz

---
+++ packages/pyHM/pyHM-0.0.7.tar.gz/pyHM-0.0.7/pyHM/__init__.py
@@ -0,0 +1,36 @@
+"""
+__init__.py
+Contains the package name and the author's name
+---
+get_author() returns the author
+mouse creates the mouse Class
+"""
+
+
+import numpy
+import random
+import pyautogui
+
+from scipy import interpolate
+
+from .mouse import Mouse
+
+
+name = 'pyHM'
+author = "Joe Tilsed - https://linkedin.com/in/joetilsed/"
+
+
+def get_author():
+    return author
+
+
+mouse_dependencies = {
+    'interpolate': interpolate,
+    'pyautogui': pyautogui,
+    'numpy': numpy,
+    'random': random
+}
+mouse = Mouse(mouse_dependencies)
+
+
+# That's all folks...
