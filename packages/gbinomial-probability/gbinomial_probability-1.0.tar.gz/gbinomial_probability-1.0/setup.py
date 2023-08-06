cb38df0881fe4bccddd7e4477890de7253381d91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:15:24 +0000

    Add package gbinomial-probability/1.0/gbinomial_probability-1.0.tar.gz

---
+++ packages/gbinomial-probability/gbinomial_probability-1.0.tar.gz/gbinomial_probability-1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='gbinomial_probability',
+    version='1.0',
+    description='Gaussian and Binomial Distributions',
+    packages=['gbinomial_probability'],
+    author= 'Seah Mei Yin',
+    author_email= 'tiffanyseah94@gmail.com',
+    zip_safe=False)
