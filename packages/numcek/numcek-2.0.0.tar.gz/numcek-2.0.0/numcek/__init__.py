8ece0ac7b026d8579baab5e5fafe0e71b0a6c1ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:15:20 +0000

    Add package numcek/2.0.0/numcek-2.0.0.tar.gz

---
+++ packages/numcek/numcek-2.0.0.tar.gz/numcek-2.0.0/numcek/__init__.py
@@ -0,0 +1,48 @@
+def factorial(number):
+    fact = 1
+    if number < 0:
+        print("Factorial for negative numbers don't exist...")
+    else:
+        for i in range(1,number+1):
+            fact = fact*i
+            i = i+1
+    return fact
+
+
+def armstrong(number):
+
+    sum = 0
+
+    tempoarary = number
+    while tempoarary > 0:
+        remainder = tempoarary % 10
+        sum += remainder ** 3
+        tempoarary //= 10
+
+    if number == sum:
+        return True
+    else:
+        return False
+
+
+def decimal(binary):
+    decimal,i,n = 0,0,0
+    while (binary != 0):
+        remainder = binary % 10
+        decimal = decimal + remainder * 2 ** i
+        binary = binary // 10
+        i = i+1
+    return decimal
+
+
+def binary(decimal):
+    bin = 0
+    ctr = 0
+    temp = decimal  #copy input decimal
+    #find binary value using while loop
+    while(temp > 0):
+         bin = ((temp%2)*(10**ctr)) + bin
+         temp = int(temp/2)
+         ctr += 1
+    return bin
+
