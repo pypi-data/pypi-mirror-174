b45aff1e0e0f1a78849233bdb865a7fe5d1ed8ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:02:34 +0000

    Add package python-search/0.9.1/python_search-0.9.1-py3-none-any.whl

---
+++ packages/python-search/python_search-0.9.1-py3-none-any.whl/python_search/entry_capture/entry_inserter_gui.py
@@ -0,0 +1,155 @@
+import logging
+from dataclasses import dataclass
+from typing import List
+
+import fire
+
+from python_search.config import ConfigurationLoader
+from python_search.entry_type.classifier_inference import \
+    ClassifierInferenceClient
+
+
+@dataclass
+class EntryData:
+    """
+    Entry _entries schema
+
+    """
+
+    key: str
+    value: str
+    type: str
+    tags: List[str]
+
+
+class EntryCaptureGUI:
+    def __init__(self):
+        self._configuration = ConfigurationLoader().load_config()
+        self._tags = self._configuration._default_tags
+
+    def launch(
+        self,
+        title: str = "Capture Entry",
+        default_key="",
+        default_content: str = "",
+        serialize_output=False,
+        default_type="Snippet",
+    ) -> EntryData:
+        """
+        Launch the _entries capture GUI.
+        """
+        import PySimpleGUI as sg
+
+        self._sg = sg
+
+        config = ConfigurationLoader().load_config()
+        sg.theme(config.simple_gui_theme)
+        font_size = config.simple_gui_font_size
+
+        content_input = sg.Input(
+            key="content",
+            default_text=default_content,
+            expand_x=True,
+            expand_y=True,
+        )
+        entry_type = sg.Combo(
+            [
+                "Snippet",
+                "Cmd",
+                "Url",
+                "File",
+                "Anonymous",
+            ],
+            key="type",
+            default_value=default_type,
+        )
+
+        tags_chucks = self._chunks(self._tags, 4)
+        layout = [
+            [sg.Text("Entry content")],
+            [content_input],
+            [sg.Text("Descriptive key name")],
+            [
+                sg.Input(
+                    key="key", default_text=default_key, expand_x=True, expand_y=True
+                )
+            ],
+            [sg.Text("Type")],
+            [entry_type],
+            [sg.Text("Tags")],
+            [self._checkbox_list(i) for i in tags_chucks],
+            [sg.Button("Write", key="write")],
+        ]
+
+        window = sg.Window(
+            title,
+            layout,
+            font=("Helvetica", font_size),
+            finalize=True,
+        )
+
+        # workaround for mac bug
+
+        content_input.update(select=True)
+
+        window["key"].bind("<Return>", "_Enter")
+        window["content"].bind("<Return>", "_Enter")
+        window["type"].bind("<Return>", "_Enter")
+        window["key"].bind("<Escape>", "_Esc")
+        window["content"].bind("<Escape>", "_Esc")
+        window["type"].bind("<Escape>", "_Esc")
+
+        while True:
+            new_type = ClassifierInferenceClient().predict_from_content(default_content)
+            print(f"New type: {new_type}")
+            if new_type:
+                entry_type.update(new_type)
+            event, values = window.read()
+            if event and (event == "write" or event.endswith("_Enter")):
+                break
+            if event == sg.WINDOW_CLOSED or event.endswith("_Esc"):
+                raise Exception("Quitting window")
+
+        window.close()
+        logging.info("values", values)
+
+        selected_tags = []
+        for key, value in values.items():
+            if key in self._tags and value == True:
+                selected_tags.append(key)
+
+        result = EntryData(
+            values["key"], values["content"], values["type"], selected_tags
+        )
+
+        if serialize_output:
+            result = result.__dict__
+
+        return result
+
+    def _classify_type(self, content):
+        from subprocess import Popen
+
+        p = Popen(
+            f"python_search _entry_type_classifier inference_client predict_from_content  '{content}'",
+            shell=True,
+            stdin=None,
+            stdout=None,
+            stderr=None,
+            close_fds=True,
+        )
+
+        print("process end")
+        return p
+
+    def _checkbox_list(self, tags):
+        return ([self._sg.Checkbox(tag, key=tag, default=False) for tag in tags],)
+
+    def _chunks(self, lst, n):
+        """Yield successive n-sized chunks from lst."""
+        for i in range(0, len(lst), n):
+            yield lst[i : i + n]
+
+
+if __name__ == "__main__":
+    fire.Fire(EntryCaptureGUI().launch)
