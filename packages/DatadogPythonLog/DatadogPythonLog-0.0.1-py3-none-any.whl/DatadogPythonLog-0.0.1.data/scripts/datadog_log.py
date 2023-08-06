5d7d9037bd5b103780ba444ab70eeaa227295f63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:21:01 +0000

    Add package DatadogPythonLog/0.0.1/DatadogPythonLog-0.0.1-py3-none-any.whl

---
+++ packages/DatadogPythonLog/DatadogPythonLog-0.0.1-py3-none-any.whl/DatadogPythonLog-0.0.1.data/scripts/datadog_log.py
@@ -0,0 +1,40 @@
+from datadog_api_client import ApiClient, Configuration
+from datadog_api_client.v2.api.logs_api import LogsApi
+from datadog_api_client.v2.model.content_encoding import ContentEncoding
+from datadog_api_client.v2.model.http_log import HTTPLog
+from datadog_api_client.v2.model.http_log_item import HTTPLogItem
+
+class DataDog:
+    def __init__(self, source, host, service):
+        self.source = source
+        self.host = host
+        self.service = service
+
+    def info(self, logs):
+        self.common(logs, 'info')
+
+    def warn(self, logs):
+        self.common(logs, 'warn')
+
+    def error(self, logs):
+        self.common(logs, 'error')
+
+    def common(self, msg, level):
+        body = HTTPLog(
+            [
+                HTTPLogItem(
+                    ddsource=self.source,
+                    ddtags="env:dev,version:5.1",
+                    hostname=self.host,
+                    message=msg,
+                    service=self.service,
+                    status=level,
+                    functionname="hello world"
+                ),
+            ]
+        )
+        configuration = Configuration()
+        with ApiClient(configuration) as api_client:
+            api_instance = LogsApi(api_client)
+            response = api_instance.submit_log(body=body)
+            print(response)
