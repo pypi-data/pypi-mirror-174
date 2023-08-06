a15e67da314c8a14f76bd866251ebefdbb25c581
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:56:38 +0000

    Add package spectrochempy/0.4.8/spectrochempy-0.4.8.tar.gz

---
+++ packages/spectrochempy/spectrochempy-0.4.8.tar.gz/spectrochempy-0.4.8/spectrochempy/core/project/baseproject.py
@@ -0,0 +1,19 @@
+# ======================================================================================================================
+#  Copyright (©) 2015-2022 LCS - Laboratoire Catalyse et Spectrochimie, Caen, France.
+#  CeCILL-B FREE SOFTWARE LICENSE AGREEMENT - See full LICENSE agreement in the root directory.
+# ======================================================================================================================
+
+
+# import uuid
+
+from traitlets import HasTraits
+
+__all__ = []
+
+
+class AbstractProject(HasTraits):
+    """
+    Project class will subclass it.
+
+    This is mainly for type comparison purpose.
+    """
