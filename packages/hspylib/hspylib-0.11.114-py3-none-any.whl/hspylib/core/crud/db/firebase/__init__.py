266cff225167582ae3cc82048a66f88830ebd731
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:37 +0000

    Add package hspylib/0.11.114/hspylib-0.11.114-py3-none-any.whl

---
+++ packages/hspylib/hspylib-0.11.114-py3-none-any.whl/hspylib/core/crud/db/firebase/__init__.py
@@ -0,0 +1,12 @@
+# _*_ coding: utf-8 _*_
+#
+# hspylib v0.11.114
+#
+# Package: main.hspylib.core.crud.db.firebase
+"""Package initialization."""
+
+__all__ = [
+  'firebase_config', 
+  'firebase_repository'
+]
+__version__ = '0.11.114'
