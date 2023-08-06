2eef75819b7868008ee4dc8c52d28f80385cdba1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:57:11 +0000

    Add package sbat/0.0.9/sbat-0.0.9.tar.gz

---
+++ packages/sbat/sbat-0.0.9.tar.gz/sbat-0.0.9/setup.py
@@ -0,0 +1,37 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="sbat",
+    version="0.0.9",
+    author="Alexandra Skyslakova",
+    author_email="alexandra.skyslakova@gmail.com",
+    description="A tool for strand bias analysis of NGS data",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/alex-skyslakova/strand-bias-analysis-tool",
+    install_requires=[
+        'Bio==1.3.8',
+        'matplotlib==3.5.2',
+        'numpy==1.22.3',
+        'pandas==1.4.2',
+        'python_dateutil==2.8.2',
+        'pytz==2022.1',
+        'setuptools==45.2.0'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: MacOS",
+        "Operating System :: Unix",
+    ],
+    packages=setuptools.find_packages(),
+    python_requires=">=3.7",
+    entry_points={
+        'console_scripts': [
+            'sbat=sbat.main:main',
+        ],
+    }
+)
