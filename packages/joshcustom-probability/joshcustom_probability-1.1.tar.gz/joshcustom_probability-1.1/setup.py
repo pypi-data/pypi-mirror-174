e2b6b4ac02dae3f6302e478c2452a7a7676a6009
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:49 +0000

    Add package joshcustom-probability/1.1/joshcustom_probability-1.1.tar.gz

---
+++ packages/joshcustom-probability/joshcustom_probability-1.1.tar.gz/joshcustom_probability-1.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='joshcustom_probability',
+      version='1.1',
+      description='Gaussian and Binomial distributions',
+      packages=['joshcustom_probability'],
+      author='Owoicho Joshua Odinya',
+      author_email='owoichojoshuaodinya@gmail.com',
+      zip_safe=False)
