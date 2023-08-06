2c330573c02c9150ceb40485463c1fcfe919304f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:14:46 +0000

    Add package duote/0.1.7/duote-0.1.7.tar.gz

---
+++ packages/duote/duote-0.1.7.tar.gz/duote-0.1.7/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+
+
+setup(
+    name='duote',  # 对外模块的名字
+    version='0.1.7',  # 版本号
+    description='测试版本,基本功能',  # 描述
+    author='JinxAndLux',  # 作者
+    install_requires=[],#依赖
+    author_email='yby1234@hotmail.com',
+    py_modules=['duote'],  # 要发布的模块
+
+)
