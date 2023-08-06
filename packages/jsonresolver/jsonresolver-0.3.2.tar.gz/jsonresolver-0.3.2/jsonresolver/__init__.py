0827bfa61499ac5497c931f5c52ce6c1cdbbda47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:29 +0000

    Add package jsonresolver/0.3.2/jsonresolver-0.3.2.tar.gz

---
+++ packages/jsonresolver/jsonresolver-0.3.2.tar.gz/jsonresolver-0.3.2/jsonresolver/__init__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of jsonresolver
+# Copyright (C) 2015 CERN.
+#
+# jsonresolver is free software; you can redistribute it and/or
+# modify it under the terms of the Revised BSD License; see LICENSE
+# file for more details.
+
+"""JSON data resolver with support for plugins."""
+
+
+from .core import JSONResolver
+from .decorators import route
+from .hookimpl import hookimpl
+
+__version__ = "0.3.2"
+
+__all__ = (
+    "JSONResolver",
+    "hookimpl",
+    "route",
+    "__version__",
+)
