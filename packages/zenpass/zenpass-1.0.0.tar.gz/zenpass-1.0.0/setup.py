613a704a710a31ddddbcfeb045d1ef3668476db3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:00:10 +0000

    Add package zenpass/1.0.0/zenpass-1.0.0.tar.gz

---
+++ packages/zenpass/zenpass-1.0.0.tar.gz/zenpass-1.0.0/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='zenpass',
+    packages=setuptools.find_packages(),
+    version='1.0.0',
+    license='Apache License, Version 2.0',
+    description='To generate random and strong passwords.',
+    author='srg',
+    author_email='srg.code@pm.me',
+    url='https://github.com/codesrg/zenpass',
+    download_url='https://github.com/codesrg/zenpass/releases',
+    include_package_data=True,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    entry_points={'console_scripts': ['zenpass=zenpass.__main__:main']
+                  },
+    python_requires='>=3.6',
+)
