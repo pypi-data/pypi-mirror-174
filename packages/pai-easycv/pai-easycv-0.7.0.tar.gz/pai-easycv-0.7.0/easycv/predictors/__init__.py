22c5c621b1406641adb31ecdf60a68de3228415c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:24:30 +0000

    Add package pai-easycv/0.7.0/pai-easycv-0.7.0.tar.gz

---
+++ packages/pai-easycv/pai-easycv-0.7.0.tar.gz/pai-easycv-0.7.0/easycv/predictors/__init__.py
@@ -0,0 +1,15 @@
+# Copyright (c) Alibaba, Inc. and its affiliates.
+from .bevformer_predictor import BEVFormerPredictor
+from .classifier import TorchClassifier
+from .detector import (DetectionPredictor, TorchFaceDetector,
+                       TorchYoloXClassifierPredictor, TorchYoloXPredictor,
+                       YoloXPredictor)
+from .face_keypoints_predictor import FaceKeypointsPredictor
+from .feature_extractor import (TorchFaceAttrExtractor,
+                                TorchFaceFeatureExtractor,
+                                TorchFeatureExtractor)
+from .hand_keypoints_predictor import HandKeypointsPredictor
+from .pose_predictor import (TorchPoseTopDownPredictor,
+                             TorchPoseTopDownPredictorWithDetector)
+from .segmentation import Mask2formerPredictor, SegmentationPredictor
+from .wholebody_keypoints_predictor import WholeBodyKeypointsPredictor
