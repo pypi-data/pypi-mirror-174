3455969f6f291678908e0fec3b49d479949d9adb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:06:41 +0000

    Add package shapelets-platform/2.0.41/shapelets_platform-2.0.41-py3-none-any.whl

---
+++ packages/shapelets-platform/shapelets_platform-2.0.41-py3-none-any.whl/shapelets/apps/widgets/layouts/vertical_layout.py
@@ -0,0 +1,48 @@
+from dataclasses import dataclass
+from typing import List, Optional, Tuple
+
+from ..widget import Widget, AttributeNames
+from .panel import PanelWidget, Panel
+
+
+@dataclass
+class VerticalLayout(Panel):
+    pass
+
+
+class VerticalLayoutWidget(PanelWidget):
+    """
+    Creates a layout that holds widget inside it vertically (stacked on-top of one another).
+    """
+
+    def __init__(self,
+                 panel_title: Optional[str] = None,
+                 panel_id: Optional[str] = None,
+                 **additional):
+        self._parent_class = VerticalLayout.__name__
+        super().__init__(panel_title, panel_id, **additional)
+        self._compatibility: Tuple = (list.__name__, List._name)
+        self.placements = dict()
+
+    def place(self, widget: Widget, width: float = 100.0, offset: int = 0):
+        """
+        Place widget inside layout
+        param widget: widget to place.
+        param width: understood as a percentage.
+        param offset: distance with the next widget.
+        """
+        if width > 100:
+            raise Exception("Width cannot be over a 100")
+        super()._place(widget)
+        self.placements[widget.widget_id] = (width, offset)
+
+    def to_dict_widget(self):
+        panel_dict = super().to_dict_widget()
+        panel_dict[AttributeNames.PROPERTIES.value].update({
+            AttributeNames.PLACEMENTS.value: [{
+                AttributeNames.WIDGET_REF.value: key,
+                AttributeNames.WIDTH.value: width,
+                AttributeNames.OFFSET.value: offset
+            } for key, (width, offset) in self.placements.items()]
+        })
+        return panel_dict
