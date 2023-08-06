d9b2adb1a3c24205b5b76a4ead4705b211844e72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:14:07 +0000

    Add package prose/2.2.0/prose-2.2-py3-none-any.whl

---
+++ packages/prose/prose-2.2-py3-none-any.whl/prose/__init__.py
@@ -0,0 +1,23 @@
+import warnings
+from astropy.wcs import FITSFixedWarning
+
+warnings.simplefilter("ignore", FITSFixedWarning)
+
+from . import config
+
+CONFIG = config.ConfigManager()
+CONFIG.check_builtins_changes()
+
+from . import visualization as viz
+
+from .io.fitsmanager import FitsManager
+from .fluxes import ApertureFluxes
+from .telescope import Telescope
+from .block import Block
+from .sequence import Sequence
+from .image import Image
+from .observation import Observation
+from .observations import Observations
+
+from pkg_resources import get_distribution
+__version__ = get_distribution('prose').version
