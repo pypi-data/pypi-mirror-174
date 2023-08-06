c6c78a9db4ff7fd84d2214aff2869d0219c2c4fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:01 +0000

    Add package clashofclans-api/0.0.4/clashofclans-api-0.0.4.tar.gz

---
+++ packages/clashofclans-api/clashofclans-api-0.0.4.tar.gz/clashofclans-api-0.0.4/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='clashofclans-api',
+    version='0.0.4',
+    description='This is an easy to use API of the game Clash of Clans',
+    long_description="NOT READY TO USE",
+    author='Luca Sebastian Koch',
+    author_email='luca.sebastian.koch@gmail.com',
+    # maintainer="",
+    # maintainer_email="",
+    url='https://github.com/Acul747/Clash-of-Clans-API',
+    # download_url="",
+    license='MIT',
+    packages=find_packages(),
+    py_modules=["clashofclans"],
+    install_requires=[
+        'requests',
+    ],
+    keywords=['Clash of Clans', 'clash of clans', 'coc', 'coc api', 'clashofclans', 'clashofclans api'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+    ]
+)
