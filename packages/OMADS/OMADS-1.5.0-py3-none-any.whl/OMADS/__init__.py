387c6b8cd9bbde472d93151bb9d1a707a25ce593
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:52 +0000

    Add package OMADS/1.5.0/OMADS-1.5.0-py3-none-any.whl

---
+++ packages/OMADS/OMADS-1.5.0-py3-none-any.whl/OMADS/__init__.py
@@ -0,0 +1,6 @@
+from .POLL import DType, Options, Parameters, Evaluator, Point,\
+    OrthoMesh, Cache, Dirs2n, PreMADS, Output, PostMADS, main
+
+__all__ = ['DType', 'Options', 'Parameters',
+           'Evaluator', 'Point', 'OrthoMesh', 'Cache',
+           'Dirs2n', 'PreMADS', 'Output', 'PostMADS', 'main']
