c02d01b08061403fa83d4e0c0e8b7af3c0655f7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:58 +0000

    Add package django-formset/0.11.dev1/django-formset-0.11.dev1.tar.gz

---
+++ packages/django-formset/django-formset-0.11.dev1.tar.gz/django-formset-0.11.dev1/formset/static/formset/js/SortableSelect-3YARVPOL.js
@@ -0,0 +1,2 @@
+import{a}from"./chunk-5VASUKZ3.js";import"./chunk-SNXKFZ2Y.js";import"./chunk-XXGBPDQQ.js";import"./chunk-ON5OQYWL.js";export{a as SortableSelectElement};
+//# sourceMappingURL=SortableSelect-3YARVPOL.js.map
