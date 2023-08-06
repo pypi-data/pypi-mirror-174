b7e41ab3bfc74569339db39bf97b33040bde8413
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:10:17 +0000

    Add package dujiajie-demo-test-add/1.0/dujiajie_demo_test-add-1.0.tar.gz

---
+++ packages/dujiajie-demo-test-add/dujiajie_demo_test-add-1.0.tar.gz/dujiajie_demo_test-add-1.0/dujiajie_demo_test/_init_.py
@@ -0,0 +1,1 @@
+name = "dujiajie_demo_test"
---
+++ packages/dujiajie-demo-test-add/dujiajie_demo_test-add-1.0.tar.gz/dujiajie_demo_test-add-1.0/setup.py
@@ -0,0 +1,7 @@
+from distutils.core import  setup
+
+
+packages = ['dujiajie_demo_test']
+
+setup(name='dujiajie_demo_test-add',version='1.0',author='djj'
+      , packages=packages, package_dir={'requests': 'requests'},)
