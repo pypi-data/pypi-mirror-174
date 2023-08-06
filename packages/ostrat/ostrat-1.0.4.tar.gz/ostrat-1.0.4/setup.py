f4e5536f36f8397603c11daf03f4d897537e7e4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:01:46 +0000

    Add package ostrat/1.0.4/ostrat-1.0.4.tar.gz

---
+++ packages/ostrat/ostrat-1.0.4.tar.gz/ostrat-1.0.4/setup.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+
+from io import open
+from setuptools import setup
+
+"""
+:authors: lexek
+:license: OSI Approved :: GNU General Public License v3 (GPLv3)
+:copyright: (c) 2022 lexek
+"""
+
+version = '1.0.4'
+
+with open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='ostrat',
+    version=version,
+
+    author='lexek',
+    author_email='minelanpro@gmail.ru',
+
+    description=(
+        u'Open-Source Telegram Remote Administration Tool - Remote PC access via Telegram Bot.'
+    ),
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+
+    url='https://sourceforge.net/projects/ostrat/',
+    download_url='https://sourceforge.net/projects/ostrat/files/ostRAT/',
+
+    license='OSI Approved :: GNU General Public License v3 (GPLv3)',
+
+    packages=['ostrat'],
+    install_requires=['requests', 'opencv', 'aiogram', 'keyboard', 'mouse', 'bs4', 'pillow', 'colorama'],
+
+    classifiers=[
+        
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: Microsoft :: Windows',
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Topic :: System :: Systems Administration',
+        'Intended Audience :: System Administrators',
+        'Intended Audience :: Information Technology',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.10',
+        
+    ]
+)
