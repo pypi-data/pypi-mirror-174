7f0cf1b5366c318033439d0a92ff461c945f78f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:49:21 +0000

    Add package helloworldbasic4message/2.0.13/helloworldbasic4message-2.0.13-py3-none-any.whl

---
+++ packages/helloworldbasic4message/helloworldbasic4message-2.0.13-py3-none-any.whl/helloworldbasic4message/utility1.py
@@ -0,0 +1,25 @@
+import utility2 as u2
+
+
+# description like this
+# abc
+# 123
+
+
+
+
+# description here
+# 100
+# 200
+# 300
+
+def utilA_sayhello(name):
+    return "version Nov 2, 2022 11:58am: hello "+name;
+
+def utilA_sayhello2(name):
+    return u2.utilB_saygoodbye(name)
+
+
+
+print("hello utility ")
+
