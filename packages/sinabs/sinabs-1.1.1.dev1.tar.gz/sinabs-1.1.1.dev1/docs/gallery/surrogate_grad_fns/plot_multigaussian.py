6de19ac23490df4abe5d0329a2e6226719235d15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:28 +0000

    Add package sinabs/1.1.1.dev1/sinabs-1.1.1.dev1.tar.gz

---
+++ packages/sinabs/sinabs-1.1.1.dev1.tar.gz/sinabs-1.1.1.dev1/docs/gallery/surrogate_grad_fns/plot_multigaussian.py
@@ -0,0 +1,17 @@
+"""
+=============
+MultiGaussian
+=============
+:class:`~sinabs.activation.MultiGaussian` surrogate gradient.
+"""
+
+import torch
+import sinabs.activation as sina
+import matplotlib.pyplot as plt
+
+x = torch.linspace(-2, 4, 500)
+plt.plot(x, sina.MultiGaussian()(v_mem=x, spike_threshold=1), label="MultiGaussian")
+plt.xlabel("Neuron membrane potential")
+plt.ylabel("Derivative")
+plt.legend()
+plt.show()
