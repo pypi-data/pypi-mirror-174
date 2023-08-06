2551d880b12beab40210d9fd94d7b5d166771083
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:22:03 +0000

    Add package vern/1.0.2/vern-1.0.2.tar.gz

---
+++ packages/vern/vern-1.0.2.tar.gz/vern-1.0.2/vern/__init__.py
@@ -0,0 +1,10 @@
+from .parse_all import *
+
+__copyright__    = 'Copyright (C) 2022 BoronSpoon'
+__version__      = '1.0.2'
+__license__      = 'MIT License'
+__author__       = 'boronspoon'
+__author_email__ = 'rayanticlimactic@gmail.com'
+__url__          = 'http://github.com/BoronSpoon/vern'
+
+__all__ = ['misc', 'parse_all', 'pase_dxf', 'parse_mat', 'parse_oscilloscope', 'parse_profilometer', 'parse_tabular', 'parse_vsm']
