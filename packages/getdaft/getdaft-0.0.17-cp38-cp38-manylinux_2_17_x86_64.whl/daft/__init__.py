829cf03d1e0a971d1aad6433d90fafcdfd72d4fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:12:56 +0000

    Add package getdaft/0.0.17/getdaft-0.0.17-cp38-cp38-manylinux_2_17_x86_64.whl

---
+++ packages/getdaft/getdaft-0.0.17-cp38-cp38-manylinux_2_17_x86_64.whl/daft/__init__.py
@@ -0,0 +1,12 @@
+from __future__ import annotations
+
+from daft.dataframe import DataFrame
+from daft.expressions import col
+from daft.logging import setup_logger
+from daft.udf import polars_udf, udf
+
+__all__ = ["DataFrame", "col", "udf", "polars_udf"]
+
+setup_logger()
+
+__version__ = "0.0.17"
