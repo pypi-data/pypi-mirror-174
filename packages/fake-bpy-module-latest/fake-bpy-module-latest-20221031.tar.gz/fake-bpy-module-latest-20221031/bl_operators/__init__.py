7bfbca015adff1ff4e4306240eece3d7c6a1d084
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:15:17 +0000

    Add package fake-bpy-module-latest/20221031/fake-bpy-module-latest-20221031.tar.gz

---
+++ packages/fake-bpy-module-latest/fake-bpy-module-latest-20221031.tar.gz/fake-bpy-module-latest-20221031/bl_operators/__init__.py
@@ -0,0 +1,49 @@
+import sys
+import typing
+from . import freestyle
+from . import node
+from . import uvcalc_lightmap
+from . import spreadsheet
+from . import assets
+from . import geometry_nodes
+from . import uvcalc_follow_active
+from . import screen_play_rendered_anim
+from . import mesh
+from . import wm
+from . import add_mesh_torus
+from . import userpref
+from . import console
+from . import image
+from . import object_quick_effects
+from . import bmesh
+from . import uvcalc_transform
+from . import presets
+from . import clip
+from . import rigidbody
+from . import object_randomize_transform
+from . import anim
+from . import object
+from . import view3d
+from . import object_align
+from . import constraint
+from . import sequencer
+from . import file
+from . import vertexpaint_dirt
+
+GenericType = typing.TypeVar("GenericType")
+
+
+def register():
+    ''' 
+
+    '''
+
+    pass
+
+
+def unregister():
+    ''' 
+
+    '''
+
+    pass
---
+++ packages/fake-bpy-module-latest/fake-bpy-module-latest-20221031.tar.gz/fake-bpy-module-latest-20221031/bl_ui/node_add_menu.py
@@ -0,0 +1,20 @@
+import sys
+import typing
+
+GenericType = typing.TypeVar("GenericType")
+
+
+def add_node_type(layout, node_type, label):
+    ''' 
+
+    '''
+
+    pass
+
+
+def draw_node_group_add_menu(context, layout):
+    ''' 
+
+    '''
+
+    pass
