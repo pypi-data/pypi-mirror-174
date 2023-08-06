1064f7885d773c88c2fd2c6d0ffc2d3b91389c49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:05 +0000

    Add package zenpass/1.0.2/zenpass-1.0.2-py3-none-any.whl

---
+++ packages/zenpass/zenpass-1.0.2-py3-none-any.whl/zenpass/__init__.py
@@ -0,0 +1,7 @@
+"""Generate a strong password which includes alphabets, numbers, special characters"""
+
+__author__ = 'srg'
+__version__ = '1.0.2'
+
+from .password import PasswordGenerator
+from .exception import ZenpassException
