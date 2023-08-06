b4c5e0b99faf8415de4e717039fb888b8a94e79b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:41 +0000

    Add package lci-sim-app-device/0.0.20/lci_sim_app_device-0.0.20-py2-none-any.whl

---
+++ packages/lci-sim-app-device/lci_sim_app_device-0.0.20-py2-none-any.whl/lci-sim-app-device/Config.py
@@ -0,0 +1,17 @@
+import os
+__APPLICATION_PATH__ = os.path.dirname(__file__)
+__APPLICATION_DATA__ = __APPLICATION_PATH__ + "/Data"
+
+if not os.path.exists(__APPLICATION_DATA__):
+	os.makedirs(__APPLICATION_DATA__)
+
+print("application data file path", __APPLICATION_DATA__)
+
+
+__APPLICATION_MD5SUM__ = __APPLICATION_PATH__ + '/md5sum.txt'
+
+
+__APPLICATION_DATABASE_PATH__ = __APPLICATION_DATA__ + "/sim.db"
+
+__DEFAULT_CONTROLLER_SOCKET__ = 'http://srv01.letscode.it:9851/'
+
