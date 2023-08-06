d624a656ff5cdbe9fe4f7181dbfcb8e20def69ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:57:06 +0000

    Add package pip-command/0.0.2/pip_command-0.0.2.tar.gz

---
+++ packages/pip-command/pip_command-0.0.2.tar.gz/pip_command-0.0.2/pip_command/pip_insatll.py
@@ -0,0 +1,29 @@
+from os import system as sm
+
+
+def install(lib_name, version=None):
+    sm(f"pip install {lib_name + '==' + version if version else lib_name}")
+
+
+def install_requirements(requirements_file_name):
+    sm(f"pip install -r {requirements_file_name}")
+
+
+def update(lib_name):
+    sm(f"pip install -U {lib_name}")
+
+
+def installs(*libs_name):
+    sm(f"pip install {' '.join(libs_name)}")
+
+
+def uninstall(lib_name):
+    sm(f"pip uninstall -y {lib_name}")
+
+
+def uninstall_requirements(requirements):
+    sm(f"pip uninstall -y {requirements}")
+
+
+def freeze(requirements_file="requirements.txt"):
+    sm(f"pip freeze > {requirements_file}")
