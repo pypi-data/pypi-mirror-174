3a569db3605d25ea0afd656208784e38cef845aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:07 +0000

    Add package gold-python/0.1.2.post1/gold_python-0.1.2.post1.tar.gz

---
+++ packages/gold-python/gold_python-0.1.2.post1.tar.gz/gold_python-0.1.2.post1/gold_python/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""Module init-file.
+
+The __init__.py files are required to make Python treat directories
+containing the file as packages.
+"""
+from gold_python.deterministic import DeterministicAutomata, DeterministicTrasducer
+from gold_python.delta import deltafunc, transducerfunc, pushdownfunc
