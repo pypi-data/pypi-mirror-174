d1576640d18747d3a2301b2b4a8c426eb3273cc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:58:59 +0000

    Add package wds-client/0.1.59/wds-client-0.1.59.tar.gz

---
+++ packages/wds-client/wds-client-0.1.59.tar.gz/wds-client-0.1.59/wds_client/api/__init__.py
@@ -0,0 +1,8 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from wds_client.api.instances_api import InstancesApi
+from wds_client.api.records_api import RecordsApi
+from wds_client.api.schema_api import SchemaApi
