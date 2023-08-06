765faf3542ef26ad8fb9f520dd97e73d570968a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:35 +0000

    Add package znlib/0.1.0/znlib-0.1.0.tar.gz

---
+++ packages/znlib/znlib-0.1.0.tar.gz/znlib-0.1.0/znlib/__init__.py
@@ -0,0 +1,17 @@
+"""The znlib package"""
+import importlib.metadata
+import importlib.util
+
+__version__ = importlib.metadata.version("znlib")
+
+__all__ = []
+
+if importlib.util.find_spec("zntrack") is not None:
+    from znlib import examples  # noqa: F401
+
+    __all__.append("examples")
+
+if importlib.util.find_spec("ase") is not None:
+    from znlib import atomistic  # noqa: F401
+
+    __all__.append("ase")
