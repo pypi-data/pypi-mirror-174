c52961c1c74521b4503ace2463a3c3d7e90c2519
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:53 +0000

    Add package grizzlies/0.1.1/grizzlies-0.1.1.tar.gz

---
+++ packages/grizzlies/grizzlies-0.1.1.tar.gz/grizzlies-0.1.1/setup.py
@@ -0,0 +1,25 @@
+from distutils.core import setup
+setup(
+  name = 'grizzlies',
+  packages = ['grizzlies'],
+  version = '0.1.1',
+  license='MIT',
+  description = 'Esta librería provee la capacidad de leer, convertir y guardar archivos csv a json, y viceversa.',
+  author = 'Sendoa Busquet, Jon Jarrín',
+  author_email = 'sendoa.busquet@alumni.mondragon.edu, jon.jarrin@alumni.mondragon.edu',
+  url = 'https://github.com/JonJarrinVitoria/grizzlies',
+  download_url = 'https://github.com/JonJarrinVitoria/grizzlies/archive/refs/tags/v_01.1.tar.gz',
+  keywords = ['CONVERT', 'JSON', 'CSV', 'NESTED', 'READ', 'SAVE'],
+  install_requires=[
+          'flatten_json',
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+  ],
+)
