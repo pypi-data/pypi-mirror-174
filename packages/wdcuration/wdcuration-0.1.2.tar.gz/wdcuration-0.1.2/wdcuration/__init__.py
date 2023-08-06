312f398702ea91d9bc7e8d7c26a50e37836cd468
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:28:17 +0000

    Add package wdcuration/0.1.2/wdcuration-0.1.2.tar.gz

---
+++ packages/wdcuration/wdcuration-0.1.2.tar.gz/wdcuration-0.1.2/wdcuration/__init__.py
@@ -0,0 +1,16 @@
+"""Top-level package for Wikidata Curation Tools."""
+
+__author__ = """Tiago Lubiana"""
+__email__ = "tiago.lubiana.alves@usp.br"
+__version__ = "0.1.2"
+
+from .wdcuration import (
+    query_wikidata,
+    search_wikidata,
+    add_key,
+    render_qs_url,
+    lookup_id,
+    today_in_quickstatements,
+    get_statement_values,
+    query_wikidata,
+)
