0ddf030fbe1faf791f1811a985821ef33ab8e745
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:08:34 +0000

    Add package civiproxy-logs2json/1.0.8/civiproxy_logs2json-1.0.8.tar.gz

---
+++ packages/civiproxy-logs2json/civiproxy_logs2json-1.0.8.tar.gz/civiproxy_logs2json-1.0.8/src/civiproxy_logs2json/__init__.py
@@ -0,0 +1,12 @@
+from importlib import resources
+try:
+    import tomllib
+except ModuleNotFoundError:
+    import tomli as tomllib
+
+__version__ = "1.0.8"
+
+_cfg = tomllib.loads(resources.read_text("civiproxy_logs2json", "config.toml"))
+REQUEST_LINE_RE = _cfg["regex"]["request_line"]
+VALUES_LINE_RE = _cfg["regex"]["values_line"]
+CLOSING_LINE_RE = _cfg["regex"]["closing_line"]
