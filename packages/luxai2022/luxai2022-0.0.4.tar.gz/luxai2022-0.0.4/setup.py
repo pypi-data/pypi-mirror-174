cef839b1450e96418feddd26414e7e3744d8a184
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:15:14 +0000

    Add package luxai2022/0.0.4/luxai2022-0.0.4.tar.gz

---
+++ packages/luxai2022/luxai2022-0.0.4.tar.gz/luxai2022-0.0.4/setup.py
@@ -0,0 +1,22 @@
+import os
+
+from setuptools import setup, find_packages
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name="luxai2022",
+    version="0.0.4",
+    author="Lux AI Challenge",
+    description="The Lux AI Challenge Season 2",
+    license="MIT",
+    keywords="reinforcement-learning machine-learning ai",
+    url="https://github.com/Lux-AI-Challenge/Lux-Design-2022",
+    packages=find_packages(exclude="kits"),
+    long_description=read("README.md"),
+    entry_points={'console_scripts': [
+        'luxai2022 = luxai_runner.cli:main']},
+)
