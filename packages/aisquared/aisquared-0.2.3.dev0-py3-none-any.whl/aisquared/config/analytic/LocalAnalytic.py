30c3a617c08f0212a3c021a4a189648c43a0f1c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:18:15 +0000

    Add package aisquared/0.2.3.dev0/aisquared-0.2.3.dev0-py3-none-any.whl

---
+++ packages/aisquared/aisquared-0.2.3.dev0-py3-none-any.whl/aisquared/config/analytic/LocalAnalytic.py
@@ -0,0 +1,53 @@
+from aisquared.base import BaseObject
+
+
+class LocalAnalytic(BaseObject):
+    """
+    Interaction with an analytic (lookup table) saved to the
+    local file system
+    """
+
+    def __init__(
+        self,
+        path,
+        input_type,
+    ):
+        """
+        Parameters
+        ----------
+        path : str or path-like or file-like
+            The path to the analytic saved on disk
+        input_type : str
+            The input type to the analytic. Either one of 'cv', 'text', or 'tabular'
+        """
+        super().__init__()
+        self.path = path
+        self.input_type = input_type
+
+    @property
+    def path(self):
+        return self._path
+
+    @path.setter
+    def path(self, value):
+        self._path = value
+
+    @property
+    def input_type(self):
+        return self._input_type
+
+    @input_type.setter
+    def input_type(self, value):
+        self._input_type = value
+
+    def to_dict(self):
+        """
+        Get the configuration object as a dictionary
+        """
+        return {
+            'className': 'LocalAnalytic',
+            'params': {
+                'path': self.path,
+                'inputType': self.input_type
+            }
+        }
