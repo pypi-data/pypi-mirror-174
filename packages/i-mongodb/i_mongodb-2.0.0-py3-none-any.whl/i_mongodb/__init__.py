dc840b1085668370e1b9c798542ff6f96e2c18e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:34:55 +0000

    Add package i-mongodb/2.0.0/i_mongodb-2.0.0-py3-none-any.whl

---
+++ packages/i-mongodb/i_mongodb-2.0.0-py3-none-any.whl/i_mongodb/__init__.py
@@ -0,0 +1,8 @@
+"""A set of functions to retrieve and save data into MongoDB.
+"""
+
+# import package modules
+from i_mongodb.i_mongodb import MongoDBInterface, MongoDBDatabase
+
+
+__version__ = '2.0.0'
