02e2900629d955b9cab9edebdb73eec5c5fbb6b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:12:03 +0000

    Add package data-manipulation/0.35/data_manipulation-0.35.tar.gz

---
+++ packages/data-manipulation/data_manipulation-0.35.tar.gz/data_manipulation-0.35/data_manipulation/__init__.py
@@ -0,0 +1,14 @@
+from data_manipulation import (
+    base,
+    beautifulsoup_,
+    django_,
+    geopandas_,
+    mysql_connector_python_,
+    pandas_,
+    psycopg2_,
+    pyspark_,
+)
+
+from . import _version
+
+__version__ = _version.get_versions()["version"]
