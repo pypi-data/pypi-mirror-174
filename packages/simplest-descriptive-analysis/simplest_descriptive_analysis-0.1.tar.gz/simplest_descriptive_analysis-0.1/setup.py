db1ef4ad356c317b0ffddd23eed8b19ede848755
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:12 +0000

    Add package simplest-descriptive-analysis/0.1/simplest_descriptive_analysis-0.1.tar.gz

---
+++ packages/simplest-descriptive-analysis/simplest_descriptive_analysis-0.1.tar.gz/simplest_descriptive_analysis-0.1/setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+setup(
+  name = 'simplest_descriptive_analysis',         
+  packages = ['simplest_descriptive_analysis'],   
+  version = '0.1',      
+  license='MIT',        
+  description = 'Esta librería permite generar un análisis descriptivo de forma rápida y sencilla',
+  author = 'Nagore Bermeosolo y Ainhoa Paredes',              
+  author_email = 'nagore.bermeosolo@alumni.mondragon.edu',   
+  url = 'https://github.com/nagorebermeosolo/simplest_descriptive_analysis',   
+  download_url = 'https://github.com/nagorebermeosolo/simplest_descriptive_analysis/archive/refs/tags/0.1.tar.gz',   
+  keywords = ['EDA', 'Descriptive Analysis', 'Statistical Analysis'],   
+  install_requires=[    
+          'pandas',
+          'numpy',
+	    'matplotlib',
+	    'seaborn',
+          'math'
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3'
+  ],
+)
