8adb7f0edb655d7bac21b8d5863fd0ec6fba7c19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:56:35 +0000

    Add package pjr-calculadora/0.0.2/pjr_calculadora-0.0.2-py3-none-any.whl

---
+++ packages/pjr-calculadora/pjr_calculadora-0.0.2-py3-none-any.whl/PrjCalculadora/calculos.py
@@ -0,0 +1,20 @@
+# Calculos
+
+
+def somar(n1, n2):
+    return n1 + n2
+
+
+def subtrair(n1, n2):
+    return n1 - n2
+
+
+def multiplicar(n1, n2):
+    return n1 * n2
+
+
+def dividir(n1, n2):
+    if n2 == 0:
+        return 'Não existe divisão por zero'
+    else:
+        return n1 / n2
---
+++ packages/pjr-calculadora/pjr_calculadora-0.0.2-py3-none-any.whl/PrjCalculadora/msg.py
@@ -0,0 +1,18 @@
+#   Mensagens
+
+def cabecalho():
+    print('---------------------------------------')
+    print('----Bem Vindo à calculadora Python-----')
+    print('---------------------------------------')
+
+
+def menuPrincipal():
+    print('1 - Somar'
+        '\n2 - Subtrair'
+        '\n3 - Multiplicar'
+        '\n4 - Dividir'
+        '\n5 - Sair do programa')
+
+
+def Rodape():
+    print('---------------------------------------')
