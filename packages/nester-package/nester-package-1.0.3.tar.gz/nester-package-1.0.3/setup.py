79dd9ba65f942406d991dd5d113cd249abfb5c85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:01:37 +0000

    Add package nester-package/1.0.3/nester-package-1.0.3.tar.gz

---
+++ packages/nester-package/nester-package-1.0.3.tar.gz/nester-package-1.0.3/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+
+setup(
+        name='nester-package',
+        version='1.0.3',
+        py_modules=['nester'],
+        author='SonaM',
+        author_email='sonalimetkar1@gmail.com',
+        description='Printing each items in list (nested or not nested) on new line',
+        long_description='README.md',
+        long_description_content_type='text/markdown',
+        url='',
+        install_requires=[],
+     )
