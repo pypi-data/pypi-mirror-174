90f19d49a6769103982824a1547700aa8ec0f710
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:27:28 +0000

    Add package fecloud/0.1.1/fecloud-0.1.1.tar.gz

---
+++ packages/fecloud/fecloud-0.1.1.tar.gz/fecloud-0.1.1/fecloud/__init__.py
@@ -0,0 +1,13 @@
+# Files
+from . import string
+
+# Folders
+# from . import lookup
+
+
+def printName():
+    print("fecloud")
+
+
+def files():
+    print(1)
---
+++ packages/fecloud/fecloud-0.1.1.tar.gz/fecloud-0.1.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='fecloud',
+      version='0.1.1',
+      description='Backend Cloud Package',
+      packages=['fecloud'],
+      install_requires=[],
+      zip_safe=False,
+      )
