4d26e7367cfb77d4f06ae95555f26b1ce5f55ae2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:20:57 +0000

    Add package zebepy/1.0.1/zebepy-1.0.1.tar.gz

---
+++ packages/zebepy/zebepy-1.0.1.tar.gz/zebepy-1.0.1/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="Zebepy",
+    version="1.0.1",
+    author="vodkarm",
+    author_email="vodkarm06@protonmail.com",
+    description="The first Zebedee python API Wrapper !!",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/vodkarm/zebepy",
+    project_urls={
+        "Bug Tracker": "https://github.com/vodkarm/zebepy/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.9",
+)
