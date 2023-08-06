127f198fd74952aade218fe1f17c008359104ebf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:08:38 +0000

    Add package flask-gordon/0.3.2/flask_gordon-0.3.2-py2.py3-none-any.whl

---
+++ packages/flask-gordon/flask_gordon-0.3.2-py2.py3-none-any.whl/flask_gordon/__init__.py
@@ -0,0 +1,21 @@
+from .__about__ import (  # noqa
+    __author__,
+    __copyright__,
+    __email__,
+    __license__,
+    __summary__,
+    __title__,
+    __uri__,
+    __version__,
+)
+
+__all__ = [
+    "__title__",
+    "__summary__",
+    "__uri__",
+    "__version__",
+    "__author__",
+    "__email__",
+    "__license__",
+    "__copyright__",
+]
