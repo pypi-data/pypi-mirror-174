610eeba9f2888bf82196bb82917fa52173faf74e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:20:48 +0000

    Add package nt3irisadapter/0.0.5/nt3irisadapter-0.0.5.tar.gz

---
+++ packages/nt3irisadapter/nt3irisadapter-0.0.5.tar.gz/nt3irisadapter-0.0.5/src/nt3irisadapter/test.py
@@ -0,0 +1,11 @@
+import iris
+
+
+def test():
+    print("Yo motherfucker")
+    myCompany = iris.cls("WW.ANR")._OpenId("1001||1001")
+    print(myCompany)
+
+
+def test2() -> str:
+    return "Hello world"
