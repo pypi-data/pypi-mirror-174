9c4ed8ca0b958c2e8fe974476b5416e61264cbd6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:01:31 +0000

    Add package tmart/1.2.10/tmart-1.2.10.tar.gz

---
+++ packages/tmart/tmart-1.2.10.tar.gz/tmart-1.2.10/tmart/__init__.py
@@ -0,0 +1,36 @@
+# This file is part of TMart.
+#
+# Copyright 2022 Yulun Wu.
+#
+# TMart is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+
+
+
+
+from .Surface import Surface, SpectralSurface
+from .Atmosphere import Atmosphere
+
+# from .tm import Tmart
+
+from .Tmart import Tmart
+
+
+from .tm_calcref import calc_ref 
+
+from .tm_geometry import dirP_to_coord
+
+
+
+
+
+
+
+
+
+
+
+
