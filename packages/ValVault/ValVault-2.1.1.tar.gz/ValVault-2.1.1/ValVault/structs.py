7644a0e364329a64527c5a689cf03608368a21cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:22:50 +0000

    Add package ValVault/2.1.1/ValVault-2.1.1.tar.gz

---
+++ packages/ValVault/ValVault-2.1.1.tar.gz/ValVault-2.1.1/ValVault/structs.py
@@ -0,0 +1,5 @@
+from dataclasses import dataclass
+
+@dataclass
+class Settings:
+	insecure: bool
---
+++ packages/ValVault/ValVault-2.1.1.tar.gz/ValVault-2.1.1/test/test_db.py
@@ -0,0 +1,20 @@
+from os import getenv
+def add_path():
+	import os.path
+	import sys
+	path = os.path.realpath(os.path.abspath(__file__))
+	sys.path.insert(0, os.path.dirname(os.path.dirname(path)))
+
+add_path()
+
+def test_db():
+	import ValVault
+	from ValVault.storage import json_write, settingsPath
+	json_write({"insecure": True},settingsPath / "config.json")
+	ValVault.init()
+	username = getenv("USERNAME")
+	password = getenv("PASSWORD")
+	ValVault.new_user(username, password)
+	assert username in ValVault.get_users(), "Username not in db"
+	assert ValVault.get_pass(username) == password, "Password not in db"
+	
