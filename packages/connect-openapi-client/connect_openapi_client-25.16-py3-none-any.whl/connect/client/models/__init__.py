fa3f1404ece754c07d7fadca55809bcb37162303
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:17:58 +0000

    Add package connect-openapi-client/25.16/connect_openapi_client-25.16-py3-none-any.whl

---
+++ packages/connect-openapi-client/connect_openapi_client-25.16-py3-none-any.whl/connect/client/models/__init__.py
@@ -0,0 +1,17 @@
+#
+# This file is part of the Ingram Micro CloudBlue Connect Python OpenAPI Client.
+#
+# Copyright (c) 2021 Ingram Micro. All Rights Reserved.
+#
+from connect.client.models.base import (  # noqa
+    Action,
+    AsyncAction,
+    AsyncCollection,
+    AsyncNS,
+    AsyncResource,
+    Collection,
+    NS,
+    Resource,
+)
+from connect.client.models.exceptions import NotYetEvaluatedError  # noqa
+from connect.client.models.resourceset import AsyncResourceSet, ResourceSet  # noqa
