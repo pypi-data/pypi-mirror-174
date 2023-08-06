d0b5a8b6d498602a9a40b2f384628de7c2e6c967
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:19:32 +0000

    Add package requests-toolkit-stable/0.12.1/requests_toolkit_stable-0.12.1-py3-none-any.whl

---
+++ packages/requests-toolkit-stable/requests_toolkit_stable-0.12.1-py3-none-any.whl/api_/__init__.py
@@ -0,0 +1,6 @@
+from api_._weather_query import weather_query as today_weather
+from api_._thesis_info import thesis_info as crawl_thesis_info
+from ._ytb_channel_info import YTBInfoBot
+from ._academic import PaperWithCodeQuery, ArxivQuery, IEEEQuery
+
+
