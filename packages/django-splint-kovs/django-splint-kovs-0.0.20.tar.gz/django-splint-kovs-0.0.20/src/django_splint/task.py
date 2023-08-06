3b68f668bad03ca97e1f0c6d83d9bb12b212283c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:04:16 +0000

    Add package django-splint-kovs/0.0.20/django-splint-kovs-0.0.20.tar.gz

---
+++ packages/django-splint-kovs/django-splint-kovs-0.0.20.tar.gz/django-splint-kovs-0.0.20/src/django_splint/task.py
@@ -0,0 +1,13 @@
+from utils.aws.ecs import AWSECSHandler
+
+
+class SplintTask(AWSECSHandler):
+
+    def delay(self, *args):
+        """Run task in AWS ECS."""
+        return AWSECSHandler().run_task(
+            command=f'./manage.py {" ".join(map(str, args))}')
+
+    def run(self, *args):
+        """Execute task."""
+        raise NotImplementedError('Task not implemented.')
