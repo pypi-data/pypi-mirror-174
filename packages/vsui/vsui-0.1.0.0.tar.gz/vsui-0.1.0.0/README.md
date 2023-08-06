093831ea0569bbb2dcf495e1f96f391edb8bf5e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:22:19 +0000

    Add package vsui/0.1.0.0/vsui-0.1.0.0.tar.gz

---
+++ packages/vsui/vsui-0.1.0.0.tar.gz/vsui-0.1.0.0/README.md
@@ -0,0 +1,1 @@
+# Volume Segmantics User Interface
---
+++ packages/vsui/vsui-0.1.0.0.tar.gz/vsui-0.1.0.0/setup.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+modules = \
+['vsui']
+install_requires = \
+['flask-socketio>=5.3.1,<6.0.0',
+ 'flask>=2.2.2,<3.0.0',
+ 'gevent-websocket>=0.10.1,<0.11.0',
+ 'gevent>=22.10.1,<23.0.0',
+ 'gunicorn>=20.1.0,<21.0.0',
+ 'pyyaml>=6.0,<7.0']
+
+entry_points = \
+{'console_scripts': ['vsui = gunicorn -k '
+                     'geventwebsocket.gunicorn.workers.GeventWebSocketWorker '
+                     '-w 1 vsui']}
+
+setup_kwargs = {
+    'name': 'vsui',
+    'version': '0.1.0.0',
+    'description': 'Flask server and vue frontend for monitoring the progress of a python script.',
+    'long_description': '# Volume Segmantics User Interface',
+    'author': 'Matthew Pimblott',
+    'author_email': 'matthew.pimblott@diamond.ac.uk',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'py_modules': modules,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
