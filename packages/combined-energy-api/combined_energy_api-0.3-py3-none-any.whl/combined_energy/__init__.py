a3071697b1dcba5559b123f119a828980288e27d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:04 +0000

    Add package combined-energy-api/0.3/combined_energy_api-0.3-py3-none-any.whl

---
+++ packages/combined-energy-api/combined_energy_api-0.3-py3-none-any.whl/combined_energy/__init__.py
@@ -0,0 +1,6 @@
+"""
+API Client for Combined Energy API
+"""
+from . import constants
+from . import exceptions
+from .client import CombinedEnergy
---
+++ packages/combined-energy-api/combined_energy_api-0.3-py3-none-any.whl/combined_energy/exceptions.py
@@ -0,0 +1,22 @@
+class CombinedEnergyError(Exception):
+    """
+    Common Error for Combined Energy API
+    """
+
+
+class CombinedEnergyTimeoutError(CombinedEnergyError):
+    """
+    Timeout occurred accessing API
+    """
+
+
+class CombinedEnergyAuthError(CombinedEnergyError):
+    """
+    Error occurred with Authentication
+    """
+
+
+class CombinedEnergyPermissionError(CombinedEnergyError):
+    """
+    Don't have permission to access particular item
+    """
