77ee40aa3c30d39d946507f3efc0fb956f37638b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:19:45 +0000

    Add package chanfig/0.0.34/chanfig-0.0.34-py3-none-any.whl

---
+++ packages/chanfig/chanfig-0.0.34-py3-none-any.whl/chanfig/__init__.py
@@ -0,0 +1,4 @@
+from .config import (Config, ConfigParser, DefaultDict, NestedDict,
+                     OrderedDict, Variable)
+
+__all__ = ["Config", "NestedDict", "OrderedDict", "Variable", "DefaultDict", "ConfigParser"]
