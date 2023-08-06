01b555692678cf59a7dbccda796853a219cfcf6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:18:48 +0000

    Add package polar-api/0.0.8/polar_api-0.0.8.tar.gz

---
+++ packages/polar-api/polar_api-0.0.8.tar.gz/polar_api-0.0.8/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.8'
+DESCRIPTION = 'API wrapper for POLAR TeamPro'
+
+setup(
+    name='polar_api',
+    version=VERSION,
+    description=DESCRIPTION,
+    packages=find_packages(),
+)
