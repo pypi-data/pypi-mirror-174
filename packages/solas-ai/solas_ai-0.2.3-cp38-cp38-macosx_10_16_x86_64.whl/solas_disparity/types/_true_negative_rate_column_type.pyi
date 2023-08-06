5c1cba7c32d2f99f86df562f619d4afb64761180
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:25 +0000

    Add package solas-ai/0.2.3/solas_ai-0.2.3-cp38-cp38-macosx_10_16_x86_64.whl

---
+++ packages/solas-ai/solas_ai-0.2.3-cp38-cp38-macosx_10_16_x86_64.whl/solas_disparity/types/_true_negative_rate_column_type.pyi
@@ -0,0 +1,15 @@
+from ._enum_base import EnumBase as EnumBase
+from _typeshed import Incomplete
+from solas_disparity import const as const
+
+class TrueNegativeRateColumnType(EnumBase):
+    TOTAL: Incomplete
+    TOTAL_LABEL: Incomplete
+    AVERAGE_LABEL: Incomplete
+    TRUE_NEGATIVE: Incomplete
+    FALSE_POSITIVE: Incomplete
+    FALSE_NEGATIVE: Incomplete
+    TRUE_POSITIVE: Incomplete
+    TRUE_NEGATIVE_RATE: Incomplete
+    DIFFERENCE: Incomplete
+    RATIO: Incomplete
