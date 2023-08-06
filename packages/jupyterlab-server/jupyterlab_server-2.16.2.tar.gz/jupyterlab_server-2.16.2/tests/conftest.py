5c41e26a811808662d2ae2fe4d1a679757c89320
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:33 +0000

    Add package jupyterlab-server/2.16.2/jupyterlab_server-2.16.2.tar.gz

---
+++ packages/jupyterlab-server/jupyterlab_server-2.16.2.tar.gz/jupyterlab_server-2.16.2/tests/conftest.py
@@ -0,0 +1,1 @@
+pytest_plugins = ["jupyterlab_server.pytest_plugin"]
---
+++ packages/jupyterlab-server/jupyterlab_server-2.16.2.tar.gz/jupyterlab_server-2.16.2/tests/test_themes_api.py
@@ -0,0 +1,6 @@
+from jupyterlab_server.test_utils import validate_request
+
+
+async def test_get_theme(jp_fetch, labserverapp):
+    r = await jp_fetch("lab", "api", "themes", "@jupyterlab", "foo", "index.css")
+    validate_request(r)
