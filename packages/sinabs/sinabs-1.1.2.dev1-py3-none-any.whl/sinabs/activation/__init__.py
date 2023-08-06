bde059dca710ed521ab59df5071b4af813ade938
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:19:55 +0000

    Add package sinabs/1.1.2.dev1/sinabs-1.1.2.dev1-py3-none-any.whl

---
+++ packages/sinabs/sinabs-1.1.2.dev1-py3-none-any.whl/sinabs/activation/__init__.py
@@ -0,0 +1,10 @@
+from .quantize import Quantize, StochasticRounding
+from .reset_mechanism import MembraneReset, MembraneSubtract
+from .spike_generation import SingleSpike, MultiSpike, MaxSpike
+from .surrogate_gradient_fn import (
+    Heaviside,
+    Gaussian,
+    MultiGaussian,
+    SingleExponential,
+    PeriodicExponential,
+)
