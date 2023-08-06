5681a27cbd6f743bdb9c9a2dad1ac73caacd2ad5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:11:40 +0000

    Add package jij-cimod/1.4.44/jij_cimod-1.4.44-cp37-cp37m-win_amd64.whl

---
+++ packages/jij-cimod/jij_cimod-1.4.44-cp37-cp37m-win_amd64.whl/cimod/__init__.py
@@ -0,0 +1,65 @@
+
+
+""""""# start delvewheel patch
+def _delvewheel_init_patch_1_1_0():
+    import os
+    import sys
+    libs_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, 'jij_cimod.libs'))
+    if sys.version_info[:2] >= (3, 8) and not os.path.exists(os.path.join(sys.base_prefix, 'conda-meta')) or sys.version_info[:2] >= (3, 10):
+        os.add_dll_directory(libs_dir)
+    else:
+        from ctypes import WinDLL
+        with open(os.path.join(libs_dir, '.load-order-jij_cimod-1.4.44')) as file:
+            load_order = file.read().split()
+        for lib in load_order:
+            WinDLL(os.path.join(libs_dir, lib))
+
+
+_delvewheel_init_patch_1_1_0()
+del _delvewheel_init_patch_1_1_0
+# end delvewheel patch
+
+# Copyright 2022 Jij Inc.
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from pkgutil import extend_path
+
+__path__ = extend_path(__path__, __name__)
+
+from cimod import cxxcimod
+
+from cimod.model.binary_polynomial_model import (
+    BinaryPolynomialModel,
+    make_BinaryPolynomialModel,
+    make_BinaryPolynomialModel_from_JSON,
+)
+from cimod.model.binary_quadratic_model import (
+    BinaryQuadraticModel,
+    make_BinaryQuadraticModel,
+    make_BinaryQuadraticModel_from_JSON,
+)
+from cimod.vartype import BINARY, SPIN, Vartype
+
+__all__ = [
+        "cxxcimod",
+        "SPIN",
+        "BINARY",
+        "Vartype",
+        "make_BinaryQuadraticModel",
+        "make_BinaryQuadraticModel_from_JSON",
+        "BinaryQuadraticModel",
+        "make_BinaryPolynomialModel",
+        "make_BinaryPolynomialModel_from_JSON",
+        "BinaryPolynomialModel",
+]
