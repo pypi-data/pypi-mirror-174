86af52dbc46a6b521da14614d1e7d2131819a15b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:31 +0000

    Add package atc-dataplatform/1.1.12/atc_dataplatform-1.1.12-py3-none-any.whl

---
+++ packages/atc-dataplatform/atc_dataplatform-1.1.12-py3-none-any.whl/atc/__init__.py
@@ -0,0 +1,17 @@
+"""
+A common set of python libraries for DataBricks.
+See https://github.com/atc-net/atc-dataplatform for details
+"""
+import importlib_metadata
+
+__version__ = importlib_metadata.version("atc-dataplatform")
+
+from atc import etl, functions, spark, sql  # noqa: F401
+from atc.config_master.config_master import ConfigMaster  # noqa: F401
+
+DEBUG = False
+
+
+def dbg(*args, **kwargs):
+    if DEBUG:
+        print(*args, **kwargs)
