4c2aba2bf3b3fc95898a3863e056fe6b91f4392e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:15:55 +0000

    Add package magic-class/0.6.12/magic-class-0.6.12.tar.gz

---
+++ packages/magic-class/magic-class-0.6.12.tar.gz/magic-class-0.6.12/magicclass/widgets/__init__.py
@@ -0,0 +1,74 @@
+"""
+Advanced widgets for magic class GUI.
+These widgets are all compatible with the ``append`` method of Container widgets.
+"""
+
+from magicgui.widgets import *  # to avoid importing both magicgui.widgets and magicclass.widgets
+
+from .containers import (
+    ButtonContainer,
+    GroupBoxContainer,
+    FrameContainer,
+    ListContainer,
+    SubWindowsContainer,
+    ScrollableContainer,
+    DraggableContainer,
+    CollapsibleContainer,
+    HCollapsibleContainer,
+    SplitterContainer,
+    StackedContainer,
+    TabbedContainer,
+    ToolBoxContainer,
+)
+from .pywidgets import ListWidget, DictWidget
+from .color import ColorEdit, ColorSlider
+from .misc import (
+    OptionalWidget,
+    ConsoleTextEdit,
+    CheckButton,
+    RangeSlider,
+    FloatRangeSlider,
+    HistoryLineEdit,
+    HistoryFileEdit,
+    SpreadSheet,
+)
+from .plot import Figure, SeabornFigure
+from .separator import Separator
+from .sequence import ListEdit, TupleEdit
+from .utils import FreeWidget
+from .logger import Logger
+
+__all__ = [
+    "ButtonContainer",
+    "ColorEdit",
+    "ColorSlider",
+    "ConsoleTextEdit",
+    "CheckButton",
+    "CollapsibleContainer",
+    "DictWidget",
+    "DraggableContainer",
+    "FrameContainer",
+    "Figure",
+    "FloatRangeSlider",
+    "FreeWidget",
+    "GroupBoxContainer",
+    "HCollapsibleContainer",
+    "HistoryFileEdit",
+    "HistoryLineEdit",
+    "ListEdit",
+    "ListContainer",
+    "ListWidget",
+    "Logger",
+    "OptionalWidget",
+    "RangeSlider",
+    "SeabornFigure",
+    "Separator",
+    "ScrollableContainer",
+    "SubWindowsContainer",
+    "SplitterContainer",
+    "SpreadSheet",
+    "StackedContainer",
+    "TabbedContainer",
+    "ToolBoxContainer",
+    "TupleEdit",
+]
