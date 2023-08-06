cb196f1ffbb488e98cafb0539ffe0728b83571b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:31 +0000

    Add package nextflowpy/0.4.0/nextflowpy-0.4.0-py3-none-any.whl

---
+++ packages/nextflowpy/nextflowpy-0.4.0-py3-none-any.whl/nextflow/__init__.py
@@ -0,0 +1,12 @@
+from shutil import which
+from .exceptions import NextflowNotInstalledError
+from .execution import Execution
+from .pipeline import Pipeline, run, run_and_poll
+
+__author__ = "Sam Ireland"
+__version__ = "0.4.0"
+
+if not which("nextflow"):
+    raise NextflowNotInstalledError(
+        "Nextflow is either not installed, not in PATH, or is not executable."
+    )
