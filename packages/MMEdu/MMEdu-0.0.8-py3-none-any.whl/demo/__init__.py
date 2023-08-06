a21b340dbf56c737f1bdc61916fbdfb4e79d3775
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:48 +0000

    Add package MMEdu/0.0.8/MMEdu-0.0.8-py3-none-any.whl

---
+++ packages/MMEdu/MMEdu-0.0.8-py3-none-any.whl/demo/__init__.py
@@ -0,0 +1,7 @@
+from .base import *
+from .cls_demo import only_infer_demo, normal_train_demo, continue_train_demo
+
+
+__all__ = [
+    'base','only_infer_demo','normal_train_demo', 'continue_train_demo'
+]
