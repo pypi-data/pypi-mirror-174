14d1dbeb31600da3fe70309720036b8a0d47176f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:04:09 +0000

    Add package streamlit-talk/0.0.3/streamlit-talk-0.0.3.tar.gz

---
+++ packages/streamlit-talk/streamlit-talk-0.0.3.tar.gz/streamlit-talk-0.0.3/streamlit_talk/frontend/build/precache-manifest.714a40aaaf9000827ffe8deb5eed4072.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "400d436ef662e136eb0738040bf766de",
+    "url": "./index.html"
+  },
+  {
+    "revision": "79473bbe60323f52bdd3",
+    "url": "./static/js/2.9c8d198f.chunk.js"
+  },
+  {
+    "revision": "3fc7fb5bfeeec1534560a2c962e360a7",
+    "url": "./static/js/2.9c8d198f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33a6052303c8ed3578d0",
+    "url": "./static/js/main.dde51873.chunk.js"
+  },
+  {
+    "revision": "7c26bca7e16783d14d15",
+    "url": "./static/js/runtime-main.11ec9aca.js"
+  }
+]);
