d1fec39c48b8e3eb234bd38b86e4ee939c967f2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:16 +0000

    Add package kit-py/0.5.3/kit_py-0.5.3.tar.gz

---
+++ packages/kit-py/kit_py-0.5.3.tar.gz/kit_py-0.5.3/kit/rpc/events.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+class Events:
+
+    def __init__(self):
+        self._events = {}
+
+    def on(self, event, fn):
+        self._events.setdefault(event, []).append(fn)
+
+    def emit(self, event, **kwargs):
+        if not event.startswith('on_'):
+            event = f'on_{event}'
+        for fn in self._events.get(event, []):
+            fn(**kwargs)
+
+    def remove(self, event, fn):
+        self._events.get(event, []).remove(fn)
+
+    def all(self):
+        return self._events
+
+events = Events()
