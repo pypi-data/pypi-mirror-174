367506538a76a2161c5d3181d208e783461a69bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:11:17 +0000

    Add package mypy-boto3-mgn/1.26.0/mypy-boto3-mgn-1.26.0.tar.gz

---
+++ packages/mypy-boto3-mgn/mypy-boto3-mgn-1.26.0.tar.gz/mypy-boto3-mgn-1.26.0/mypy_boto3_mgn/__init__.pyi
@@ -0,0 +1,51 @@
+"""
+Main interface for mgn service.
+
+Usage::
+
+    ```python
+    from boto3.session import Session
+    from mypy_boto3_mgn import (
+        Client,
+        DescribeJobLogItemsPaginator,
+        DescribeJobsPaginator,
+        DescribeLaunchConfigurationTemplatesPaginator,
+        DescribeReplicationConfigurationTemplatesPaginator,
+        DescribeSourceServersPaginator,
+        DescribeVcenterClientsPaginator,
+        mgnClient,
+    )
+
+    session = Session()
+    client: mgnClient = session.client("mgn")
+
+    describe_job_log_items_paginator: DescribeJobLogItemsPaginator = client.get_paginator("describe_job_log_items")
+    describe_jobs_paginator: DescribeJobsPaginator = client.get_paginator("describe_jobs")
+    describe_launch_configuration_templates_paginator: DescribeLaunchConfigurationTemplatesPaginator = client.get_paginator("describe_launch_configuration_templates")
+    describe_replication_configuration_templates_paginator: DescribeReplicationConfigurationTemplatesPaginator = client.get_paginator("describe_replication_configuration_templates")
+    describe_source_servers_paginator: DescribeSourceServersPaginator = client.get_paginator("describe_source_servers")
+    describe_vcenter_clients_paginator: DescribeVcenterClientsPaginator = client.get_paginator("describe_vcenter_clients")
+    ```
+"""
+from .client import mgnClient
+from .paginator import (
+    DescribeJobLogItemsPaginator,
+    DescribeJobsPaginator,
+    DescribeLaunchConfigurationTemplatesPaginator,
+    DescribeReplicationConfigurationTemplatesPaginator,
+    DescribeSourceServersPaginator,
+    DescribeVcenterClientsPaginator,
+)
+
+Client = mgnClient
+
+__all__ = (
+    "Client",
+    "DescribeJobLogItemsPaginator",
+    "DescribeJobsPaginator",
+    "DescribeLaunchConfigurationTemplatesPaginator",
+    "DescribeReplicationConfigurationTemplatesPaginator",
+    "DescribeSourceServersPaginator",
+    "DescribeVcenterClientsPaginator",
+    "mgnClient",
+)
