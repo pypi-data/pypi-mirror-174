3e3cba63b6b533e3a3a544305a72b0552087b5c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:19:25 +0000

    Add package numintegration/2.0/numintegration-2.0-py3-none-any.whl

---
+++ packages/numintegration/numintegration-2.0-py3-none-any.whl/rmgintegration/__init__.py
@@ -0,0 +1,3 @@
+__all__ = ["integration"]
+
+from .integration import midpoint_rule, plot_midpoint_rule, trapezoidal_rule, plot_trapezoidal_rule, simpson_rule, plot_simpson_rule, gauss_rule, plot_gauss_quadrature
