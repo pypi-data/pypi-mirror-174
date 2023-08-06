df88c256eea7d0ebb82c830d597964e65885ae2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:15:55 +0000

    Add package gstools/1.4.1/gstools-1.4.1-cp311-cp311-win_amd64.whl

---
+++ packages/gstools/gstools-1.4.1-cp311-cp311-win_amd64.whl/gstools/covmodel/__init__.py
@@ -0,0 +1,98 @@
+# -*- coding: utf-8 -*-
+"""
+GStools subpackage providing a set of handy covariance models.
+
+.. currentmodule:: gstools.covmodel
+
+Subpackages
+^^^^^^^^^^^
+
+.. autosummary::
+   :toctree: generated
+
+   plot
+
+Covariance Base-Class
+^^^^^^^^^^^^^^^^^^^^^
+Class to construct user defined covariance models
+
+.. autosummary::
+   :toctree: generated
+
+   CovModel
+
+Covariance Models
+^^^^^^^^^^^^^^^^^
+Standard Covariance Models
+
+.. autosummary::
+   :toctree: generated
+
+   Gaussian
+   Exponential
+   Matern
+   Integral
+   Stable
+   Rational
+   Cubic
+   Linear
+   Circular
+   Spherical
+   HyperSpherical
+   SuperSpherical
+   JBessel
+
+Truncated Power Law Covariance Models
+
+.. autosummary::
+   :toctree: generated
+
+   TPLGaussian
+   TPLExponential
+   TPLStable
+   TPLSimple
+"""
+
+from gstools.covmodel.base import CovModel
+from gstools.covmodel.models import (
+    Circular,
+    Cubic,
+    Exponential,
+    Gaussian,
+    HyperSpherical,
+    Integral,
+    JBessel,
+    Linear,
+    Matern,
+    Rational,
+    Spherical,
+    Stable,
+    SuperSpherical,
+)
+from gstools.covmodel.tpl_models import (
+    TPLExponential,
+    TPLGaussian,
+    TPLSimple,
+    TPLStable,
+)
+
+__all__ = [
+    "CovModel",
+    "Gaussian",
+    "Exponential",
+    "Matern",
+    "Integral",
+    "Stable",
+    "Rational",
+    "Cubic",
+    "Linear",
+    "Circular",
+    "Spherical",
+    "HyperSpherical",
+    "SuperSpherical",
+    "JBessel",
+    "TPLGaussian",
+    "TPLExponential",
+    "TPLStable",
+    "TPLSimple",
+]
