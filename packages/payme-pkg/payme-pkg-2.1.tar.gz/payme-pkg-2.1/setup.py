ceb9a41573149cb348b397e806eb624c54c11ca4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:02 +0000

    Add package payme-pkg/2.1/payme-pkg-2.1.tar.gz

---
+++ packages/payme-pkg/payme-pkg-2.1.tar.gz/payme-pkg-2.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='payme-pkg',
+    version='2.1',
+    license='MIT',
+    author="Muhammadali Akbarov",
+    author_email='muhammadali17abc@gmail.com',
+    packages=find_packages('lib'),
+    package_dir={'': 'lib'},
+    url='https://github.com/Muhammadali-Akbarov/payme-pkg',
+    keywords='paymeuz paycomuz payme-merchant merchant-api subscribe-api payme-pkg payme-api',
+    install_requires=[
+        'requests==2.28.1',
+        'djangorestframework==3.14.0'
+      ],
+)
