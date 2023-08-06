9db8f42a58c91eae7ddcfe963966b1e4afc1edf6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:50 +0000

    Add package cdisc-rules-engine/0.4.9.2/cdisc_rules_engine-0.4.9.2-py3-none-any.whl

---
+++ packages/cdisc-rules-engine/cdisc_rules_engine-0.4.9.2-py3-none-any.whl/cdisc_rules_engine/models/dictionaries/whodrug/base_whodrug_term.py
@@ -0,0 +1,20 @@
+from abc import abstractmethod
+
+
+class BaseWhoDrugTerm:
+    """
+    This class contains some common implementation
+    between all WhoDrug terms.
+    """
+
+    def __init__(self, record_params: dict):
+        self.type: str = record_params["type"]
+        self.code: str = record_params["code"]
+
+    @classmethod
+    @abstractmethod
+    def from_txt_line(cls, line: str) -> "BaseWhoDrugTerm":
+        """
+        Creates an instance from the given line.
+        Does not save it to the DB.
+        """
