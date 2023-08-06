5952e9059b9e4816a467dcd4067520c52d788d6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:39 +0000

    Add package pymmcore-plus/0.5.1/pymmcore_plus-0.5.1.tar.gz

---
+++ packages/pymmcore-plus/pymmcore_plus-0.5.1.tar.gz/pymmcore_plus-0.5.1/examples/run_mda.py
@@ -0,0 +1,22 @@
+from useq import MDASequence
+
+from pymmcore_plus import CMMCorePlus
+
+# see https://github.com/pymmcore-plus/useq-schema
+sequence = MDASequence(
+    channels=["DAPI", {"config": "FITC", "exposure": 50}],
+    time_plan={"interval": 2, "loops": 5},
+    z_plan={"range": 4, "step": 0.5},
+    axis_order="tpcz",
+)
+
+mmc = CMMCorePlus.instance()
+mmc.loadSystemConfiguration()
+
+
+@mmc.mda.events.frameReady.connect
+def new_frame(img, event):
+    print(img.shape)
+
+
+mda_thread = mmc.run_mda(sequence)
