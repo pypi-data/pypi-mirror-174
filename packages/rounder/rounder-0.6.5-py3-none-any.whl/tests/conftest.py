3f4dc0a873eda5830617ad68eebd070e652ae153
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:01 +0000

    Add package rounder/0.6.5/rounder-0.6.5-py3-none-any.whl

---
+++ packages/rounder/rounder-0.6.5-py3-none-any.whl/tests/conftest.py
@@ -0,0 +1,54 @@
+import pytest
+import fractions
+import decimal
+
+from copy import deepcopy
+
+
+@pytest.fixture(scope="session")
+def n():
+    return 10
+
+
+@pytest.fixture(scope="session")
+def list_len():
+    return 10, 20, 100, 1000
+
+
+@pytest.fixture(scope="session")
+def limits():
+    return -500, 500
+
+
+@pytest.fixture(scope="session")
+def digits_range():
+    return range(10)
+
+
+@pytest.fixture()
+def complex_object():
+    obj = {
+        "a": 12.22221111,
+        "string": "something nice, ha?",
+        "callable": lambda x: x ** 2,
+        "b": 2,
+        "c": 1.222,
+        "d": [1.12343, 0.023492],
+        "e": {
+            "ea": 1 / 44,
+            "eb": {1.333, 2.999},
+            "ec": dict(eca=1.565656, ecb=1.765765765),
+        },
+    }
+    return deepcopy(obj)
+
+@pytest.fixture()
+def object_with_various_numbers():
+    obj = {
+        "complex number": 1.2-2j,
+        "string": "something nice, ha?",
+        "callable": lambda x: x ** 2,
+        "decimal": decimal.Decimal("1.2"),
+        "fraction": fractions.Fraction(1, 4),
+    }
+    return deepcopy(obj)
