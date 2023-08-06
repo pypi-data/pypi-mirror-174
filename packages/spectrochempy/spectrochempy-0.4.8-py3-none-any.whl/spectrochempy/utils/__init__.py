3b7a2af15fdfb89b5de8435d05934bb48d0e60c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:56:38 +0000

    Add package spectrochempy/0.4.8/spectrochempy-0.4.8-py3-none-any.whl

---
+++ packages/spectrochempy/spectrochempy-0.4.8-py3-none-any.whl/spectrochempy/utils/__init__.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+# flake8: noqa
+
+# ======================================================================================================================
+#  Copyright (©) 2015-2022 LCS - Laboratoire Catalyse et Spectrochimie, Caen, France.
+#  CeCILL-B FREE SOFTWARE LICENSE AGREEMENT - See full LICENSE agreement in the root directory.
+# ======================================================================================================================
+"""
+Package containing various utilities classes and functions.
+"""
+# some useful constants
+# ------------------------------------------------------------------
+# import numpy as np
+
+# masked arrays
+# ------------------------------------------------------------------
+from numpy.ma.core import (
+    masked as MASKED,
+    nomask as NOMASK,
+    MaskedArray,
+    MaskedConstant,
+)  # noqa: F401
+
+# import util files content
+# ------------------------------------------------------------------
+
+from .fake import *
+from .print import *
+from .file import *
+from .jsonutils import *
+from .misc import *
+from .packages import *
+from .plots import *
+from .system import *
+from .traits import *
+from .zip import *
+from .exceptions import *
+from .version import *
+from .print_versions import *
