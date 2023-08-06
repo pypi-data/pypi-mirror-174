d10f9ab28524d95dc494c659f21ba81ecab2461b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:19:41 +0000

    Add package random-paaakg/0.0/random_paaakg-0.0.tar.gz

---
+++ packages/random-paaakg/random_paaakg-0.0.tar.gz/random_paaakg-0.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='random_paaakg',
+      version='0.0',
+      description='deskrrr',
+      packages=['random_paaakg'],
+      author_email='sonterkub@gmail.com',
+      zip_safe=False)
