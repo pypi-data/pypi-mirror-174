a1a6ff1f56d303f0986f4019b721612fc44d7baa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:01:42 +0000

    Add package pymegafon/1.1.1/pymegafon-1.1.1-py3-none-any.whl

---
+++ packages/pymegafon/pymegafon-1.1.1-py3-none-any.whl/pymegafon/common.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+    Sets common variables and such.
+"""
+
+LK_START_URL = 'https://old.lk.megafon.ru/login/'
+LK_LOGIN_URL = 'https://old.lk.megafon.ru/dologin/'
+API_GET_BALANCE_URL = 'https://old.lk.megafon.ru/api/lk/balance/get'
+API_GET_INTERNET_REMININGS_URL = 'https://old.lk.megafon.ru/api/options/remaindersMini'
