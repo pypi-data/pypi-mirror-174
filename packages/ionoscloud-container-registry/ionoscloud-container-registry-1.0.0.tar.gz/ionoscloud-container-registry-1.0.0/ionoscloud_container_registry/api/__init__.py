69c50d002590ea8fc67ec62549baf4e27188d96b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:01 +0000

    Add package ionoscloud-container-registry/1.0.0/ionoscloud-container-registry-1.0.0.tar.gz

---
+++ packages/ionoscloud-container-registry/ionoscloud-container-registry-1.0.0.tar.gz/ionoscloud-container-registry-1.0.0/ionoscloud_container_registry/api/__init__.py
@@ -0,0 +1,10 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from ionoscloud_container_registry.api.locations_api import LocationsApi
+from ionoscloud_container_registry.api.names_api import NamesApi
+from ionoscloud_container_registry.api.registries_api import RegistriesApi
+from ionoscloud_container_registry.api.repositories_api import RepositoriesApi
+from ionoscloud_container_registry.api.tokens_api import TokensApi
