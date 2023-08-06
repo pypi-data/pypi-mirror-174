1ec6877384f9afa532b75597e7bb612dd3e0aeff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:19:35 +0000

    Add package daktari/0.0.70/daktari-0.0.70.tar.gz

---
+++ packages/daktari/daktari-0.0.70.tar.gz/daktari-0.0.70/daktari/__main__.py
@@ -0,0 +1,35 @@
+import logging
+import os
+import sys
+
+from pyfiglet import Figlet
+
+from daktari.check_runner import run_checks
+from daktari.config import read_config
+from daktari.options import argument_parser
+
+
+def print_logo(title: str):
+    figlet = Figlet(font="slant")
+    print(figlet.renderText(title))
+
+
+def main() -> int:
+    args = argument_parser.parse_args()
+    if args.debug:
+        logging.basicConfig(level=logging.DEBUG)
+    config = read_config(args.config_path)
+    if config is None:
+        return 1
+
+    os.chdir(args.config_path.parent.absolute())
+
+    if config.title:
+        print_logo(config.title)
+    all_passed = run_checks(config.checks)
+    print("")
+    return 0 if all_passed else 1
+
+
+if __name__ == "__main__":
+    sys.exit(main())
