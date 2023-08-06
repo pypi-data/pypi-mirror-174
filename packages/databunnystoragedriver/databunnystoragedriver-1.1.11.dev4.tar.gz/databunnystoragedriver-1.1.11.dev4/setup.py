aa9344b5b789634c6e67426fe7e8da745dad5104
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:27:20 +0000

    Add package databunnystoragedriver/1.1.11.dev4/databunnystoragedriver-1.1.11.dev4.tar.gz

---
+++ packages/databunnystoragedriver/databunnystoragedriver-1.1.11.dev4.tar.gz/databunnystoragedriver-1.1.11.dev4/setup.py
@@ -0,0 +1,23 @@
+# Copyright (c) Databunny Pte. Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import setuptools
+import sys, os
+src_path = os.path.join(os.path.realpath(os.path.dirname(__file__)), 'src')
+sys.path.append(src_path)
+setuptools.setup(
+    packages=["src/driver"],
+    setup_requires=['pbr>=2.0.0'],
+    pbr=True)
