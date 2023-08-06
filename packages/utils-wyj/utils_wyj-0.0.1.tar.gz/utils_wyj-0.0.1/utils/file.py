449c2628aafb75c7f89b817e747144f7c38fee79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:33 +0000

    Add package utils-wyj/0.0.1/utils_wyj-0.0.1.tar.gz

---
+++ packages/utils-wyj/utils_wyj-0.0.1.tar.gz/utils_wyj-0.0.1/utils/file.py
@@ -0,0 +1,12 @@
+from typing import Union
+import os
+def get_onprem_file_md5(onprem_file_name: str, format='hex') -> Union[str, bytearray]:
+    pass
+
+
+def get_onprem_file_sha256(onprem_file_name):
+    pass
+
+
+def findfile(path, target_file_name):
+    pass
