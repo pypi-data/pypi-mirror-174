fd9c9d8dd1b9abc55bf010a2400bf8a1cb823f2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:17:29 +0000

    Add package wabisabi/0.3.0/wabisabi-0.3.0-py3-none-any.whl

---
+++ packages/wabisabi/wabisabi-0.3.0-py3-none-any.whl/wabisabi/__init__.py
@@ -0,0 +1,6 @@
+__author__ = """Mark Harfouche"""
+__email__ = 'mark.harfouche@gmail.com'
+from .default_parameter_change import default_parameter_change  # noqa
+from .kwonly_change import kwonly_change                        # noqa
+from .kwarg_name_change import kwarg_name_change                # noqa
+from ._version import __version__                               # noqa
