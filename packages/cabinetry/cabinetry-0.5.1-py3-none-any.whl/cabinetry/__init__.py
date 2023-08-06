5d5f94d19c7fc120a7f6e77cff0e3f17d3bf6a17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:15:24 +0000

    Add package cabinetry/0.5.1/cabinetry-0.5.1-py3-none-any.whl

---
+++ packages/cabinetry/cabinetry-0.5.1-py3-none-any.whl/cabinetry/__init__.py
@@ -0,0 +1,26 @@
+"""The cabinetry library."""
+
+import logging
+
+import cabinetry.configuration  # noqa: F401
+import cabinetry.fit  # noqa: F401
+import cabinetry.model_utils  # noqa: F401
+import cabinetry.route  # noqa: F401
+import cabinetry.smooth  # noqa: F401
+import cabinetry.tabulate  # noqa: F401
+import cabinetry.templates  # noqa: F401
+import cabinetry.visualize  # noqa: F401
+import cabinetry.workspace  # noqa: F401
+
+
+__version__ = "0.5.1"
+
+
+def set_logging() -> None:
+    """Sets up customized and verbose logging output.
+
+    Logging can be alternatively customized with the Python ``logging`` module directly.
+    """
+    logging.basicConfig(format="%(levelname)s - %(name)s - %(message)s")
+    logging.getLogger("cabinetry").setLevel(logging.DEBUG)
+    logging.getLogger("pyhf").setLevel(logging.INFO)
