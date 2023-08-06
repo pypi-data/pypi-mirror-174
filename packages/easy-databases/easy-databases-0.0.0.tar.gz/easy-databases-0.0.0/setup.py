4098226a172277be49bff0047728b338a8df4a7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:50 +0000

    Add package easy-databases/0.0.0/easy-databases-0.0.0.tar.gz

---
+++ packages/easy-databases/easy-databases-0.0.0.tar.gz/easy-databases-0.0.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(
+    description=
+        "Easy-to-use pure Python databases that match basic data "
+        "structures, including lists, sets, and dictionaries. No fancy "
+        "querying language required.",
+)
