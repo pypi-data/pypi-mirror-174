3b0ae9769db028acb350f202c283c47212a57f7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:18:33 +0000

    Add package nebula3-python/3.3.0/nebula3-python-3.3.0.tar.gz

---
+++ packages/nebula3-python/nebula3-python-3.3.0.tar.gz/nebula3-python-3.3.0/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+# Copyright (c) 2020 vesoft inc. All rights reserved.
+#
+# This source code is licensed under Apache 2.0 License.
+
+
+from setuptools import setup, find_packages
+from pathlib import Path
+
+base_dir = Path(__file__).parent
+long_description = (base_dir / 'README.md').read_text()
+
+
+setup(
+    name='nebula3-python',
+    version='3.3.0',
+    license='Apache 2.0',
+    author='vesoft-inc',
+    author_email='info@vesoft.com',
+    description='Python client for NebulaGraph v3.3',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/vesoft-inc/nebula-python',
+    install_requires=['httplib2', 'future', 'six', 'pytz'],
+    packages=find_packages(),
+    platforms=['3.6, 3.7'],
+    package_dir={'nebula3': 'nebula3'},
+)
