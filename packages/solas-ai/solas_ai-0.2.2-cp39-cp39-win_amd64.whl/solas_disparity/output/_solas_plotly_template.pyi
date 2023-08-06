46a3e8c2d859bf2a2ad4af4b87e8668351802e1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:23 +0000

    Add package solas-ai/0.2.2/solas_ai-0.2.2-cp39-cp39-win_amd64.whl

---
+++ packages/solas-ai/solas_ai-0.2.2-cp39-cp39-win_amd64.whl/solas_disparity/output/_solas_plotly_template.pyi
@@ -0,0 +1,3 @@
+from _typeshed import Incomplete
+
+SOLAS_PLOTLY_TEMPLATE: Incomplete
---
+++ packages/solas-ai/solas_ai-0.2.2-cp39-cp39-win_amd64.whl/solas_disparity/types/_positive_predictive_value_column_type.pyi
@@ -0,0 +1,15 @@
+from ._enum_base import EnumBase as EnumBase
+from _typeshed import Incomplete
+from solas_disparity import const as const
+
+class PositivePredictiveValueColumnType(EnumBase):
+    TOTAL: Incomplete
+    TOTAL_LABEL: Incomplete
+    AVERAGE_LABEL: Incomplete
+    TRUE_NEGATIVE: Incomplete
+    FALSE_POSITIVE: Incomplete
+    FALSE_NEGATIVE: Incomplete
+    TRUE_POSITIVE: Incomplete
+    POSITIVE_PREDICTIVE_VALUE: Incomplete
+    DIFFERENCE: Incomplete
+    RATIO: Incomplete
