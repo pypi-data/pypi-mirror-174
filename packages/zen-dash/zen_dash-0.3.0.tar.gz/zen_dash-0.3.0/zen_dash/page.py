301eb7bc445764e75c3d1536c2eb41639b2b705e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:10:17 +0000

    Add package zen-dash/0.3.0/zen_dash-0.3.0.tar.gz

---
+++ packages/zen-dash/zen_dash-0.3.0.tar.gz/zen_dash-0.3.0/zen_dash/page.py
@@ -0,0 +1,18 @@
+from enum import Enum
+import optparse
+from typing import List, Optional
+
+from zen_dash.support import BaseUpdate
+
+
+class Instance(BaseUpdate):
+    url: str
+    
+
+class Row(BaseUpdate):
+    data: List[Instance]
+    layoutGap: Optional[str] = "30px"
+
+class Page(BaseUpdate):
+    rows: List[Row]
+    first_page_fragment: str
