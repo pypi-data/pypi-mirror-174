94f74e36f88a5cbc293f8059ac649099b93f163b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:17:20 +0000

    Add package ansys-fluent-core/0.12.dev3/ansys_fluent_core-0.12.dev3-py3-none-any.whl

---
+++ packages/ansys-fluent-core/ansys_fluent_core-0.12.dev3-py3-none-any.whl/ansys/fluent/core/solver/settings_231/__init__.py
@@ -0,0 +1,9 @@
+#
+# This is an auto-generated file.  DO NOT EDIT!
+#
+
+"""A package providing Fluent's Settings Objects in Python."""
+from ansys.fluent.core.solver.flobject import *
+
+SHASH = "c7fb09e4363df79ed7a100d2471b36b934fc6546110d389a85ac48d7f7750566"
+from .root import root
---
+++ packages/ansys-fluent-core/ansys_fluent_core-0.12.dev3-py3-none-any.whl/ansys/fluent/core/solver/settings_231/mesh_info.py
@@ -0,0 +1,19 @@
+#
+# This is an auto-generated file.  DO NOT EDIT!
+#
+
+from ansys.fluent.core.solver.flobject import *
+
+from ansys.fluent.core.solver.flobject import _ChildNamedObjectAccessorMixin
+
+from ansys.fluent.core.solver.flobject import _CreatableNamedObjectMixin
+
+from ansys.fluent.core.solver.flobject import _NonCreatableNamedObjectMixin
+
+class mesh_info(Integer):
+    """
+    Print zone information size.
+    """
+
+    fluent_name = "mesh-info"
+
