e4035260ca3bacdc4c5a5bff32493660cf526c5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:28:59 +0000

    Add package cryopy/0.0.33/cryopy-0.0.33.tar.gz

---
+++ packages/cryopy/cryopy-0.0.33.tar.gz/cryopy-0.0.33/src/cryopy/Helium/test.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov  1 16:21:43 2022
+
+@author: valentinsauvage
+"""
+
+import Helium3
+import Helium4
+import Helium7
+
+C_h3 = Helium3.molar_specific_heat(0.1,0)
+C_h4 = Helium4.molar_specific_heat(0.1,0)
+C_h7 = Helium7.molar_specific_heat(0.1,0,0.066)
+
