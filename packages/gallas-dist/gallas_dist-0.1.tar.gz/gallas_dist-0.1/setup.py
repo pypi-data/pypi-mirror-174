4316ce5635c5918ba9809b8ace5b46fa06dc8b9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:13:26 +0000

    Add package gallas-dist/0.1/gallas_dist-0.1.tar.gz

---
+++ packages/gallas-dist/gallas_dist-0.1.tar.gz/gallas_dist-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='gallas_dist',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['gallas_dist'],
+      author= 'Gallas',
+      author_email= 'fniang89@gmail.com',
+      zip_safe=False)
