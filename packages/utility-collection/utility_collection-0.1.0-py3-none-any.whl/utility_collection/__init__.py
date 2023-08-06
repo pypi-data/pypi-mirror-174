c4def5cc3178516aaef71b87826b1311d8b7bce3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:27 +0000

    Add package utility-collection/0.1.0/utility_collection-0.1.0-py3-none-any.whl

---
+++ packages/utility-collection/utility_collection-0.1.0-py3-none-any.whl/utility_collection/__init__.py
@@ -0,0 +1,6 @@
+from .download_file_samples.__main__ import main as download_file_samples
+
+
+class UtilityCollection:
+    def download_file_samples(self) -> None:
+        download_file_samples()
