cb7f584868690c4f7c1b36157f624f3fb0cc14e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:14:38 +0000

    Add package mbpy/0.0.3/mbpy-0.0.3.tar.gz

---
+++ packages/mbpy/mbpy-0.0.3.tar.gz/mbpy-0.0.3/setup.py
@@ -0,0 +1,14 @@
+from setuptools import find_packages, setup
+setup(
+    name='mbpy',
+    packages=find_packages(),
+    version='0.0.3',
+    description='microBees Python Library',
+    author='microbeestech',
+    license='MIT',
+    install_requires=[],
+    setup_requires=[''],
+    python_requires='>=3.6',                # Minimum version requirement of the package
+    py_modules=["mbpy"],             # Name of the python package
+
+)
