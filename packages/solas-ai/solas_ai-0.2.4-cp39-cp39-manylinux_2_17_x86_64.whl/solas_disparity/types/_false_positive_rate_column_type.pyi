9c8397849c14d879ec1f796ee4bd6312d88e8d36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:58:36 +0000

    Add package solas-ai/0.2.4/solas_ai-0.2.4-cp39-cp39-manylinux_2_17_x86_64.whl

---
+++ packages/solas-ai/solas_ai-0.2.4-cp39-cp39-manylinux_2_17_x86_64.whl/solas_disparity/types/_false_positive_rate_column_type.pyi
@@ -0,0 +1,15 @@
+from ._enum_base import EnumBase as EnumBase
+from _typeshed import Incomplete
+from solas_disparity import const as const
+
+class FalsePositiveRateColumnType(EnumBase):
+    TOTAL: Incomplete
+    TOTAL_LABEL: Incomplete
+    AVERAGE_LABEL: Incomplete
+    TRUE_NEGATIVE: Incomplete
+    FALSE_POSITIVE: Incomplete
+    FALSE_NEGATIVE: Incomplete
+    TRUE_POSITIVE: Incomplete
+    FALSE_POSITIVE_RATE: Incomplete
+    DIFFERENCE: Incomplete
+    RATIO: Incomplete
