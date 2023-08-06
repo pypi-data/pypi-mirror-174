05c0ee62fb5766c7b739e4c8d07e8ab1714712b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:40:25 +0000

    Add package megasepryte/1.0.0/megasepryte-1.0.0-py3-none-any.whl

---
+++ packages/megasepryte/megasepryte-1.0.0-py3-none-any.whl/starter.py
@@ -0,0 +1,36 @@
+import megasepryte
+
+project = megasepryte.Project("Gk")
+#
+#
+
+
+project.init_project("C:/Users/atlee/Jottacloud/Programmering/MegaDrive/Prosjekt/Gk")
+
+project.new_scene("utenfor_kontoret")
+project.import_aseprite_file("utenfor_kontoret", "C:/Users/atlee/Jottacloud/Programmering/MegaDrive/Prosjekt/Gk/art/Levels/utenfor_kontoret/utenfor_kontoret_v2.ase")
+#project.export_scene("gk_startscreen")
+
+#project.import_deflemask_file("C:/Users/atlee/Jottacloud/Programmering/Deflemask/songs/GENESIS/Mitt/house_loop.dmf")
+
+
+# open file
+#project.list_project_file_scenes("C:/Users/atlee/Jottacloud/Programmering/MegaDrive/Prosjekt/megasepryte/megasepryte/savefolder/megasepryte.megasepryte")
+
+#project.open_project_file("C:/Users/atlee/Jottacloud/Programmering/MegaDrive/Prosjekt/megasepryte/megasepryte/savefolder/megasepryte.megasepryte")
+
+#project.export_scene("first_scene")
+#project.export_scene("penis")
+# open file
+
+
+
+
+
+#project.new_scene("penis")
+#project.import_aseprite_file("penis", "C:/Users/atlee/Jottacloud/Programmering/MegaDrive/Prosjekt/megasepryte/art/penis.ase")
+#project.export_scene("penis")
+
+#project.test_scene("utenfor_kontoret")
+#project.test_scene("first_scene")
+#project.save_project()#"C:/Users/atlee/Jottacloud/Programmering/MegaDrive/Prosjekt/megasepryte")
