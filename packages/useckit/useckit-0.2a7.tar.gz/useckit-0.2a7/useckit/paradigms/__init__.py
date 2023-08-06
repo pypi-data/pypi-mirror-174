a98e0549d33c710f9e59d9184b8922c1fb966fef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:22:33 +0000

    Add package useckit/0.2a7/useckit-0.2a7.tar.gz

---
+++ packages/useckit/useckit-0.2a7.tar.gz/useckit-0.2a7/useckit/paradigms/__init__.py
@@ -0,0 +1,5 @@
+from useckit.paradigms.anomaly_detection import *
+from useckit.paradigms.distance_learning import *
+from useckit.paradigms.siamese_thresholding import *
+from useckit.paradigms.time_series_classification import *
+from useckit.paradigms._paradigm_base import *
