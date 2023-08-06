5638e92c3baa96b3259baac728eda79d9ef3a515
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:22:23 +0000

    Add package moonworm/0.5.2/moonworm-0.5.2-py3-none-any.whl

---
+++ packages/moonworm/moonworm-0.5.2-py3-none-any.whl/moonworm/crawler/networks.py
@@ -0,0 +1,43 @@
+from typing import Dict
+
+try:
+    from moonstreamdb.db import yield_db_session_ctx
+    from moonstreamdb.models import (
+        Base,
+        EthereumBlock,
+        EthereumLabel,
+        EthereumTransaction,
+        PolygonBlock,
+        PolygonLabel,
+        PolygonTransaction,
+        XDaiBlock,
+        XDaiLabel,
+        XDaiTransaction,
+    )
+except ImportError:
+    print("this feature requires moonstreamdb which is not installed")
+    print("to enable, run: `pip install moonworm[moonstream]`")
+    raise ImportError(
+        "moonstreamdb not installed, to install, run: `pip install moonworm[moonstream]`"
+    )
+
+
+from .utils import Network
+
+MODELS: Dict[Network, Dict[str, Base]] = {
+    Network.ethereum: {
+        "blocks": EthereumBlock,
+        "labels": EthereumLabel,
+        "transactions": EthereumTransaction,
+    },
+    Network.polygon: {
+        "blocks": PolygonBlock,
+        "labels": PolygonLabel,
+        "transactions": PolygonTransaction,
+    },
+    Network.xdai: {
+        "blocks": XDaiBlock,
+        "labels": XDaiLabel,
+        "transactions": XDaiTransaction,
+    },
+}
---
+++ packages/moonworm/moonworm-0.5.2-py3-none-any.whl/moonworm/version.py
@@ -0,0 +1,1 @@
+MOONWORM_VERSION = "0.5.2"
