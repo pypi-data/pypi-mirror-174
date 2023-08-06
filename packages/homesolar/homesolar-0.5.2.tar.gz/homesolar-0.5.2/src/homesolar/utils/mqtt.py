32bd791abc6901fce4c6e7183d8522e755829eb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:59 +0000

    Add package homesolar/0.5.2/homesolar-0.5.2.tar.gz

---
+++ packages/homesolar/homesolar-0.5.2.tar.gz/homesolar-0.5.2/src/homesolar/utils/mqtt.py
@@ -0,0 +1,9 @@
+# Companion Class
+class MqttTopic:
+    SENSORS = "homesolar/sensors/#"
+    ACTUATORS = "homesolar/actuators/#"
+    CLIENT = "homesolar/app/clients/#"
+    REQUEST = "homesolar/app/request/#"
+    RESPONSE = "homesolar/app/response"
+    SUMMARY = "homesolar/summary"
+    BATTERY = "homesolar/battery"
