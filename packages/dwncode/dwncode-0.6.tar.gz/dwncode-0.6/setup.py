b9d5c367baf658d5889e3df44418ee03f2826acf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:37:23 +0000

    Add package dwncode/0.6/dwncode-0.6.tar.gz

---
+++ packages/dwncode/dwncode-0.6.tar.gz/dwncode-0.6/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name="dwncode",
+      version="0.6",
+      description="Dwn Code",
+      long_description="For TE Students",
+      author="JOD",
+      packages=['dwncode'],
+      install_requires=[])
