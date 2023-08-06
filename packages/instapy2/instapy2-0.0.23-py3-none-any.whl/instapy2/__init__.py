ed0cfd439502b5d90ce13f26e95f5eed71e1f605
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:34 +0000

    Add package instapy2/0.0.23/instapy2-0.0.23-py3-none-any.whl

---
+++ packages/instapy2/instapy2-0.0.23-py3-none-any.whl/instapy2/__init__.py
@@ -0,0 +1,5 @@
+from .configuration import Configuration
+from .instapy2 import InstaPy2
+from .instapy2_base import InstaPy2Base
+
+from .types import CommentType, FollowType, LikeType
---
+++ packages/instapy2/instapy2-0.0.23-py3-none-any.whl/instapy2/types/__init__.py
@@ -0,0 +1,3 @@
+from .comment_type import CommentType
+from .follow_type import FollowType
+from .like_type import LikeType
