24c5cd4a0f46126a540b12ef5068fe4dc3d96177
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:59 +0000

    Add package olorenchemengine/1.0.8/olorenchemengine-1.0.8.tar.gz

---
+++ packages/olorenchemengine/olorenchemengine-1.0.8.tar.gz/olorenchemengine-1.0.8/src/olorenchemengine/external/__init__.py
@@ -0,0 +1,9 @@
+from .ChemProp.main import ChemPropModel
+from .GINNetwork.main import GINModel
+from .HondaSTRep.main import HondaSTRep
+from .HondaSTRep.operations import WordVocab
+from .mol2vec.main import Mol2Vec
+from .MolCLR.main import MolCLR, MolCLRVecRep
+from .piCalculax import calc_pI
+from .SPGNN.main import SPGNN, SPGNNVecRep
+from .stoned import *
