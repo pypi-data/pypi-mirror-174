2d1c632461cf19483e7a90df59802247881c3f0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:12 +0000

    Add package drf-useful-decorators/0.0.6/drf_useful_decorators-0.0.6.tar.gz

---
+++ packages/drf-useful-decorators/drf_useful_decorators-0.0.6.tar.gz/drf_useful_decorators-0.0.6/drf_useful_decorators/__init__.py
@@ -0,0 +1,8 @@
+'''
+:authors: lumer
+:license: Apache License, Version 2.0, see LICENSE file
+:copyright: (c) 2022 lumer
+'''
+
+from .decorators import exceptions_endpoint
+from .methods import get_response
