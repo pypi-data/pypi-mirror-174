2baa75bc07bf51840225e1814e9b423387a85ade
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:45 +0000

    Add package Glances/3.3.0.2/Glances-3.3.0.2-py3-none-any.whl

---
+++ packages/Glances/Glances-3.3.0.2-py3-none-any.whl/glances/outputs/static/js/components/plugin-processcount.vue
@@ -0,0 +1,44 @@
+<template>
+    <section id="processcount" class="plugin">
+        <span class="title">TASKS</span>
+        <span>{{ total }} ({{ thread }} thr),</span>
+        <span>{{ running }} run,</span>
+        <span>{{ sleeping }} slp,</span>
+        <span>{{ stopped }} oth</span>
+        <span class="title">{{ sorter.auto ? 'sorted automatically' : 'sorted' }}</span>
+        <span>by {{ sorter.getColumnLabel(sorter.column) }}</span>
+    </section>
+</template>
+
+<script>
+export default {
+    props: {
+        data: {
+            type: Object
+        },
+        sorter: {
+            type: Object
+        }
+    },
+    computed: {
+        stats() {
+            return this.data.stats['processcount'];
+        },
+        total() {
+            return this.stats['total'] || 0;
+        },
+        running() {
+            return this.stats['running'] || 0;
+        },
+        sleeping() {
+            return this.stats['sleeping'] || 0;
+        },
+        stopped() {
+            return this.stats['stopped'] || 0;
+        },
+        thread() {
+            return this.stats['thread'] || 0;
+        }
+    }
+};
+</script>
