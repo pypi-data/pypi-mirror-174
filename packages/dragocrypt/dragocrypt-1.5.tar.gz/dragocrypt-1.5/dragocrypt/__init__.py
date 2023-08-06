b2412ba6a520cf2d851cf9dcd9d0cb0c746164da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:37 +0000

    Add package dragocrypt/1.5/dragocrypt-1.5.tar.gz

---
+++ packages/dragocrypt/dragocrypt-1.5.tar.gz/dragocrypt-1.5/dragocrypt/__init__.py
@@ -0,0 +1,1 @@
+from .dragocrypt import *
---
+++ packages/dragocrypt/dragocrypt-1.5.tar.gz/dragocrypt-1.5/setup.py
@@ -0,0 +1,54 @@
+import os , codecs
+
+from setuptools import setup, find_packages
+
+pkg = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(pkg, "README.md"), encoding="utf-8") as fh:
+	
+    long_description = "\n" + fh.read()
+
+
+setup(
+
+    name="dragocrypt",
+    
+    version="1.5",
+    
+    author="The Jordan Ghost",
+    
+    author_email="Novayou0@gmail.com",
+    
+    description = ("For developers"),
+    
+    long_description_content_type="text/markdown",
+    
+    url="https://github.com/NovaTools4",
+    
+    project_urls={
+        "Bug Tracker": "https://github.com/pypa/sampleproject/issues",
+    },
+    
+    long_description=long_description,
+    
+    packages=find_packages(),
+    
+    install_requires=[],
+    
+    keywords=['dragocrypt'],
+    
+    classifiers=[
+    
+        "Development Status :: 1 - Planning",
+        
+        "Intended Audience :: Developers",
+        
+        "Programming Language :: Python :: 3",
+        
+        "Operating System :: Unix",
+        
+        "Operating System :: MacOS :: MacOS X",
+        
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
