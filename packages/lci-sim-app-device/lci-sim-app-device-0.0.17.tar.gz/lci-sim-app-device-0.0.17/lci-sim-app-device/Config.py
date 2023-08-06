8e3daf84f881ab8e5fef6db759547ba4be5e1ca5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:01 +0000

    Add package lci-sim-app-device/0.0.17/lci-sim-app-device-0.0.17.tar.gz

---
+++ packages/lci-sim-app-device/lci-sim-app-device-0.0.17.tar.gz/lci-sim-app-device-0.0.17/lci-sim-app-device/Config.py
@@ -0,0 +1,10 @@
+import os
+__APPLICATION_PATH__ = os.path.dirname(__file__)
+__APPLICATION_DATA__ = __APPLICATION_PATH__ + "/Data"
+
+if not os.path.exists(__APPLICATION_DATA__):
+	os.makedirs(__APPLICATION_DATA__)
+
+__APPLICATION_DATABASE_PATH__ = __APPLICATION_DATA__ + "/sim.db"
+
+__DEFAULT_CONTROLLER_SOCKET__ = 'http://srv01.letscode.it:9851/'
