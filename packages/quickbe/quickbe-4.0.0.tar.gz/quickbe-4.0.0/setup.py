457e21af566f669489f4594fd4a7e209f35df2b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:20:17 +0000

    Add package quickbe/4.0.0/quickbe-4.0.0.tar.gz

---
+++ packages/quickbe/quickbe-4.0.0.tar.gz/quickbe-4.0.0/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="quickbe",
+    version="4.0.0",
+    author="Eldad Bishari",
+    author_email="eldad@1221tlv.org",
+    description="Quick and simple back-end infrastructure",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/eldad1221/quickbe",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'flask==2.0.1',
+        'schedule==1.1.0',
+        'psutil==5.8.0',
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)
