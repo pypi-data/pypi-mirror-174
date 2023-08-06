87893b17cbfc0401019e1b45dd897a57bdcb65d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:36 +0000

    Add package multitax/1.2.1/multitax-1.2.1-py3-none-any.whl

---
+++ packages/multitax/multitax-1.2.1-py3-none-any.whl/multitax/greengenestx.py
@@ -0,0 +1,54 @@
+from .multitax import MultiTax
+
+
+class GreengenesTx(MultiTax):
+    _default_urls = [
+        "https://gg-sg-web.s3-us-west-2.amazonaws.com/downloads/greengenes_database/gg_13_5/gg_13_5_taxonomy.txt.gz"]
+    _rank_codes = [("k__", "kingdom"),
+                   ("p__", "phylum"),
+                   ("c__", "class"),
+                   ("o__", "order"),
+                   ("f__", "family"),
+                   ("g__", "genus"),
+                   ("s__", "species")]
+
+    def __init__(self, **kwargs):
+        # forwards.tsv
+        self._forwards = {}
+        super().__init__(**kwargs)
+
+    def __repr__(self):
+        args = ['{}={}'.format(k, repr(v)) for (k, v) in vars(self).items()]
+        return 'GreengenesTx({})'.format(', '.join(args))
+
+    def _parse(self, fhs, **kwargs):
+        nodes = {}
+        ranks = {}
+        names = {}
+
+        for source, fh in fhs.items():
+            for line in fh:
+                try:
+                    _, lineage = line.rstrip().split('\t')
+                except:
+                    _, lineage = line.decode().rstrip().split('\t')
+                lin = lineage.split("; ")
+                for i in range(len(lin))[::-1]:
+                    # assert rank
+                    assert lin[i][:3] == self._rank_codes[i][0]
+                    # taxid = "c__Deinococci", rank = "class", name = "Deinococci"
+                    taxid = lin[i]
+                    name = lin[i][3:]
+                    if not name:
+                        continue  # empty entry "s__"
+                    rank = self._rank_codes[i][1]
+                    if i == 0:
+                        parent_taxid = self._default_root_node
+                    else:
+                        parent_taxid = lin[i-1]
+                    if taxid not in nodes:
+                        nodes[taxid] = parent_taxid
+                        names[taxid] = name
+                        ranks[taxid] = rank
+
+        return nodes, ranks, names
---
+++ packages/multitax/multitax-1.2.1-py3-none-any.whl/multitax/gtdbtx.py
@@ -0,0 +1,53 @@
+from .multitax import MultiTax
+
+
+class GtdbTx(MultiTax):
+
+    _default_urls = ["https://data.gtdb.ecogenomic.org/releases/latest/ar53_taxonomy.tsv.gz",
+                     "https://data.gtdb.ecogenomic.org/releases/latest/bac120_taxonomy.tsv.gz"]
+    _rank_codes = [("d__", "domain"),
+                   ("p__", "phylum"),
+                   ("c__", "class"),
+                   ("o__", "order"),
+                   ("f__", "family"),
+                   ("g__", "genus"),
+                   ("s__", "species")]
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    def __repr__(self):
+        args = ['{}={}'.format(k, repr(v)) for (k, v) in vars(self).items()]
+        return 'GtdbTx({})'.format(', '.join(args))
+
+    def _parse(self, fhs, **kwargs):
+        nodes = {}
+        ranks = {}
+        names = {}
+        for source, fh in fhs.items():
+            for line in fh:
+                try:
+                    _, lineage = line.rstrip().split('\t')
+                except:
+                    _, lineage = line.decode().rstrip().split('\t')
+                lin = lineage.split(";")
+                for i in range(len(lin))[::-1]:
+                    # assert rank
+                    assert lin[i][:3] == self._rank_codes[i][0]
+                    # taxid = "c__Deinococci", rank = "class", name = "Deinococci"
+                    taxid = lin[i]
+                    name = lin[i][3:]
+                    # empty entry "s__"
+                    if not name:
+                        continue
+                    rank = self._rank_codes[i][1]
+                    if i == 0:
+                        parent_taxid = self._default_root_node
+                    else:
+                        parent_taxid = lin[i-1]
+                    if taxid not in nodes:
+                        nodes[taxid] = parent_taxid
+                        names[taxid] = name
+                        ranks[taxid] = rank
+
+        return nodes, ranks, names
