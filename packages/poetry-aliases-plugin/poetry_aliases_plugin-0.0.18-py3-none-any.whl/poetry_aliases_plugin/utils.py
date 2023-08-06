d986a35377b1c17de50c8c1d0736ce9e57828994
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:08:49 +0000

    Add package poetry-aliases-plugin/0.0.18/poetry_aliases_plugin-0.0.18-py3-none-any.whl

---
+++ packages/poetry-aliases-plugin/poetry_aliases_plugin-0.0.18-py3-none-any.whl/poetry_aliases_plugin/utils.py
@@ -0,0 +1,42 @@
+from typing import Callable
+
+from poetry_aliases_plugin import config
+
+
+class PluginException(RuntimeError):
+    def __str__(self) -> str:
+        return f'{config.PLUGIN_NAME}: {self.args[0]}'
+
+
+class PluginCommandException(PluginException):
+
+    command: str
+    exception: BaseException
+
+    def __init__(self, command: str, exception: BaseException, *args: object) -> None:
+        self.command = command
+        self.exception = exception
+        super().__init__(*args, *exception.args)
+
+    def __str__(self) -> str:
+        return f'{config.PLUGIN_NAME}: {self.command}: {type(self.exception)} {self.args[0]}'
+
+
+def plugin_exception_wrapper(func: Callable):
+    def wrapper(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+
+        except PluginException as ex:
+            raise ex
+
+        except BaseException as ex:
+            raise PluginException(ex) from ex
+
+    return wrapper
+
+
+def normalize_command(command: str) -> str:
+    command = command.removeprefix('poetry').strip()
+    command = command if command.startswith('run') else f'run {command}'
+    return command.strip()
