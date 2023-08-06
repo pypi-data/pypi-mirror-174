3ae28bd9cb0cf74ee0848bc472c2b614c863cbcd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:46 +0000

    Add package dm3loc/1.1.0/dm3loc-1.1.0.tar.gz

---
+++ packages/dm3loc/dm3loc-1.1.0.tar.gz/dm3loc-1.1.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='dm3loc',
+    version='1.1.0',
+    description='Deep-learning Framework with Multi-head Self-attention for Multi-label mRNA Subcellular Localization Prediction and Analyses',
+    author='Duolin Wang',
+    author_email='deepduoduo@gmail.com',
+    install_requires = ['numpy', 'scipy', 'scikit-learn','matplotlib','h5py==2.10.0','keras==2.2.4', 'tensorflow==1.13.1', 'protobuf==3.20.0'],
+    packages=find_packages(),
+    include_package_data=True,
+    package_data = {
+        'model': ['DM3Loc/model/*'],
+        'testdata': ['DM3Loc/testdata/*']
+    }
+
+)
