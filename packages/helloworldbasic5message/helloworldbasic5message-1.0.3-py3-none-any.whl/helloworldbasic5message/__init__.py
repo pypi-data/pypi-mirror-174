03c5f01961ecb9cf8d9c6b62473b8a2cc6283975
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:58 +0000

    Add package helloworldbasic5message/1.0.3/helloworldbasic5message-1.0.3-py3-none-any.whl

---
+++ packages/helloworldbasic5message/helloworldbasic5message-1.0.3-py3-none-any.whl/helloworldbasic5message/__init__.py
@@ -0,0 +1,24 @@
+# description like this
+# abc
+# 123
+
+try:
+    import helloworldbasic4message.utility1 as hb1
+    print("success loaded")
+except:
+    print("failed import")
+
+
+
+def saygreeting(name):
+    return hb1.utilA_sayhello(name)
+
+
+
+
+
+
+
+
+
+
