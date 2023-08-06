b7aef761fb942c46aa79dd6cebd496ed939e2be4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:33:34 +0000

    Add package mutwo.core/0.63.0/mutwo.core-0.63.0-py3-none-any.whl

---
+++ packages/mutwo.core/mutwo.core-0.63.0-py3-none-any.whl/mutwo/core_parameters/__init__.py
@@ -0,0 +1,19 @@
+"""Abstractions for attributes that can be assigned to Event objects.
+
+"""
+
+from . import configurations
+from . import abc
+
+from .durations import *
+from .tempos import *
+
+from . import durations, tempos
+
+from mutwo import core_utilities
+
+__all__ = core_utilities.get_all(durations, tempos)
+
+
+# Force flat structure
+del core_utilities, durations, tempos
