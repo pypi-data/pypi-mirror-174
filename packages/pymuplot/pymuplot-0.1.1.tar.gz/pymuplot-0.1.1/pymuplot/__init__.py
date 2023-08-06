dfcfeec8c92a5632751980ec611a9fe198370fe0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:19:45 +0000

    Add package pymuplot/0.1.1/pymuplot-0.1.1.tar.gz

---
+++ packages/pymuplot/pymuplot-0.1.1.tar.gz/pymuplot-0.1.1/pymuplot/__init__.py
@@ -0,0 +1,3 @@
+"""Initial script."""
+
+__version__ = "0.1.1"
---
+++ packages/pymuplot/pymuplot-0.1.1.tar.gz/pymuplot-0.1.1/test/test_init.py
@@ -0,0 +1,7 @@
+import unittest
+from pymuplot import __version__
+
+
+class TestInit(unittest.TestCase):
+    def test_version(self):
+        self.assertEqual(__version__, "0.1.0")
