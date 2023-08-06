abb437523d73e29387caeecb2f6c1f001b497fd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:28 +0000

    Add package kluff/0.0.9/kluff-0.0.9-py3-none-any.whl

---
+++ packages/kluff/kluff-0.0.9-py3-none-any.whl/kluff/templates/react/src/App.js
@@ -0,0 +1,30 @@
+import "./App.css";
+import { useEffect } from 'react';
+import axios from 'axios';
+
+function App() {
+  const backend_url = 'http://localhost:5000'
+  let data = "";
+
+  hitAPI = () => {
+    axios.get(backend_url)
+      .then(res => {
+        console.log(res.data)
+        data = res.data
+      })
+    }
+
+  useEffect(() => {
+      hitAPI();
+  }, []);
+
+
+  return (
+    <div className="app">
+      <h1>{ data }</h1>
+      <h4>Data from Backend at { backend_url }</h4>
+    </div>
+  );
+}
+
+export default App;
