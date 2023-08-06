035ce9fccaacfa8cec1fe030b995e26640ea20e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:25:05 +0000

    Add package mercadopago-qr/1.0.2/mercadopago_qr-1.0.2-py3.6.egg

---
+++ packages/mercadopago-qr/mercadopago_qr-1.0.2-py3.6.egg/mercadopago_qr/qr/qr.py
@@ -0,0 +1,24 @@
+from .. http.http import http
+from .. http.server import server
+from .. http.queries import queries
+import logging
+_logger = logging.getLogger(__name__)
+
+class qr:
+
+    def __init__(self):
+        _logger.warning("init qr code service...")
+
+    def validate(self, data):
+        if(len(data) > 0):
+            return True
+        return False
+
+    def generate_qr_code(self, params={}):
+        qr_request = queries()
+        url = http()
+        endpoint = str(url.get_endpoint('api')) + str(url.get_endpoint('qr'))
+        mercadopago = server(client_secret=params['client_secret'])
+        headers = qr_request.get_headers_qr(mercadopago)
+        response = qr_request.post(url=endpoint, headers=headers, data=params['data'])
+        return response
