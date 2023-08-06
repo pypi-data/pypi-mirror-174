5b561c25d49f107080bb553a8dc12c178facedec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:18:41 +0000

    Add package redis-job-controller/0.0.8/redis-job-controller-0.0.8.tar.gz

---
+++ packages/redis-job-controller/redis-job-controller-0.0.8.tar.gz/redis-job-controller-0.0.8/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+import os
+
+
+version = os.getenv("PACKAGE_VERSION", "0.0.8a")
+
+
+# with open('README.rst') as f:
+#     readme = f.read()
+
+# with open('LICENSE') as f:
+#     license = f.read()
+
+
+setup(
+    name='redis-job-controller',
+    version=version,
+    description='Package for enqueuing and fetching async jobs, using Redis as a queue and result repository',
+#    long_description=readme,
+    author='Karol Ważny',
+#    author_email='me@kennethreitz.com',
+#    url='https://github.com/kennethreitz/samplemod',
+#    license=license,
+    packages=find_packages(exclude=('tests', 'docs')),
+    install_requires=['redis']
+)
