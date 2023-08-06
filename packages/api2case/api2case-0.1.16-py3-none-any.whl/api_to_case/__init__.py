f82ae341e4dd6633ee92ecb3174a003ba9d94cfa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:20 +0000

    Add package api2case/0.1.16/api2case-0.1.16-py3-none-any.whl

---
+++ packages/api2case/api2case-0.1.16-py3-none-any.whl/api_to_case/__init__.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+"""
+-*- coding: utf-8 -*-
+Author   : JiQing
+Email    : qing.ji@extremevision.com.cn
+Date     : 2022/10/24 10:06
+Desc     :
+FileName : __init__.py.py
+Software : PyCharm
+"""
+__title__ = 'api2case'
+__description__ = 'Convert Swagger API to YAML testcase for jixing_test_frame'
+__url__ = 'https://github.com/CaptainJi/api2case'
+__version__ = '0.1.16'
+__author__ = 'Captain Ji'
+__author_email__ = 'qing.ji@extremevision.com.cn'
+__license__ = 'Apache-2.0'
+__copyright__ = 'Copyright 2022'
