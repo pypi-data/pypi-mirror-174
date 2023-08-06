edc8b5624ab76422587dee63859df0d2189e3748
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:08:32 +0000

    Add package poetry-aliases-plugin/0.0.17/poetry_aliases_plugin-0.0.17-py3-none-any.whl

---
+++ packages/poetry-aliases-plugin/poetry_aliases_plugin-0.0.17-py3-none-any.whl/poetry_aliases_plugin/config.py
@@ -0,0 +1,48 @@
+import logging
+
+from poetry.core.pyproject.toml import PyProjectTOML
+
+PLUGIN_NAME = 'poetry-aliases-plugin'
+
+logger = logging.getLogger('poetry_aliases_plugin')
+
+
+class AliasesConfig(object):
+    """Обертка над конфигурацией pyproject"""
+
+    pyproject: PyProjectTOML
+
+    def __init__(self, pyproject: PyProjectTOML) -> None:
+        self.pyproject = pyproject
+
+    def validate(self):
+        assert self.settings['engine'] in ('poetry', 'subprocess'), 'Движок "%s" не допустим' % self.settings['engine']
+
+    @property
+    def _default_aliases(self):
+        return {'this': 'poetry run python -m this'}
+
+    @property
+    def aliases(self) -> dict:
+        aliases = self.pyproject.data.get('tool', {}).get('aliases', {})
+        aliases = self._default_aliases | aliases
+
+        if 'settings' in aliases:
+            del aliases['settings']
+
+        return aliases
+
+    @property
+    def _default_setting(self):
+        return {
+            'find_poetry_command': False,
+            'engine': 'poetry',
+        }
+
+    def validate_settings(self, settings: dict):
+        pass
+
+    @property
+    def settings(self):
+        settings = self.pyproject.data.get('tool', {}).get('aliases', {}).get('settings', {})
+        return self._default_setting | settings
