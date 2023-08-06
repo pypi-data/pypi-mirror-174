2b56a777298529912dbd408ff13837b4d07b95f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:03 +0000

    Add package ostrat/1.0.5/ostrat-1.0.5.tar.gz

---
+++ packages/ostrat/ostrat-1.0.5.tar.gz/ostrat-1.0.5/ostrat/__init__.py
@@ -0,0 +1,11 @@
+"""
+:authors: lexek
+:license: OSI Approved :: GNU General Public License v3 (GPLv3)
+:copyright: (c) 2022 lexek
+"""
+
+from .ostrat import *
+
+__author__ = 'lexek'
+__version__ = '1.0.5'
+__email__ = 'minelanpro@gmail.com'
