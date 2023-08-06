5bc7435848dc97a6f145184984ff2e4117f58b6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:37 +0000

    Add package docarray/0.18.2.dev30/docarray-0.18.2.dev30.tar.gz

---
+++ packages/docarray/docarray-0.18.2.dev30.tar.gz/docarray-0.18.2.dev30/docarray/__init__.py
@@ -0,0 +1,12 @@
+__version__ = '0.18.2.dev30'
+
+import os
+
+from docarray.document import Document
+from docarray.array import DocumentArray
+from docarray.dataclasses import dataclass, field
+
+if 'DA_RICH_HANDLER' in os.environ:
+    from rich.traceback import install
+
+    install()
