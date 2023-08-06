b582e7efe6ca6e2a448501e8080994dde03d3891
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:57:04 +0000

    Add package loggertelebot/0.1.0/loggertelebot-0.1.0.tar.gz

---
+++ packages/loggertelebot/loggertelebot-0.1.0.tar.gz/loggertelebot-0.1.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import find_packages, setup
+setup(
+    name='loggertelebot',
+    packages=find_packages(include=['telelogging']),
+    version='0.1.0',
+    description='My first Python library',
+    author='c0mrade',
+    license='MIT',
+    install_requires=[
+        'requests',
+        'pymongo',
+    ],
+
+)
