7046a5f8ddcc47c175adf97949904016d9bf634e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:06 +0000

    Add package bruces/0.3.0/bruces-0.3.0-py3-none-any.whl

---
+++ packages/bruces/bruces-0.3.0-py3-none-any.whl/bruces/_helpers.py
@@ -0,0 +1,94 @@
+from datetime import datetime
+
+import numpy as np
+
+__all__ = [
+    "from_csep",
+    "to_decimal_year",
+]
+
+
+_datetime_like = (datetime, np.datetime64)
+
+
+def from_csep(catalog):
+    """
+    Convert a :class:`csep.Catalog` to :class:`bruces.Catalog`.
+
+    Parameters
+    ----------
+    catalog : :class:`csep.Catalog`
+        Input catalog.
+
+    Returns
+    -------
+    :class:`bruces.Catalog`
+        Output catalog.
+
+    """
+    import utm
+
+    from ._catalog import Catalog
+
+    dates = catalog.get_datetimes()
+    latitudes = catalog.get_latitudes()
+    longitudes = catalog.get_longitudes()
+    depths = catalog.get_depths()
+    magnitudes = catalog.get_magnitudes()
+
+    eastings, northings, _, _ = utm.from_latlon(latitudes, longitudes)
+    eastings *= 1.0e-3
+    northings *= 1.0e-3
+
+    return Catalog(dates, eastings, northings, depths, magnitudes)
+
+
+def to_decimal_year(dates):
+    """
+    Convert datetime_like to decimal year.
+
+    Parameters
+    ----------
+    dates : datetime_like or sequence of datetime_like
+        Date time or list of date times.
+
+    Returns
+    -------
+    scalar or list
+        Decimal year or list of decimal years.
+
+    """
+
+    def decimal_year(d):
+        """Convert a datetime_like to decimal year."""
+        if isinstance(d, datetime):
+            year = d.year
+        elif isinstance(d, np.datetime64):
+            d = d.astype("M8[ms]").tolist()
+            year = d.year
+        else:
+            raise TypeError()
+
+        d1 = datetime(year, 1, 1)
+        d2 = datetime(year + 1, 1, 1)
+
+        return (
+            year
+            + (d.replace(tzinfo=None) - d1).total_seconds() / (d2 - d1).total_seconds()
+        )
+
+    ndim = np.ndim(dates)
+    if ndim == 0:
+        if not isinstance(dates, _datetime_like):
+            raise TypeError()
+    elif ndim == 1:
+        if any(not isinstance(date, _datetime_like) for date in dates):
+            raise TypeError()
+    else:
+        raise TypeError()
+
+    return (
+        decimal_year(dates)
+        if ndim == 0
+        else np.array([decimal_year(date) for date in dates])
+    )
---
+++ packages/bruces/bruces-0.3.0-py3-none-any.whl/bruces/decluster/gardner_knopoff/_gardner_knopoff.py
@@ -0,0 +1,59 @@
+import numpy as np
+
+from ..._common import dist3d, jitted
+from .._helpers import register
+
+
+def decluster(catalog):
+    """
+    Decluster earthquake catalog using Gardner-Knopoff's method.
+
+    Parameters
+    ----------
+    catalog : :class:`bruces.Catalog`
+        Earthquake catalog.
+
+    Returns
+    -------
+    :class:`bruces.Catalog`
+        Declustered earthquake catalog.
+
+    """
+    t = catalog.years * 365.25  # Days
+    x = catalog.eastings
+    y = catalog.northings
+    z = catalog.depths
+    m = catalog.magnitudes
+
+    bg = _decluster(t, x, y, z, m)
+    return catalog[bg]
+
+
+@jitted
+def _decluster(t, x, y, z, m):
+    """Gardner-Knopoff's method."""
+    N = len(t)
+
+    bg = np.ones(N, dtype=np.bool_)
+    for i in range(N):
+        mag = m[i]
+
+        # Calculate distance window length
+        dr = 0.1238 * mag + 0.983
+        dr = 10.0**dr
+
+        # Calculate time window length
+        dt = 0.032 * mag + 2.7389 if mag >= 6.5 else 0.5409 * mag - 0.547
+        dt = 10.0**dt
+
+        # Loop over catalog
+        for j in range(N):
+            if bg[j] and m[j] < mag and 0 < t[j] - t[i] < dt:
+                r_ij = dist3d(x[i], y[i], z[i], x[j], y[j], z[j])
+                if r_ij < dr:
+                    bg[j] = False
+
+    return bg
+
+
+register("gardner-knopoff", decluster)
