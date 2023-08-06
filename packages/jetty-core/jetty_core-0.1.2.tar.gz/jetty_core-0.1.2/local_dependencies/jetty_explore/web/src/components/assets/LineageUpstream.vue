c529e3d3118e91a1350c2695c4bdaf65aaa4f2ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:22:24 +0000

    Add package jetty-core/0.1.2/jetty_core-0.1.2.tar.gz

---
+++ packages/jetty-core/jetty_core-0.1.2.tar.gz/jetty_core-0.1.2/local_dependencies/jetty_explore/web/src/components/assets/LineageUpstream.vue
@@ -0,0 +1,70 @@
+<template>
+  <JettyTable
+    title="Upstream Lineage"
+    :rows-per-page="10"
+    :row-transformer="rowTransformer"
+    :columns="columns"
+    :csv-config="csvConfig"
+    :fetchPath="'/api/asset/' + nodeId(props.node) + '/lineage_upstream'"
+    v-slot="{ props: { row } }: { props: { row: AssetWithPaths } }"
+    :tip="`Assets upstream from ${nodeNameAsString(
+      props.node
+    )}, based on data lineage`"
+  >
+    <q-tr>
+      <q-td key="name">
+        <AssetHeadline :asset="row.node" />
+      </q-td>
+      <q-td key="paths" class="q-px-none">
+        <NodePath :paths="row.paths" />
+      </q-td>
+    </q-tr>
+  </JettyTable>
+</template>
+
+<script lang="ts" setup>
+import JettyTable from '../JettyTable.vue';
+import { AssetWithPaths } from 'src/components/models';
+import { getPathAsString, nodeNameAsString, nodeId } from 'src/util';
+import NodePath from '../NodePath.vue';
+import AssetHeadline from './AssetHeadline.vue';
+
+import { mapNodeSummaryforSearch } from 'src/util/search';
+
+const props = defineProps(['node']);
+
+// Filters by name, or platform
+const rowTransformer = (row: AssetWithPaths): string =>
+  mapNodeSummaryforSearch(row.node);
+
+const columns = [
+  {
+    name: 'name',
+    label: 'Asset Name',
+    field: 'name',
+    sortable: true,
+    align: 'left',
+  },
+  {
+    name: 'paths',
+    label: 'Paths',
+    field: 'paths',
+    sortable: false,
+    align: 'left',
+  },
+];
+
+const csvConfig = {
+  filename: nodeNameAsString(props.node) + '_upstream_assets_by_lineage.csv',
+  columnNames: ['Asset Name', 'Asset Platform', 'Path'],
+  // accepts a row and returns the proper mapping
+  mappingFn: (filteredSortedRows: AssetWithPaths[]) =>
+    filteredSortedRows.flatMap((r) =>
+      r.paths.map((p) => [
+        nodeNameAsString(r.node),
+        r.node.Asset.connectors,
+        getPathAsString(p),
+      ])
+    ),
+};
+</script>
---
+++ packages/jetty-core/jetty_core-0.1.2.tar.gz/jetty_core-0.1.2/local_dependencies/jetty_explore/web/src/components/users/AssetsTable.vue
@@ -0,0 +1,121 @@
+<template>
+  <JettyTable
+    title="User-Accessible Assets"
+    :rows-per-page="20"
+    :row-transformer="rowTransformer"
+    :columns="columns"
+    :csv-config="csvConfig"
+    :fetchPath="'/api/user/' + nodeId(props.node) + '/assets'"
+    v-slot="{
+      props: { row },
+    }: {
+      props: { row: AssetWithEffectivePermissions },
+    }"
+    :tip="`All the assets ${nodeNameAsString(
+      props.node
+    )} has access too, including the privilege levels and
+    sources of those privileges`"
+  >
+    <q-tr>
+      <q-td key="name">
+        <q-item class="q-px-none">
+          <q-item-section>
+            <router-link
+              :to="'/asset/' + nodeId(row.node)"
+              style="text-decoration: none; color: inherit"
+            >
+              <q-item-label> {{ nodeNameAsString(row.node) }}</q-item-label>
+            </router-link>
+
+            <q-item-label caption>
+              <JettyBadge
+                v-for="connector in row.node.Asset.connectors"
+                :key="connector"
+                :name="connector"
+              />
+            </q-item-label>
+          </q-item-section>
+        </q-item>
+      </q-td>
+      <q-td key="privileges" style="padding-right: 0px">
+        <q-list separator>
+          <q-item
+            v-for="privilege in row.privileges"
+            :key="privilege.privilege"
+            class="q-px-none"
+          >
+            <div class="q-pr-lg flex flex-center">
+              {{ privilege.privilege }}
+            </div>
+            <div>
+              <ul class="q-my-none">
+                <li
+                  v-for="reason in privilege.reasons"
+                  :key="reason"
+                  style="padding-top: 2px; padding-bottom: 2px"
+                >
+                  {{ reason }}
+                </li>
+              </ul>
+            </div>
+          </q-item>
+        </q-list>
+      </q-td>
+    </q-tr>
+  </JettyTable>
+</template>
+
+<script lang="ts" setup>
+import JettyTable from '../JettyTable.vue';
+import JettyBadge from '../JettyBadge.vue';
+import { AssetSummary, EffectivePermission } from '../models';
+import { nodeNameAsString, nodeId } from 'src/util';
+import { mapNodeSummaryforSearch } from 'src/util/search';
+
+interface AssetWithEffectivePermissions {
+  node: AssetSummary;
+  privileges: EffectivePermission[];
+}
+
+const props = defineProps(['node']);
+
+const rowTransformer = (row: AssetWithEffectivePermissions): string =>
+  [
+    mapNodeSummaryforSearch(row.node),
+    ...row.privileges.map((p) => p.privilege),
+  ].join(' ');
+
+const columns = [
+  {
+    name: 'name',
+    label: 'Asset Name',
+    field: 'name',
+    sortable: true,
+    align: 'left',
+  },
+  {
+    name: 'privileges',
+    label: 'Privileges',
+    field: 'privileges',
+    sortable: false,
+    align: 'left',
+  },
+];
+
+const csvConfig = {
+  filename: nodeNameAsString(props.node) + '_assets.csv',
+  columnNames: ['Asset Name', 'Asset Platform', 'Privilege', 'Explanation'],
+  // accepts a row and returns the proper mapping
+  mappingFn: (filteredSortedRows: AssetWithEffectivePermissions[]) =>
+    filteredSortedRows.flatMap((r) =>
+      r.privileges.flatMap((p) =>
+        p.reasons.map((e) => [
+          nodeNameAsString(r.node),
+          r.node.Asset.connectors.join(', '),
+          p.privilege,
+          e,
+        ])
+      )
+    ),
+};
+</script>
