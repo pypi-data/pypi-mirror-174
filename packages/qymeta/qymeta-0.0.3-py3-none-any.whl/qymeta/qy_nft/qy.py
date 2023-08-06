dd2e4ba9db48a9d95f1f06ae9fc499766ba65117
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:17 +0000

    Add package qymeta/0.0.3/qymeta-0.0.3-py3-none-any.whl

---
+++ packages/qymeta/qymeta-0.0.3-py3-none-any.whl/qymeta/qy_nft/qy.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# _*_ coding: utf-8 _*_
+# @Time : 2022/10/27 17:29
+# @Author : justin.郑 3907721@qq.com
+# @File : qy_nft.py
+# @desc : 清元链NFT接口
+
+
+def qy_test():
+    return "测试成功"
+
