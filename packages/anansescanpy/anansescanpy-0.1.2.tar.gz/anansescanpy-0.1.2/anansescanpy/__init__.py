d79f3c32c24a03eb5bf782bb4e609d44e22ab1e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:15 +0000

    Add package anansescanpy/0.1.2/anansescanpy-0.1.2.tar.gz

---
+++ packages/anansescanpy/anansescanpy-0.1.2.tar.gz/anansescanpy-0.1.2/anansescanpy/__init__.py
@@ -0,0 +1,6 @@
+__version__ = "0.1.2"
+
+from .anansescanpy import export_CPM_scANANSE
+from .anansescanpy import export_ATAC_scANANSE
+from .anansescanpy import config_scANANSE
+from .anansescanpy import DEGS_scANANSE
