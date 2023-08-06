7e1b05ba5b2a04b31203ae9a2cde1d83ce71fde6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:56:18 +0000

    Add package dane/0.3.5/DANE-0.3.5-py3-none-any.whl

---
+++ packages/dane/DANE-0.3.5-py3-none-any.whl/dane/__init__.py
@@ -0,0 +1,6 @@
+# for simplifying imports, e.g.:
+# `from dane import Document` instead of `from dane.document import Document`
+from .document import Document
+from .tasks import Task
+from .results import Result
+from .errors import *
