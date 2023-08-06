c31f930047a6808af1aa99c531cc2be85aeda290
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:19:35 +0000

    Add package miskibin/1.0.4/miskibin-1.0.4.tar.gz

---
+++ packages/miskibin/miskibin-1.0.4.tar.gz/miskibin-1.0.4/setup.py
@@ -0,0 +1,16 @@
+# setup.py sdist bdist_wheel
+from setuptools import setup
+import setuptools
+
+setup(
+    name="miskibin",
+    package_dir={"": "src"},
+    version="1.0.4",
+    author="Michał Skibiński",
+    description="This is package with useful tools",
+    long_description="This is a package  with useful tools",
+    url="https://github.com/michalskibinski109/miskibin",
+    python_requires=">=3.7, <4",
+    packages=["miskibin"],
+    license="MIT",
+)
