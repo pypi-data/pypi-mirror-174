21e01d9655b580d2a6bf8172cd95bd851bc9dc9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:35:03 +0000

    Add package wxalerts/0.0.1/wxalerts-0.0.1.tar.gz

---
+++ packages/wxalerts/wxalerts-0.0.1.tar.gz/wxalerts-0.0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='wxalerts',
+    version='0.0.1',
+    description='get weather alerts for a state',
+    license='GPLv3',
+    packages=['wxalerts'],
+    author='QÆZZ',
+    author_email='russian.devv@gmail.com',
+    keywords=['example'],
+    url='https://github.com/QAEZZ/wxalerts',
+    download_url='https://github.com/QAEZZ/wxalerts',
+    install_requires=['requests']
+)
