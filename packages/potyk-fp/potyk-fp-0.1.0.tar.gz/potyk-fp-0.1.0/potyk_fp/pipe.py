3b8a6a8799cd99a79d9de4713eaeb301bd9be5a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:24:52 +0000

    Add package potyk-fp/0.1.0/potyk-fp-0.1.0.tar.gz

---
+++ packages/potyk-fp/potyk-fp-0.1.0.tar.gz/potyk-fp-0.1.0/potyk_fp/pipe.py
@@ -0,0 +1,14 @@
+def pipe(*callables):
+    """
+    >>> sum_and_str = pipe(sum, str)
+    >>> sum_and_str([1, 2])
+    '3'
+    """
+
+    def _apply(val):
+        for callable_ in callables:
+            val = callable_(val)
+        else:
+            return val
+
+    return _apply
