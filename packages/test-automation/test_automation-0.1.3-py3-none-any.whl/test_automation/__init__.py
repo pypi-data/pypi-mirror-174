b895ab1892017f3554d97fab7246bd1bf803b366
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:56 +0000

    Add package test-automation/0.1.3/test_automation-0.1.3-py3-none-any.whl

---
+++ packages/test-automation/test_automation-0.1.3-py3-none-any.whl/test_automation/__init__.py
@@ -0,0 +1,15 @@
+from .api import *
+from .vault import *
+from .ui import *
+from .dashboard import *
+
+# API
+# VAULT
+# UI
+# DASHBOARD
+
+__all__ = ["login_user", "restaurant", "login_admin", "send_message_salck", "update_message_slack", "send_message_salck_fulfilment", "update_message_slack_fulfilment", "cart", "cart_submit", "login_vms", \
+           "get_path_value", "get_key_value", \
+           "get_test_list"
+           ]
+
