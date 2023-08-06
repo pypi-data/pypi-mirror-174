acf3fb5a63051bc9be1bdb618f934d8723098950
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:22 +0000

    Add package molbloom/0.1.3/molbloom-0.1.3.tar.gz

---
+++ packages/molbloom/molbloom-0.1.3.tar.gz/molbloom-0.1.3/molbloom/__init__.py
@@ -0,0 +1,63 @@
+from .version import __version__
+from molbloom.bloom import BloomFilter
+import os
+import molbloom.data
+from importlib_resources import files
+
+filters = {"zinc20": None, "instock": None}
+# just put in .cache
+DEFAULT_PATH = os.path.join(os.path.expanduser("~"), ".cache", "molbloom")
+filter_urls = {
+    "zinc20": "https://www.dropbox.com/s/mvn1ij9ooq5ikk9/zinc20.bloom?dl=1",
+    "instock": None,
+}
+
+
+def _download_progress(count, block_size, total_size):
+    percent = int(count * block_size * 100 / total_size)
+    print("\r", end="")
+    print("Downloading filter... {:d}%".format(percent), end="")
+    if percent >= 100:
+        print("")
+
+
+def _load_big_filter(name):
+    # check if it's present
+    filter_path = os.path.join(DEFAULT_PATH, f"{name}.bloom")
+    if not os.path.exists(DEFAULT_PATH):
+        os.makedirs(DEFAULT_PATH)
+        if not os.path.exists(filter_path):
+            print("Downloading filter...")
+            import urllib.request
+
+            urllib.request.urlretrieve(
+                filter_urls[name], filter_path, reporthook=_download_progress
+            )
+
+    filters[name] = BloomFilter(str(filter_path))
+
+
+def _load_filter(name):
+    global filters
+    if filters[name] is None:
+        if filter_urls[name] is None:
+            # should be in package
+            ap = files(molbloom.data).joinpath(f"{name}.bloom")
+            filters[name] = BloomFilter(str(ap))
+        else:
+            _load_big_filter(name)
+
+
+def buy(smiles, instock=True, canonicalize=False):
+    if instock:
+        name = "instock"
+    else:
+        name = "zinc20"
+    _load_filter(name)
+    if canonicalize:
+        try:
+            from rdkit import Chem
+        except ImportError:
+            raise ImportError("To canonicalize SMILES, rdkit is required.")
+        smiles = Chem.MolToSmiles(Chem.MolFromSmiles(smiles), canonical=True)
+    return smiles in filters[name]
---
+++ packages/molbloom/molbloom-0.1.3.tar.gz/molbloom-0.1.3/setup.py
@@ -0,0 +1,44 @@
+from gettext import install
+from setuptools import Extension, setup
+from Cython.Build import cythonize
+
+exec(open("molbloom/version.py").read())
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+
+setup(
+    name="molbloom",
+    ext_modules=cythonize(
+        [
+            Extension(
+                "molbloom.bloom",
+                ["molbloom/bloom.pyx"],
+                libraries=["m"],
+                include_dirs=["molbloom/bloom"],
+            )
+        ]
+    ),
+    include_dirs=["molbloom/bloom"],
+    version=__version__,
+    description="Purchaseable SMILES filter",
+    author="Andrew White",
+    author_email="andrew.white@rochester.edu",
+    url="https://whitead.github.io/molbloom/",
+    license="MIT",
+    package_data={"molbloom": ["data/*.bloom"]},
+    install_requires=["importlib_resources"],
+    setup_requires=["cython"],
+    packages=["molbloom"],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Scientific/Engineering :: Chemistry",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Typing :: Typed",
+    ],
+)
