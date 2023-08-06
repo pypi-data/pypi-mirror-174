8ce24ea0925ca80a3ce05aa4a90ac9a7c829bc6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:22:46 +0000

    Add package nfsysu-stock-upadate/0.0.1/nfsysu_stock_upadate-0.0.1-py3-none-any.whl

---
+++ packages/nfsysu-stock-upadate/nfsysu_stock_upadate-0.0.1-py3-none-any.whl/nfsysu_stock_upadate/data_update/detect.py
@@ -0,0 +1,13 @@
+﻿try:
+    import matlab
+    import matlab.engine
+except:
+    raise Exception(r"请先配置matlab使得python能够调用  \n  https://ng-fukgin.gitee.io/%E6%95%99%E7%A8%8B/2022/01/25/MATLAB_Engine_for_Python/")
+class Detect:
+    def __init__(self,filename):
+        self.filename=filename
+    def detect(self,nargout=2):
+        self.nargout=nargout
+        eng = matlab.engine.start_matlab()#可以为所欲为的调用matlab内置函数
+        return eng.detect(self.filename,nargout=self.nargout)
+    
