715257ff4342b1c65824879f777f0d14a043a819
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:20:33 +0000

    Add package py-simple-ttk/0.1.7/py_simple_ttk-0.1.7-py3-none-any.whl

---
+++ packages/py-simple-ttk/py_simple_ttk-0.1.7-py3-none-any.whl/py_simple_ttk/widgets/ButtonWidgets.py
@@ -0,0 +1,35 @@
+import tkinter as tk
+from tkinter import ttk
+from .Labeler import Labeler
+
+
+class LabeledButton(Labeler, ttk.Button):
+    """Labeled Button widget"""
+
+    def __init__(self, parent: ttk.Frame, labeltext: str, is_child: bool = False, **kw):
+        self.is_child = is_child
+        self.var = tk.StringVar()
+        self.default = None
+        if kw.get("text"):
+            self.var.set((text := kw.pop("text")))
+            if not kw.get("default"):
+                self.default = text
+        if kw.get("default"):
+            self.default = kw.pop("default")
+        if self.default is None:
+            self.default = ""
+        Labeler.__init__(self, parent, labeltext, header=not is_child, labelside="top")
+        ttk.Button.__init__(self, self.frame, textvariable=self.var)
+        ttk.Button.pack(self, fill="both", expand=True, side="left")
+
+    def set(self, val: str) -> None:
+        """Set button text"""
+        self.var.set(val)
+
+    def get(self) -> str:
+        """Get button text"""
+        return self.var.get()
+
+    def clear(self) -> None:
+        """Set button text to default"""
+        self.set(self.default)
