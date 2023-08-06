580bfa321b824908f62570049fe11ac6f9cc31d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:16 +0000

    Add package weconnect-cupra/0.48.3/weconnect_cupra-0.48.3-py3-none-any.whl

---
+++ packages/weconnect-cupra/weconnect_cupra-0.48.3-py3-none-any.whl/weconnect/api/cupra/domain.py
@@ -0,0 +1,16 @@
+from enum import Enum
+
+
+class Domain(Enum):
+    ALL = 'all'
+    ALL_CAPABLE = 'allCapable'
+    USER_CAPABILITIES = 'userCapabilities'
+    MEASUREMENTS = 'measurements'
+    SERVICES = 'services'
+    ENGINES = 'engines'
+
+    # Standard domains from VW api
+    CHARGING = 'charging'
+
+    def __str__(self):
+        return self.value
