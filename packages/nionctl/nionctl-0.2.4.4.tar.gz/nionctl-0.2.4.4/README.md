d84b7adff18b43fe0b71e58f11fc6531e721c5d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:16:24 +0000

    Add package nionctl/0.2.4.4/nionctl-0.2.4.4.tar.gz

---
+++ packages/nionctl/nionctl-0.2.4.4.tar.gz/nionctl-0.2.4.4/README.md
@@ -0,0 +1,31 @@
+
+# `nionctl`
+
+An abbreviation of common linux command-line utilities into one ctl
+
+
+## Installation
+
+Install `nionctl` with pip:
+
+```bash
+pip install nionctl
+```
+The PyPi page is availible [here.](https://pypi.org/project/nionctl/)
+
+
+## Current commands (WIP):
+
+- Basic WiFi controlling (uses `nmcli`)
+- Basic Bluetooth controlling (uses `bluetoothctl`)
+- `git clone` from url to default directory
+- Basic archiving utilities (supports `.zip`, `.tar`, `.tar.gz`, `.tar.bz2`)
+- Simple image editing (converting, rotating, resizing, negating etc)
+
+
+
+## Contributing
+
+Any kinds of contributions are very welcome!
+Currently the main goals are to bring more utilities and make the ctl cross-platform. Any tests on other linux distros are also welcome!
+
---
+++ packages/nionctl/nionctl-0.2.4.4.tar.gz/nionctl-0.2.4.4/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['nionctl', 'nionctl.lib']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['typer[all]>=0.6.1,<0.7.0']
+
+entry_points = \
+{'console_scripts': ['nionctl = nionctl.main:main_app']}
+
+setup_kwargs = {
+    'name': 'nionctl',
+    'version': '0.2.4.4',
+    'description': '',
+    'long_description': '\n# `nionctl`\n\nAn abbreviation of common linux command-line utilities into one ctl\n\n\n## Installation\n\nInstall `nionctl` with pip:\n\n```bash\npip install nionctl\n```\nThe PyPi page is availible [here.](https://pypi.org/project/nionctl/)\n\n\n## Current commands (WIP):\n\n- Basic WiFi controlling (uses `nmcli`)\n- Basic Bluetooth controlling (uses `bluetoothctl`)\n- `git clone` from url to default directory\n- Basic archiving utilities (supports `.zip`, `.tar`, `.tar.gz`, `.tar.bz2`)\n- Simple image editing (converting, rotating, resizing, negating etc)\n\n\n\n## Contributing\n\nAny kinds of contributions are very welcome!\nCurrently the main goals are to bring more utilities and make the ctl cross-platform. Any tests on other linux distros are also welcome!\n\n',
+    'author': '8Dion8',
+    'author_email': 'shvartserinfo@gmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
