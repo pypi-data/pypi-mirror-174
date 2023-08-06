ce7bacd329dabbffbcc8dbcc50fab509c0158ae6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:17:07 +0000

    Add package aspcmds/1.2/aspcmds-1.2.tar.gz

---
+++ packages/aspcmds/aspcmds-1.2.tar.gz/aspcmds-1.2/setup - Copy.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+ 
+classifiers = [
+  'Development Status :: 5 - Production/Stable',
+  'Intended Audience :: Education',
+  'Operating System :: Microsoft :: Windows :: Windows 10',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3'
+]
+ 
+setup(
+  name='aspcmds',
+  version='0.0.1',
+  description='A simple new added commands.',
+  long_description = long_description,
+  long_description_content_type='text/markdown',
+  url='',  
+  author='ImpairedAsp5640',
+  author_email='gamingprofile61@gmail.com',
+  license='MIT', 
+  classifiers=classifiers,
+  keywords='commands', 
+  packages=find_packages(),
+  install_requires=[''] 
+)
