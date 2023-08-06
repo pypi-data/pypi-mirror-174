58a83bba5f1044797aa8cda72dcaf3c1fb865743
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:56:37 +0000

    Add package starfit/0.9.2/starfit-0.9.2.tar.gz

---
+++ packages/starfit/starfit-0.9.2.tar.gz/starfit-0.9.2/setup.py
@@ -0,0 +1,43 @@
+import os
+
+from numpy.distutils.core import Extension, setup
+
+flags = ["-fPIC", "-Ofast", "-g", "-funroll-loops", "-fno-second-underscore", "-w"]
+
+cwd = os.getcwd()
+starfit_src = os.path.join(cwd, "src/starfit")
+
+
+fit_sources = [
+    os.path.join(starfit_src, "fitness", x)
+    for x in [
+        "fitness.pyf",
+        "type_def.f90",
+        "powell.f90",
+        "norm.f90",
+        "mleqs.f90",
+        "star_data.f90",
+        "abu_data.f90",
+        "solver.f90",
+    ]
+]
+# to skip fitness.pyf if need be
+fit_sources = [x for x in fit_sources if os.path.isfile(x)]
+
+module_solver = Extension(
+    "starfit.fitness._solver",
+    sources=fit_sources,
+    extra_f90_compile_args=flags,
+    f2py_options=[
+        "--f2cmap",
+        os.path.join(starfit_src, "fitness", ".f2py_f2cmap"),
+    ],
+)
+
+module_solgen = Extension(
+    "starfit.solgen._solgen",
+    sources=[os.path.join(starfit_src, "solgen/solgen.f90")],
+    extra_f90_compile_args=flags,
+)
+
+setup(ext_modules=[module_solver, module_solgen])
