80bd24936d1e4dff8808db21f871e542b0961a3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:56:35 +0000

    Add package wagtailmedia/0.12.0/wagtailmedia-0.12.0.tar.gz

---
+++ packages/wagtailmedia/wagtailmedia-0.12.0.tar.gz/wagtailmedia-0.12.0/src/wagtailmedia/apps.py
@@ -0,0 +1,28 @@
+from django.apps import AppConfig
+from django.db.models import ForeignKey
+
+
+class WagtailMediaAppConfig(AppConfig):
+    default_auto_field = "django.db.models.AutoField"
+    name = "wagtailmedia"
+    label = "wagtailmedia"
+    verbose_name = "Wagtail media"
+
+    def ready(self):
+        from wagtail import VERSION as WAGTAIL_VERSION
+
+        from .signal_handlers import register_signal_handlers
+
+        register_signal_handlers()
+
+        if WAGTAIL_VERSION >= (3, 0):
+            from wagtail.admin.compare import register_comparison_class
+
+            from .edit_handlers import MediaFieldComparison
+            from .models import get_media_model
+
+            # Set up image ForeignKeys to use ImageFieldComparison as the comparison class
+            # when comparing page revisions
+            register_comparison_class(
+                ForeignKey, to=get_media_model(), comparison_class=MediaFieldComparison
+            )
