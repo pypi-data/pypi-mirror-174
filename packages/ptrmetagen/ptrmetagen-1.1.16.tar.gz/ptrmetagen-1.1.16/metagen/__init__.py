c032f97acd34f1b1c05cefc2b08e5a8ba0d230b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:25 +0000

    Add package ptrmetagen/1.1.16/ptrmetagen-1.1.16.tar.gz

---
+++ packages/ptrmetagen/ptrmetagen-1.1.16.tar.gz/ptrmetagen-1.1.16/metagen/__init__.py
@@ -0,0 +1,5 @@
+from metagen.main import PTRMetagen
+from metagen.metadata import Application, Configuration, Scope, Place, Period, LayerTemplate, Attribute, SpatialWMS, \
+    RelationSpatial, SpatialAttribute, SpatialWMTS, SpatialVector, Style, Case, RelationAttribute, Tag
+
+__version__ = '1.1.16'
