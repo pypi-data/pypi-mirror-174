51316a525a9c048e6a519c8009cf5edb6eb8599c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:49 +0000

    Add package saas-co/1.2/saas_co-1.2.tar.gz

---
+++ packages/saas-co/saas_co-1.2.tar.gz/saas_co-1.2/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='saas_co',
+    version='1.2',
+    license='MIT',
+    author="David Schwartz",
+    author_email='david.schwartz@devfactory.com',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    url='https://github.com/gmyrianthous/example-publish-pypi',
+    keywords='cost optimization cur',
+    install_requires=[
+          'boto3',
+          'botocore',
+          'pandas',
+          'awswrangler',
+          'jsonpath-ng'
+      ],
+)
