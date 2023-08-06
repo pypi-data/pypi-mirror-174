28f20fde3ecfa40f3a2f7ab077f51ef1e1790c1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:06:26 +0000

    Add package math2eb/0.1.0/math2eb-0.1.0.tar.gz

---
+++ packages/math2eb/math2eb-0.1.0.tar.gz/math2eb-0.1.0/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['math2eb']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'math2eb',
+    'version': '0.1.0',
+    'description': 'A better math module for python',
+    'long_description': None,
+    'author': 'O.Plimer',
+    'author_email': None,
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
