21969bea1981808b749f75426ace3597fe70998e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:44 +0000

    Add package wyjtools/0.0.1/wyjtools-0.0.1.tar.gz

---
+++ packages/wyjtools/wyjtools-0.0.1.tar.gz/wyjtools-0.0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='wyjtools',
+    version='0.0.1',
+    keywords='tools',
+    description='a library to grap all the useful tools',
+    license='MIT License',
+    author='Yujue Wang',
+    author_email='spsc83@gmail.com',
+    packages=find_packages(),
+    include_package_data=True,
+    platforms='any',
+    install_requires=[],
+)
