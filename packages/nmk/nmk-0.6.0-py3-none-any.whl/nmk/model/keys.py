8a88507f287c3024be0804f7ff6edf5e8cd9ff8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:36 +0000

    Add package nmk/0.6.0/nmk-0.6.0-py3-none-any.whl

---
+++ packages/nmk/nmk-0.6.0-py3-none-any.whl/nmk/model/keys.py
@@ -0,0 +1,8 @@
+class NmkRootConfig:
+    PYTHON_PATH = "pythonPath"
+    BASE_DIR = "BASEDIR"
+    ROOT_DIR = "ROOTDIR"
+    CACHE_DIR = "CACHEDIR"
+    PROJECT_DIR = "PROJECTDIR"
+    PROJECT_FILES = "PROJECTFILES"
+    ENV = "ENV"
