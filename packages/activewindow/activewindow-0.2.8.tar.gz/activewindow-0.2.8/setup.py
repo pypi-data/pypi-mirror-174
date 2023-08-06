46617adfe55141b66cd6293cb485846f0437cc82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:07 +0000

    Add package activewindow/0.2.8/activewindow-0.2.8.tar.gz

---
+++ packages/activewindow/activewindow-0.2.8.tar.gz/activewindow-0.2.8/setup.py
@@ -0,0 +1,33 @@
+# import distutils.util
+from setuptools import setup
+# print(['<sys>', '<psutil>', ['win32gui'] if platform == 'win32' else ['<re>', '<subprocess>']])
+# print(distutils.util.get_platform())
+setup(name='activewindow',
+      version='0.2.8',
+      author='Sonter',
+      author_email='sonterkub@gmail.com',
+      description='Active window information',
+      long_description='Gives you information about active window in Windows / X11',
+      # packages=['activewindow'],
+      py_modules=['sys'],
+      install_requires=['setuptools',
+                        'wheel',
+                        # 'sys',
+                        'psutil',
+                        "ctypes;platform_system=='Windows'",
+                        "ctypes;platform_system=='win-amd64'",
+                        # "win32gui;platform_system=='Windows'",
+                        # "win32gui;platform_system=='win-amd64'",
+                        "pywin32;platform_system=='Windows'",
+                        "pywin32;platform_system=='win-amd64'",
+                        "re;platform_system=='linux-x86_64'",
+                        "subprocess;platform_system=='linux-x86_64'"],
+      keywords=['active window', 'activewindow', 'x11'],
+      zip_safe=False,
+      platforms=['linux-x86_64', 'win-amd64'],
+      classifiers=[
+            "Programming Language :: Python :: 3.10",
+            "Operating System :: Microsoft :: Windows",
+            "Operating System :: POSIX :: Linux"
+      ])
+# py_modules=['win32gui', 'psutil', 'ctypes'],
