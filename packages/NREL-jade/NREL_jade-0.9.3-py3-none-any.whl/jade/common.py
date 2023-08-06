8d12ac972395052a8f2c5902f5dc2f3921109d08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:19 +0000

    Add package NREL-jade/0.9.3/NREL_jade-0.9.3-py3-none-any.whl

---
+++ packages/NREL-jade/NREL_jade-0.9.3-py3-none-any.whl/jade/common.py
@@ -0,0 +1,18 @@
+"""Common definitions for the jade package."""
+
+import os
+
+
+OUTPUT_DIR = "output"
+EVENTS_DIR = "events"
+JOBS_OUTPUT_DIR = "job-outputs"
+JOBS_STDIO_DIR = "job-stdio"
+RESULTS_DIR = "results"
+SCRIPTS_DIR = "scripts"
+STATS_DIR = "stats"
+CONFIG_FILE = "config.json"
+RESULTS_FILE = "results.json"
+ANALYSIS_DIR = "analysis"
+POST_PROCESSING_CONFIG_FILE = "post-config.json"
+HPC_CONFIG_FILE = "hpc_config.toml"
+DEFAULT_SUBMISSION_GROUP = "default"
