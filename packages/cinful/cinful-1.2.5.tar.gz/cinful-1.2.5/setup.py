09d304d5d29eeee3ee1b9740f26f9b12065010be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:58:59 +0000

    Add package cinful/1.2.5/cinful-1.2.5.tar.gz

---
+++ packages/cinful/cinful-1.2.5.tar.gz/cinful-1.2.5/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+import os
+
+this_directory = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(this_directory, 'README.md')) as f:
+    long_description = f.read()
+
+
+
+console_scripts = '''
+    [console_scripts]
+    cinful = cinful.cinful:main
+    cinful_init = cinful.cinful_initialize:install
+'''
+
+setup(
+    name= 'cinful',
+    version= '1.2.5',
+    author= 'Aaron Feller',
+    author_email = 'aaronleefeller@gmail.com',
+    include_package_data = True,
+    url='https://github.com/wilkelab/cinful',
+    description=('cinful: A fully automated pipeline to identify microcins'
+                 'with associated immunity proteins and export machinery'),
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    license='GPL-3',
+    py_modules=['cinful', 'cinful_init'],
+    entry_points=console_scripts,
+    packages=['cinful'],
+)
