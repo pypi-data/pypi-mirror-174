5f93e11c548ff4e6b769f7a7b514c54248f9ce58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:12:49 +0000

    Add package InsurAutoML/0.2.2.11/InsurAutoML-0.2.2.11-cp39-cp39-manylinux_2_5_x86_64.manylinux1_x86_64.manylinux_2_24_x86_64.whl

---
+++ packages/InsurAutoML/InsurAutoML-0.2.2.11-cp39-cp39-manylinux_2_5_x86_64.manylinux1_x86_64.manylinux_2_24_x86_64.whl/InsurAutoML/_model/_ML.py
@@ -0,0 +1,42 @@
+"""
+File: _ML.py
+Author: Panyi Dong
+GitHub: https://github.com/PanyiDong/
+Mathematics Department, University of Illinois at Urbana-Champaign (UIUC)
+
+Project: My_AutoML
+Latest Version: 0.2.0
+Relative Path: /My_AutoML/_model/_ML.py
+File Created: Wednesday, 13th April 2022 12:08:45 am
+Author: Panyi Dong (panyid2@illinois.edu)
+
+-----
+Last Modified: Friday, 15th April 2022 12:20:28 am
+Modified By: Panyi Dong (panyid2@illinois.edu)
+
+-----
+MIT License
+
+Copyright (c) 2022 - 2022, Panyi Dong
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+
+   
