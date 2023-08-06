d3c539bc3d7af5e49018a1a5971f3028c7113bca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:05 +0000

    Add package rmg-numerical-integration/2.3/rmg_numerical_integration-2.3-py3-none-any.whl

---
+++ packages/rmg-numerical-integration/rmg_numerical_integration-2.3-py3-none-any.whl/rmg_numerical_integration/__init__.py
@@ -0,0 +1,11 @@
+__all__ = ["midpoint", "trapezoidal", "simpson", "gaussian_quadrature"]
+
+#from .midpoint import midpoint_rule, plot_midpoint_rule
+#from .trapezoidal import trapezoidal_rule, plot_trapezoidal_rule
+#from .simpson import simpson_rule, plot_simpson_rule
+#from .gaussian_quadrature import gauss_rule, plot_gauss_quadrature
+
+import midpoint
+import trapezoidal
+import simpson
+import gaussian_quadrature
