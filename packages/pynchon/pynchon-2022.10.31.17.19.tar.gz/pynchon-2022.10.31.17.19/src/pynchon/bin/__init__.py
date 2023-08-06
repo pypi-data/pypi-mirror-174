c353a44af14d00b8740a061f321538f5a6d19ab6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:58:54 +0000

    Add package pynchon/2022.10.31.17.19/pynchon-2022.10.31.17.19.tar.gz

---
+++ packages/pynchon/pynchon-2022.10.31.17.19.tar.gz/pynchon-2022.10.31.17.19/src/pynchon/bin/__init__.py
@@ -0,0 +1,21 @@
+"""
+"""
+# import os
+# import sys
+# import importlib
+# import inspect
+# import json
+#
+# import jinja2
+# import click
+#
+# import pynchon
+# from pynchon import (annotate, util)
+# LOGGER = pynchon.get_logger(__name__)
+#
+# from .common import *
+# from .groups import entry
+# from .groups import project,gen,gen_api,gen_cli,ast
+
+from pynchon.bin import (api, cli, project)
+from pynchon.bin.groups import entry
