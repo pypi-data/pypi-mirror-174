6807b3fcc66186ba9e19b96a2fa3414077918d15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:23:58 +0000

    Add package trello-api-wrapper/0.0.12/trello_api_wrapper-0.0.12.tar.gz

---
+++ packages/trello-api-wrapper/trello_api_wrapper-0.0.12.tar.gz/trello_api_wrapper-0.0.12/trello_api_wrapper/__init__.py
@@ -0,0 +1,25 @@
+from typing import Type
+
+from board import Board
+from card import Card
+from card_list import CardList
+from constructors import has_board, has_card, has_list
+from user import User
+
+
+class Trello(User):
+    def user(self) -> Type[User]:
+        """Instantiate a new Trello User."""
+        return User(self.apikey, self.token)
+
+    def board(self, board_id: str) -> Type[Board]:
+        """Get by ID a new instance of a Board the User has."""
+        return Board(self, board_id) if has_board(self, board_id) else None
+
+    def card_list(self, list_id: str) -> Type[CardList]:
+        """Get by ID a new instance of a CardList the Board has."""
+        return CardList(self, list_id) if has_list(self, list_id) else None
+
+    def card(self, card_id: str) -> Type[Card]:
+        """Get by ID a new instance of a Card the CardList has."""
+        return Card(self, card_id) if has_card(self, card_id) else None
---
+++ packages/trello-api-wrapper/trello_api_wrapper-0.0.12.tar.gz/trello_api_wrapper-0.0.12/trello_api_wrapper/card_list.py
@@ -0,0 +1,94 @@
+import json
+from typing import Dict, List, Type
+
+import requests
+
+from ..base_class import BaseClass
+from ..constructors import has_card
+from ..trello_requests import get_request, was_successful
+
+CREATE_CARD_URL = "https://api.trello.com/1/cards"
+
+
+class CardList(BaseClass):
+    """List class definition. It holds and interacts with the Cards."""
+
+    def __init__(self, trello: Type[BaseClass], list_id: str) -> None:
+        super().__init__(trello.apikey, trello.token)
+        self.id = list_id
+        temp_list = fetch_data(self)
+        self.name = temp_list["name"]
+        self.closed = temp_list["closed"]
+        self.__cards = []
+        self.__cards = fetch_cards(self)
+
+    @property
+    def cards(self) -> List[str]:
+        return self.__cards
+
+    def create_card(self, title, description, label_ids):
+        """Create a new Trello card"""
+        params = {
+            "name": title,
+            "desc": description,
+            "pos": "top",
+            "idList": self.id,
+            "key": super().apikey,
+            "token": super().token,
+            "idLabels": label_ids,
+        }
+        response = requests.request("POST", CREATE_CARD_URL, params=params)
+        card_id = response.json()["id"] if response.ok else None
+        return card_id, response.status_code
+
+    def __str__(self) -> str:
+        card_list = {
+            "id": self.id,
+            "name": self.name,
+            "closed": self.closed,
+            "cards": self.cards,
+        }
+        return json.dumps(card_list)
+
+
+def fetch_data(card_list) -> Dict[str, str]:
+    """Loads CardList information."""
+    url = f"https://api.trello.com/1/lists/{card_list.id}"
+    response = get_request(card_list, url)
+    if was_successful(response):
+        temp_list = {
+            "name": response["data"]["name"],
+            "closed": response["data"]["closed"],
+        }
+    else:
+        temp_list = {"name": None, "closed": None}
+    return temp_list
+
+
+def add_card(card_list, card: Dict[str, str]) -> List[str]:
+    """Add a new Card to the List."""
+    should_add = True
+    if len(card_list.cards) != 0 and has_card(card_list, card["id"]):
+        for stored_card in card_list.cards:
+            if stored_card["id"] == card["id"]:
+                should_add = False
+    if should_add:
+        card_list.cards.append(
+            {
+                "id": card["id"],
+                "name": card["name"],
+                "closed": card["closed"],
+            }
+        )
+    return card_list.cards
+
+
+def fetch_cards(card_list) -> Dict[str, str]:
+    """Requests all Cards the current CardList has from Trello API."""
+    url = f"https://api.trello.com/1/lists/{card_list.id}/cards"
+    response = get_request(card_list, url)
+    cards = []
+    if was_successful(response):
+        for card in response["data"]:
+            cards = add_card(card_list, card)
+    return cards
