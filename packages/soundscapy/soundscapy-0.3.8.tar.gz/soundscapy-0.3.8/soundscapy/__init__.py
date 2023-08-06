79b0325418d898924bf080b99b7345c154a2d48c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:52 +0000

    Add package soundscapy/0.3.8/soundscapy-0.3.8.tar.gz

---
+++ packages/soundscapy/soundscapy-0.3.8.tar.gz/soundscapy-0.3.8/soundscapy/__init__.py
@@ -0,0 +1,11 @@
+"""
+Soundscapy
+==========
+
+Soundscapy is a Python library for soundscape analysis and visualisation.
+"""
+
+from soundscapy.analysis._AnalysisSettings import AnalysisSettings
+from soundscapy.analysis._Binaural import Binaural
+
+from soundscapy.plotting.circumplex import scatter, density, jointplot
