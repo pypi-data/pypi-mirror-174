28361f6898ed5c97489b8f9c1874e098e064733d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:19 +0000

    Add package pglistener/0.1.0/pglistener-0.1.0-py3-none-any.whl

---
+++ packages/pglistener/pglistener-0.1.0-py3-none-any.whl/pglistener/__init__.py
@@ -0,0 +1,32 @@
+import os
+import ctypes
+import time
+import threading
+import asyncio
+
+
+def call_hello(golib, w):
+    golib.Hello(w)
+
+
+async def read_hello(r):
+    loop = asyncio.get_event_loop()
+    reader = asyncio.StreamReader(loop=loop)
+    protocol = asyncio.StreamReaderProtocol(reader)
+    transport, _ = await loop.connect_read_pipe(lambda: protocol, r)
+    while True:
+        out = await reader.read(16)
+        print("=== from go:", out)
+
+
+def main():
+    r, w = os.pipe2(os.O_NONBLOCK | os.O_CLOEXEC)
+    golib = ctypes.cdll.LoadLibrary('../gomodule/libgomodule.so')
+    t = threading.Thread(target=call_hello, args=[golib, w])
+    t.setDaemon(True)
+    t.start()
+    asyncio.get_event_loop().run_until_complete(read_hello(os.fdopen(r)))
+
+
+if __name__ == '__main__':
+    main()
