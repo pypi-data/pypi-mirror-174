37104570144762cffb3a5e1a02f2957ba3ba0645
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:23:53 +0000

    Add package bagbag/0.45.5/bagbag-0.45.5.tar.gz

---
+++ packages/bagbag/bagbag-0.45.5.tar.gz/bagbag-0.45.5/src/bagbag/__init__.py
@@ -0,0 +1,34 @@
+import ipdb
+import traceback
+import sys
+import selenium
+
+from . import Lg
+from . import Tools
+from . import Time
+from . import Base64
+from . import Json
+from . import Os
+from . import Funcs
+from . import Re
+from . import Hash
+from . import Http
+from . import Socket 
+from . import Random
+from . import Math
+
+from . import Cryptoo as Crypto
+
+from .File import File
+from .Thread import Thread
+from .Process import Process
+from .Python import Range
+from .String import String
+
+# import re
+# import forbiddenfruit
+
+# def __hasChinese(self) -> bool:
+#     return len(re.findall(r'[\u4e00-\u9fff]+', self)) != 0
+
+# forbiddenfruit.curse(str, "HasChinese", __hasChinese)
