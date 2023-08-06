d4462e86c775aeae8718b6c7da86c6a0fe5bf5ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:17:23 +0000

    Add package PyGine/0.0.3/PyGine-0.0.3-py3-none-any.whl

---
+++ packages/PyGine/PyGine-0.0.3-py3-none-any.whl/PyGine/SpriteComponent.py
@@ -0,0 +1,20 @@
+from PyGine.Component import Component
+
+
+class SpriteComponent(Component) :
+    def __init__(self,parent, sprite=" ") :
+        super().__init__(parent)
+        self.sprite = sprite
+        self.parent = parent
+
+    def start(self):
+        pass
+
+    def update(self,dt) :
+        pass
+
+    def getSprite(self) :
+        return self.sprite
+
+    def setSprite(self, sprite) :
+        self.sprite = sprite
