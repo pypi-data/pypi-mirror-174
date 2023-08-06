1f894dfc8be5ab98911539aeac087c7fb1bb8f14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:27 +0000

    Add package solas-ai/0.2.3/solas_ai-0.2.3-cp38-cp38-win_amd64.whl

---
+++ packages/solas-ai/solas_ai-0.2.3-cp38-cp38-win_amd64.whl/solas_disparity/types/_residual_standardized_mean_difference_column_type.pyi
@@ -0,0 +1,11 @@
+from ._enum_base import EnumBase as EnumBase
+from _typeshed import Incomplete
+from solas_disparity import const as const
+
+class ResidualStandardizedMeanDifferenceColumnType(EnumBase):
+    TOTAL: Incomplete
+    AVERAGE_PREDICTION: Incomplete
+    AVERAGE_LABEL: Incomplete
+    AVERAGE_RESIDUAL: Incomplete
+    STANDARD_DEVIATION_OF_RESIDUALS: Incomplete
+    RESIDUAL_SMD: Incomplete
