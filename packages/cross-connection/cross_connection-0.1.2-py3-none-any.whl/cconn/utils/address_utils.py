28c154c442de5b33ab0a12c648a977807e6a05fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:10:54 +0000

    Add package cross-connection/0.1.2/cross_connection-0.1.2-py3-none-any.whl

---
+++ packages/cross-connection/cross_connection-0.1.2-py3-none-any.whl/cconn/utils/address_utils.py
@@ -0,0 +1,25 @@
+# coding: utf-8
+
+import ipaddress
+import socket
+
+
+class AddressUtils:
+    @staticmethod
+    def hostname() -> str:
+        return socket.gethostname()
+
+    @staticmethod
+    def host_address() -> str:
+        try:
+            socket.gethostbyname(AddressUtils.hostname())
+        except Exception:
+            return ''
+
+    @staticmethod
+    def ipv4_str_to_int(ipv4: str) -> int:
+        return int(ipaddress.IPv4Address(ipv4))
+
+    @staticmethod
+    def ipv4_int_to_str(ipv4: int) -> str:
+        return str(ipaddress.IPv4Address(ipv4))
