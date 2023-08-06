e36b26e93ad1fe701f99afe79754fa5ac97a0954
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:21:36 +0000

    Add package useckit/0.2a2/useckit-0.2a2.tar.gz

---
+++ packages/useckit/useckit-0.2a2.tar.gz/useckit-0.2a2/setup.py
@@ -0,0 +1,38 @@
+import setuptools
+
+
+def parse_requirements(filename):
+    """ load requirements from a pip requirements file """
+    lineiter = (line.strip() for line in open(filename))
+    return [line for line in lineiter if line and not line.startswith("#")]
+
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="useckit",
+    version="0.2.a2",
+    author="Jonathan Liebers",
+    author_email="git@jo2k.de",
+    description="",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://example.org",
+    #packages=[
+    #    'useckit',
+    #    'useckit.models',
+    #    'useckit.utils',
+    #    'useckit.paradigms'
+    #],
+    package_dir = {'': 'useckit', 'useckit': 'useckit'},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+    install_requires=parse_requirements('requirements.txt')
+)
+
+# TODO twine not needed as requirement for user
