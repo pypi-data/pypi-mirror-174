ca3df55a5367374697574d57fafea99d2b04100b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:56:52 +0000

    Add package Resiliparse/0.13.6/Resiliparse-0.13.6-cp38-cp38-win_amd64.whl

---
+++ packages/Resiliparse/Resiliparse-0.13.6-cp38-cp38-win_amd64.whl/resiliparse/__init__.py
@@ -0,0 +1,34 @@
+
+
+""""""# start delvewheel patch
+def _delvewheel_init_patch_1_1_0():
+    import os
+    import sys
+    libs_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, 'Resiliparse.libs'))
+    if sys.version_info[:2] >= (3, 8) and not os.path.exists(os.path.join(sys.base_prefix, 'conda-meta')) or sys.version_info[:2] >= (3, 10):
+        os.add_dll_directory(libs_dir)
+    else:
+        from ctypes import WinDLL
+        with open(os.path.join(libs_dir, '.load-order-Resiliparse-0.13.6')) as file:
+            load_order = file.read().split()
+        for lib in load_order:
+            WinDLL(os.path.join(libs_dir, lib))
+
+
+_delvewheel_init_patch_1_1_0()
+del _delvewheel_init_patch_1_1_0
+# end delvewheel patch
+
+# Copyright 2021 Janek Bevendorff
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
