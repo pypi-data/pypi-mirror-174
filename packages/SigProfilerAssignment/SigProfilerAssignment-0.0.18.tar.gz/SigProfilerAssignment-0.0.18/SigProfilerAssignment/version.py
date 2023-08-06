c14733db6301e966d41796d63abb90b56ae5ee4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:22:35 +0000

    Add package SigProfilerAssignment/0.0.18/SigProfilerAssignment-0.0.18.tar.gz

---
+++ packages/SigProfilerAssignment/SigProfilerAssignment-0.0.18.tar.gz/SigProfilerAssignment-0.0.18/SigProfilerAssignment/version.py
@@ -0,0 +1,7 @@
+
+# THIS FILE IS GENERATED FROM SigProfilerAssignment SETUP.PY
+short_version = '0.0.18'
+version = '0.0.18'
+Update = '(1) Import decomposition plots from SPA (2) Resolve issue with m_for_subgroups (3) Expand tests'
+    
+    
