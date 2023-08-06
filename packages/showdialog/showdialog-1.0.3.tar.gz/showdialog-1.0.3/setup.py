9b8a743ba92cd3f9e897e40ad84761ea0334bef1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:22 +0000

    Add package showdialog/1.0.3/showdialog-1.0.3.tar.gz

---
+++ packages/showdialog/showdialog-1.0.3.tar.gz/showdialog-1.0.3/setup.py
@@ -0,0 +1,26 @@
+import setuptools  # type: ignore
+
+with open('README.md', 'rt') as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name='showdialog',
+    version='1.0.3',
+    author='Chechkenev Andrey (@DarkCat09)',
+    author_email='aacd0709@mail.ru',
+    description='Simple module for showing GTK dialog',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Environment :: X11 Applications :: GTK'
+    ],
+    install_requires=[
+        'PyGObject>=3.42.2'
+    ],
+    packages=['showdialog'],
+    python_requires=">=3.6",
+)
