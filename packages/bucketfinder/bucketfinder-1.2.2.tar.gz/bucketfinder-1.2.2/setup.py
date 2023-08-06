85009f8bef0940a4ff3fee8cf0f13bd937ce04ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:47:38 +0000

    Add package bucketfinder/1.2.2/bucketfinder-1.2.2.tar.gz

---
+++ packages/bucketfinder/bucketfinder-1.2.2.tar.gz/bucketfinder-1.2.2/setup.py
@@ -0,0 +1,26 @@
+import glob
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+binfiles = glob.glob('bin/*')
+
+setuptools.setup(
+    name='bucketfinder',
+    version='1.2.2',
+    scripts = binfiles,
+    author='phx',
+    author_email='phx@example.com',
+    description='working version of bucket_finder.rb ruby script',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/phx/bucketfinder',
+    classifiers=[
+         "Programming Language :: Ruby",
+         "License :: OSI Approved :: MIT License",
+    ],
+    package_dir={"": "."},
+    packages=setuptools.find_packages(where="."),
+    python_requires=">=3.6",
+)
