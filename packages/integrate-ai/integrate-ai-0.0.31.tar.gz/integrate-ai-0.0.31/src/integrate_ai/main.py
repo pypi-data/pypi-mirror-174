a25acd11f3e634aa32d8c1b6f47c8009413f3e42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:06:42 +0000

    Add package integrate-ai/0.0.31/integrate-ai-0.0.31.tar.gz

---
+++ packages/integrate-ai/integrate-ai-0.0.31.tar.gz/integrate-ai-0.0.31/src/integrate_ai/main.py
@@ -0,0 +1,45 @@
+"""Module containing top-level Typer app functionality."""
+
+import subprocess
+import typer
+import integrate_ai.sdk as sdk
+import integrate_ai.client as client
+from rich import print as rprint
+
+app = typer.Typer(no_args_is_help=True)
+
+app.add_typer(sdk.app, name="sdk")
+app.add_typer(client.app, name="client")
+
+package = "integrate-ai"
+
+
+@app.callback()
+def docs(
+    no_prompt: bool = typer.Option(False, "--no-prompt", help="Disable prompts. Confirmations will default to `Y`.")
+):
+    """
+    The CLI interface to manage integrate.ai operations.
+
+    Your IAI token will be required in many commmands. For convenience, you can set this in the `IAI_TOKEN` environment variable to avoid repeated prompts.
+    """
+    pass
+
+
+@app.command()
+def version():
+    """
+    The currently installed version of the cli.
+    """
+    show_version_command = f"pip show {package} | grep Version"
+    rprint(f"Getting current version for {package}.")
+    subprocess.run(show_version_command, check=True, shell=True)
+
+
+def main():
+
+    app()
+
+
+if __name__ == "__main__":
+    main()  # pragma: no cover
