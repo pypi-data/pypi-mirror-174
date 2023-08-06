f9a8268224f251039154347aab407bfffc66293b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:20 +0000

    Add package python-snippets/0.0.6/python-snippets-0.0.6.tar.gz

---
+++ packages/python-snippets/python-snippets-0.0.6.tar.gz/python-snippets-0.0.6/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+-------------------------------------------------
+   File Name：     setup
+   Description :
+   Author :       chenhao
+   date：          2021/4/6
+-------------------------------------------------
+   Change Activity:
+                   2021/4/6:
+-------------------------------------------------
+"""
+from setuptools import setup, find_packages
+
+REQ = [
+    "tqdm",
+    "pydantic"
+]
+
+setup(
+    name='python-snippets',
+    version='0.0.6',
+    install_requires=REQ,
+    packages=find_packages(exclude=['tests*']),
+    package_dir={"": "."},
+    package_data={},
+    url='https://github.com/jerrychen1990/python-snippets.git',
+    license='MIT',
+    author='Chen Hao',
+    author_email='jerrychen1990@gmail.com',
+    zip_safe=True,
+    description='useful python snippets',
+    long_description="useful python snippets"
+)
