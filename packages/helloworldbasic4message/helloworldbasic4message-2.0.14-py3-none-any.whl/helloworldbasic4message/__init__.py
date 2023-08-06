b1ae89d7d8091327e91fe72abc6f10b8fabc3268
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:52 +0000

    Add package helloworldbasic4message/2.0.14/helloworldbasic4message-2.0.14-py3-none-any.whl

---
+++ packages/helloworldbasic4message/helloworldbasic4message-2.0.14-py3-none-any.whl/helloworldbasic4message/__init__.py
@@ -0,0 +1,16 @@
+
+from utils import *
+
+
+def sayhello1b(name):
+    # want to return utility say hello
+    return "in progress"
+
+
+
+print("hello main init  c")
+
+
+
+
+
---
+++ packages/helloworldbasic4message/helloworldbasic4message-2.0.14-py3-none-any.whl/helloworldbasic4message/utility2.py
@@ -0,0 +1,6 @@
+
+
+def utilB_saygoodbye(name):
+    return "version Nov 2, 2022 11:58am: goodbye "+name;
+
+
