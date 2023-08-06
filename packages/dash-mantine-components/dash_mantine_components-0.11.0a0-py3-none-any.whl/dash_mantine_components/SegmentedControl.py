16bbb0046761a72a989feccb7454e05db044573c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:47 +0000

    Add package dash-mantine-components/0.11.0a0/dash_mantine_components-0.11.0a0-py3-none-any.whl

---
+++ packages/dash-mantine-components/dash_mantine_components-0.11.0a0-py3-none-any.whl/dash_mantine_components/SegmentedControl.py
@@ -0,0 +1,125 @@
+# AUTO GENERATED FILE - DO NOT EDIT
+
+from dash.development.base_component import Component, _explicitize_args
+
+
+class SegmentedControl(Component):
+    """A SegmentedControl component.
+Horizontal control made of multiple segments, alternative to RadioGroup. For more information, see: https://mantine.dev/core/segmented-control/
+
+Keyword arguments:
+
+- id (string; optional):
+    Unique ID to identify this component in Dash callbacks.
+
+- className (string; optional):
+    Often used with CSS to style elements with common properties.
+
+- color (boolean | number | string | dict | list; optional):
+    Active control color from theme.colors, defaults to white in light
+    color scheme and theme.colors.dark[9] in dark.
+
+- data (list of strings; required):
+    Data based on which controls are rendered.
+
+- disabled (boolean; optional):
+    Disabled input state.
+
+- fullWidth (boolean; optional):
+    True if component should have 100% width.
+
+- m (number; optional):
+    margin props.
+
+- mb (number; optional):
+    margin props.
+
+- ml (number; optional):
+    margin props.
+
+- mr (number; optional):
+    margin props.
+
+- mt (number; optional):
+    margin props.
+
+- mx (number; optional):
+    margin props.
+
+- my (number; optional):
+    margin props.
+
+- orientation (a value equal to: 'horizontal', 'vertical'; optional):
+    Display Vertically.
+
+- p (number; optional):
+    padding props.
+
+- pb (number; optional):
+    padding props.
+
+- pl (number; optional):
+    padding props.
+
+- pr (number; optional):
+    padding props.
+
+- pt (number; optional):
+    padding props.
+
+- px (number; optional):
+    padding props.
+
+- py (number; optional):
+    padding props.
+
+- radius (number; optional):
+    Border-radius from theme or number to set border-radius in px.
+
+- size (a value equal to: 'xs', 'sm', 'md', 'lg', 'xl'; optional):
+    Controls font-size, paddings and height.
+
+- style (boolean | number | string | dict | list; optional):
+    Inline style.
+
+- styles (dict; optional):
+    Mantine styles API.
+
+- sx (boolean | number | string | dict | list; optional):
+    With sx you can add styles to component root element. If you need
+    to customize styles of other elements within component use styles
+    prop.
+
+- transitionDuration (number; optional):
+    Transition duration in ms, set to 0 to turn off transitions.
+
+- transitionTimingFunction (string; optional):
+    Transition timing function for all transitions, defaults to
+    theme.transitionTimingFunction.
+
+- unstyled (boolean; optional):
+    Remove all Mantine styling from the component.
+
+- value (string; optional):
+    Current selected value."""
+    _children_props = []
+    _base_nodes = ['children']
+    _namespace = 'dash_mantine_components'
+    _type = 'SegmentedControl'
+    @_explicitize_args
+    def __init__(self, data=Component.REQUIRED, value=Component.UNDEFINED, disabled=Component.UNDEFINED, fullWidth=Component.UNDEFINED, color=Component.UNDEFINED, size=Component.UNDEFINED, radius=Component.UNDEFINED, transitionDuration=Component.UNDEFINED, transitionTimingFunction=Component.UNDEFINED, orientation=Component.UNDEFINED, className=Component.UNDEFINED, style=Component.UNDEFINED, styles=Component.UNDEFINED, id=Component.UNDEFINED, unstyled=Component.UNDEFINED, sx=Component.UNDEFINED, m=Component.UNDEFINED, my=Component.UNDEFINED, mx=Component.UNDEFINED, mt=Component.UNDEFINED, mb=Component.UNDEFINED, ml=Component.UNDEFINED, mr=Component.UNDEFINED, p=Component.UNDEFINED, py=Component.UNDEFINED, px=Component.UNDEFINED, pt=Component.UNDEFINED, pb=Component.UNDEFINED, pl=Component.UNDEFINED, pr=Component.UNDEFINED, **kwargs):
+        self._prop_names = ['id', 'className', 'color', 'data', 'disabled', 'fullWidth', 'm', 'mb', 'ml', 'mr', 'mt', 'mx', 'my', 'orientation', 'p', 'pb', 'pl', 'pr', 'pt', 'px', 'py', 'radius', 'size', 'style', 'styles', 'sx', 'transitionDuration', 'transitionTimingFunction', 'unstyled', 'value']
+        self._valid_wildcard_attributes =            []
+        self.available_properties = ['id', 'className', 'color', 'data', 'disabled', 'fullWidth', 'm', 'mb', 'ml', 'mr', 'mt', 'mx', 'my', 'orientation', 'p', 'pb', 'pl', 'pr', 'pt', 'px', 'py', 'radius', 'size', 'style', 'styles', 'sx', 'transitionDuration', 'transitionTimingFunction', 'unstyled', 'value']
+        self.available_wildcard_properties =            []
+        _explicit_args = kwargs.pop('_explicit_args')
+        _locals = locals()
+        _locals.update(kwargs)  # For wildcard attrs and excess named props
+        args = {k: _locals[k] for k in _explicit_args}
+
+        for k in ['data']:
+            if k not in args:
+                raise TypeError(
+                    'Required argument `' + k + '` was not specified.')
+
+        super(SegmentedControl, self).__init__(**args)
---
+++ packages/dash-mantine-components/dash_mantine_components-0.11.0a0-py3-none-any.whl/dash_mantine_components/Tabs.py
@@ -0,0 +1,129 @@
+# AUTO GENERATED FILE - DO NOT EDIT
+
+from dash.development.base_component import Component, _explicitize_args
+
+
+class Tabs(Component):
+    """A Tabs component.
+Switch between different views. For more information, see: https://mantine.dev/core/tabs/
+
+Keyword arguments:
+
+- children (a list of or a singular dash component, string or number; optional):
+    Tabs content.
+
+- id (string; optional):
+    Unique ID to identify this component in Dash callbacks.
+
+- activateTabWithKeyboard (boolean; optional):
+    Determines whether tab should be activated with arrow key press,
+    defaults to True.
+
+- allowTabDeactivation (boolean; optional):
+    Determines whether tab can be deactivated, defaults to False.
+
+- className (string; optional):
+    Often used with CSS to style elements with common properties.
+
+- color (boolean | number | string | dict | list; optional):
+    Key of theme.colors.
+
+- inverted (boolean; optional):
+    Determines whether tabs should have inverted styles.
+
+- keepMounted (boolean; optional):
+    If set to False, Tabs.Panel content will not stay mounted when tab
+    is not active.
+
+- loop (boolean; optional):
+    Determines whether arrow key presses should loop though items
+    (first to last and last to first).
+
+- m (number; optional):
+    margin props.
+
+- mb (number; optional):
+    margin props.
+
+- ml (number; optional):
+    margin props.
+
+- mr (number; optional):
+    margin props.
+
+- mt (number; optional):
+    margin props.
+
+- mx (number; optional):
+    margin props.
+
+- my (number; optional):
+    margin props.
+
+- orientation (a value equal to: 'horizontal', 'vertical'; optional):
+    Tabs orientation, vertical or horizontal.
+
+- p (number; optional):
+    padding props.
+
+- pb (number; optional):
+    padding props.
+
+- pl (number; optional):
+    padding props.
+
+- placement (a value equal to: 'right', 'left'; optional):
+    Tabs.List placement relative to Tabs.Panel, applicable only for
+    orientation=\"vertical\", left by default.
+
+- pr (number; optional):
+    padding props.
+
+- pt (number; optional):
+    padding props.
+
+- px (number; optional):
+    padding props.
+
+- py (number; optional):
+    padding props.
+
+- radius (number; optional):
+    Tabs border-radius from theme.radius or number ti set value from
+    theme, defaults to theme.defaultRadius.
+
+- style (boolean | number | string | dict | list; optional):
+    Inline style.
+
+- styles (dict; optional):
+    Mantine styles API.
+
+- sx (boolean | number | string | dict | list; optional):
+    With sx you can add styles to component root element. If you need
+    to customize styles of other elements within component use styles
+    prop.
+
+- unstyled (boolean; optional):
+    Remove all Mantine styling from the component.
+
+- value (string; optional):
+    Value for controlled component.
+
+- variant (a value equal to: 'default', 'outline', 'pills'; optional):
+    Controls component visuals."""
+    _children_props = []
+    _base_nodes = ['children']
+    _namespace = 'dash_mantine_components'
+    _type = 'Tabs'
+    @_explicitize_args
+    def __init__(self, children=None, value=Component.UNDEFINED, orientation=Component.UNDEFINED, loop=Component.UNDEFINED, activateTabWithKeyboard=Component.UNDEFINED, allowTabDeactivation=Component.UNDEFINED, variant=Component.UNDEFINED, color=Component.UNDEFINED, radius=Component.UNDEFINED, inverted=Component.UNDEFINED, keepMounted=Component.UNDEFINED, placement=Component.UNDEFINED, className=Component.UNDEFINED, style=Component.UNDEFINED, styles=Component.UNDEFINED, id=Component.UNDEFINED, unstyled=Component.UNDEFINED, sx=Component.UNDEFINED, m=Component.UNDEFINED, my=Component.UNDEFINED, mx=Component.UNDEFINED, mt=Component.UNDEFINED, mb=Component.UNDEFINED, ml=Component.UNDEFINED, mr=Component.UNDEFINED, p=Component.UNDEFINED, py=Component.UNDEFINED, px=Component.UNDEFINED, pt=Component.UNDEFINED, pb=Component.UNDEFINED, pl=Component.UNDEFINED, pr=Component.UNDEFINED, **kwargs):
+        self._prop_names = ['children', 'id', 'activateTabWithKeyboard', 'allowTabDeactivation', 'className', 'color', 'inverted', 'keepMounted', 'loop', 'm', 'mb', 'ml', 'mr', 'mt', 'mx', 'my', 'orientation', 'p', 'pb', 'pl', 'placement', 'pr', 'pt', 'px', 'py', 'radius', 'style', 'styles', 'sx', 'unstyled', 'value', 'variant']
+        self._valid_wildcard_attributes =            []
+        self.available_properties = ['children', 'id', 'activateTabWithKeyboard', 'allowTabDeactivation', 'className', 'color', 'inverted', 'keepMounted', 'loop', 'm', 'mb', 'ml', 'mr', 'mt', 'mx', 'my', 'orientation', 'p', 'pb', 'pl', 'placement', 'pr', 'pt', 'px', 'py', 'radius', 'style', 'styles', 'sx', 'unstyled', 'value', 'variant']
+        self.available_wildcard_properties =            []
+        _explicit_args = kwargs.pop('_explicit_args')
+        _locals = locals()
+        _locals.update(kwargs)  # For wildcard attrs and excess named props
+        args = {k: _locals[k] for k in _explicit_args if k != 'children'}
+
+        super(Tabs, self).__init__(children=children, **args)
