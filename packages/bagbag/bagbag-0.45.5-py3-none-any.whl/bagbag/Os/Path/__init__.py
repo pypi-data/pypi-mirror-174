03210beead29c442d5699aff4699ae9824e43138
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:23:53 +0000

    Add package bagbag/0.45.5/bagbag-0.45.5-py3-none-any.whl

---
+++ packages/bagbag/bagbag-0.45.5-py3-none-any.whl/bagbag/Os/Path/__init__.py
@@ -0,0 +1,39 @@
+import os
+
+def Basedir(path:str) -> str:
+    return os.path.dirname(path)
+
+def Join(*path) -> str:
+    return os.path.join(*path)
+
+def Exists(path:str) -> bool:
+    return os.path.exists(path)
+
+def NotExists(path:str) -> bool:
+    return not os.path.exists(path)
+
+def Uniquify(path:str) -> str:
+    """
+    If the file exists, add a number to the end of the file name until it doesn't exist
+    
+    :param path: The path to the file you want to uniquify
+    :type path: str
+    :return: The path of the file with a number appended to the end of the file name.
+    """
+    filename, extension = os.path.splitext(path)
+    counter = 1
+
+    while os.path.exists(path):
+        path = filename + "." + str(counter) + extension
+        counter += 1
+
+    return path
+
+def IsDir(path:str) -> bool:
+    return os.path.isdir(path)
+
+def Basename(path:str) -> str:
+    return os.path.basename(path)
+
+if __name__ == "__main__":
+    print(Join("a", "b"))
