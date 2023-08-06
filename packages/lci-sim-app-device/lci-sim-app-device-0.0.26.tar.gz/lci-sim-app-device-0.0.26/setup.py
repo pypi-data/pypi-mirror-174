1abcc0f4ce380377cd21396cfebdce66b42c8172
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:13:04 +0000

    Add package lci-sim-app-device/0.0.26/lci-sim-app-device-0.0.26.tar.gz

---
+++ packages/lci-sim-app-device/lci-sim-app-device-0.0.26.tar.gz/lci-sim-app-device-0.0.26/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.26' 
+DESCRIPTION = 'Sim App Device package'
+LONG_DESCRIPTION = 'Software to run, e.g. on a raspberry pi, to communicate with the SIM800 overlay and connect to the sim-app.ovh controller. Details can be found at https://sim-app.ovh or github.'
+
+# Setting up
+setup(
+       # the name must match the folder name 'verysimplemodule'
+        name="lci-sim-app-device", 
+        version=VERSION,
+        author="Let's Code It",
+        author_email="kontakt@letscode.it",
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        packages=find_packages(),
+        install_requires=[], # add any additional packages that 
+        # needs to be installed along with your package. Eg: 'caer'
+        
+        keywords=['python', 'sim app device', 'sim', 'sim app'],
+        classifiers= [
+            #"Development Status :: 3 - Alpha",
+            #"Intended Audience :: Education",
+            "Programming Language :: Python :: 3",
+            #"Operating System :: MacOS :: MacOS X",
+            #"Operating System :: Microsoft :: Windows",
+        ],
+
+        include_package_data=True,
+        #package_data={'lci-sim-app-device': ['Assets']},
+        python_requires='>=3.7,<3.8',
+)
+
+#fehpah-Xepgi2-zabnit
