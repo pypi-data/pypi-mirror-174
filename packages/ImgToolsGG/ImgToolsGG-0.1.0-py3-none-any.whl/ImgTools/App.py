3a9bb346e4ad178c9d86153cd14504d90ed8c648
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:24 +0000

    Add package ImgToolsGG/0.1.0/ImgToolsGG-0.1.0-py3-none-any.whl

---
+++ packages/ImgToolsGG/ImgToolsGG-0.1.0-py3-none-any.whl/ImgTools/App.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+class ImageUtils:
+    """
+    This is a demo class, just return a image name.
+
+    :param multiplier: The multiplier.
+    :type multiplier: int
+    """
+    def __init__(self, image_name):
+        self.image_name = image_name
+
+    def get_image_name(self):
+        """
+        Get image name
+        :return: The image name.
+        :rtype: str
+        """
+        return self.image_name
+
---
+++ packages/ImgToolsGG/ImgToolsGG-0.1.0-py3-none-any.whl/ImgTools/__init__.py
@@ -0,0 +1,1 @@
+from ImgTools.App import ImageUtils
