e39013e04afce92295e6118e83fad5d9b0ba3233
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:22:10 +0000

    Add package pharmpy-core/0.83.0/pharmpy_core-0.83.0-py3-none-any.whl

---
+++ packages/pharmpy-core/pharmpy_core-0.83.0-py3-none-any.whl/pharmpy/modeling/compartments.py
@@ -0,0 +1,42 @@
+def get_lag_times(model):
+    """Get lag times for all compartments
+
+    Parameters
+    ----------
+    model : Pharmpy model
+
+    Result
+    ------
+    dict
+        Dictionary from compartment name to lag time expression
+    """
+    odes = model.statements.ode_system
+    names = odes.compartment_names
+    d = {}
+    for name in names:
+        cmt = odes.find_compartment(name)
+        if cmt.lag_time:
+            d[name] = cmt.lag_time
+    return d
+
+
+def get_bioavailability(model):
+    """Get bioavailability of doses for all compartments
+
+    Parameters
+    ----------
+    model : Pharmpy model
+
+    Result
+    ------
+    dict
+        Dictionary from compartment name to bioavailability expression
+    """
+    odes = model.statements.ode_system
+    names = odes.compartment_names
+    d = {}
+    for name in names:
+        cmt = odes.find_compartment(name)
+        if cmt.bioavailability != 1:
+            d[name] = cmt.bioavailability
+    return d
