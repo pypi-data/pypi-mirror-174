312e651b7722ee7f75c3e1513ad03e917e71b54e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:15:42 +0000

    Add package distribution-Package-1337/1.1/distribution_Package_1337-1.1.tar.gz

---
+++ packages/distribution-Package-1337/distribution_Package_1337-1.1.tar.gz/distribution_Package_1337-1.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distribution_Package_1337',
+      version='1.01',
+      description='Gaussian and Binomial distributions',
+      packages=['distribution_Package_1337'],
+      author = 'Emmanuel Ambrose Dan',
+      author_email = 'emmanueldan@uniuyo.edu.ng',
+      zip_safe=False)
