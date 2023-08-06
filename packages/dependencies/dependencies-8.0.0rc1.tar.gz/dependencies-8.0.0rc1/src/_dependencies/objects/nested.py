1f3120931393de8121de0b628a548bad8409a0e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:16 +0000

    Add package dependencies/8.0.0rc1/dependencies-8.0.0rc1.tar.gz

---
+++ packages/dependencies/dependencies-8.0.0rc1.tar.gz/dependencies-8.0.0rc1/src/_dependencies/objects/nested.py
@@ -0,0 +1,30 @@
+from _dependencies.scope import _Scope
+from _dependencies.spec import _Spec
+
+
+class _InjectorTypeType(type):
+    pass
+
+
+def _is_nested_injector(name, dependency):
+    return isinstance(dependency, _InjectorTypeType)
+
+
+def _build_nested_injector_spec(name, dependency):
+    return _Spec(
+        _NestedInjectorFactory(dependency),
+        {"__self__": False},
+        {"__self__"},
+        set(),
+        lambda: "'Injector'",
+        False,
+    )
+
+
+class _NestedInjectorFactory:
+    def __init__(self, injector):
+        self.injector = injector
+
+    def __call__(self, __self__):
+        graph = self.injector.__dependencies__.copy()
+        return _Scope(self.injector.__name__, graph, lambda graph, cache: None), None
