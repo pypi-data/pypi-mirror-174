844c3800e1f5709cfc4ed2569a68c561101fab2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:24:31 +0000

    Add package modulepy/0.13/modulepy-0.13.tar.gz

---
+++ packages/modulepy/modulepy-0.13.tar.gz/modulepy-0.13/modulepy/__init__.py
@@ -0,0 +1,6 @@
+from loguru import logger as log
+from pathlib import Path
+
+MODULE_BASE_DIRECTORY = Path("/tmp")
+
+__all__ = ['log', MODULE_BASE_DIRECTORY]
---
+++ packages/modulepy/modulepy-0.13.tar.gz/modulepy-0.13/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    long_description=open("README.md", "r").read(),
+    name="modulepy",
+    version="0.13",
+    description="module framework",
+    author="Pascal Eberlein",
+    author_email="pascal@eberlein.io",
+    url="https://github.com/nbdy/modulepy",
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+    ],
+    keywords="modularity modules",
+    packages=find_packages(),
+    long_description_content_type="text/markdown"
+)
