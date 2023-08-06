de71d6c8ccdb542785cb0fb043625918ab3647e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:53 +0000

    Add package long-list/0.2/long_list-0.2.tar.gz

---
+++ packages/long-list/long_list-0.2.tar.gz/long_list-0.2/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='long_list',
+    version='0.2',
+    license='MIT',
+    author="Tawfiq Khalilieh",
+    author_email='taw.coding@gmail.com',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    url='https://github.com/tawfiqkhalilieh/LongList',
+    keywords='list long_list infinitylist',
+    install_requires=[],
+)
