05089c947bb621f215c6ee3da02baf1373710a05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:01:19 +0000

    Add package tinytim/1.8.0/tinytim-1.8.0-py3-none-any.whl

---
+++ packages/tinytim/tinytim-1.8.0-py3-none-any.whl/tinytim/__init__.py
@@ -0,0 +1,13 @@
+__version__ = '1.8.0'
+
+
+import tinytim.columns
+import tinytim.copy
+import tinytim.data
+import tinytim.edit
+import tinytim.filter
+import tinytim.group
+import tinytim.rows
+import tinytim.utils
+import tinytim.validate
+import tinytim.join
