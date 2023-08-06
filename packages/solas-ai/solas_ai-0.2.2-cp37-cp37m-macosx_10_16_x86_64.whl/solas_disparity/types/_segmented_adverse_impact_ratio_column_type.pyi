ed1cabb98dd0438c34257df30e1bcbc4dd717e4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:18 +0000

    Add package solas-ai/0.2.2/solas_ai-0.2.2-cp37-cp37m-macosx_10_16_x86_64.whl

---
+++ packages/solas-ai/solas_ai-0.2.2-cp37-cp37m-macosx_10_16_x86_64.whl/solas_disparity/types/_segmented_adverse_impact_ratio_column_type.pyi
@@ -0,0 +1,11 @@
+from ._enum_base import EnumBase as EnumBase
+from _typeshed import Incomplete
+from solas_disparity import const as const
+
+class SegmentedAdverseImpactRatioColumnType(EnumBase):
+    TOTAL: Incomplete
+    LABEL_FAVORABLE: Incomplete
+    PERCENT_LABEL_FAVORABLE: Incomplete
+    FAVORABLE: Incomplete
+    PERCENT_FAVORABLE: Incomplete
+    AIR: Incomplete
