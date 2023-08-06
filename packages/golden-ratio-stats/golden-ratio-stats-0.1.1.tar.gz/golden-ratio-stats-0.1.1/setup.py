78439179772d630718d887d5e1dcccad6208dc89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:13:22 +0000

    Add package golden-ratio-stats/0.1.1/golden-ratio-stats-0.1.1.tar.gz

---
+++ packages/golden-ratio-stats/golden-ratio-stats-0.1.1.tar.gz/golden-ratio-stats-0.1.1/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="golden-ratio-stats",
+    version="0.1.1",
+    author="Serhat Erdem",
+    author_email="erdemserhat1994@gmail.com",
+    description="Golden Ratio in Statistics",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/serhateerdem",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.9"
+    ],
+    python_requires=">=3.9",
+    install_requires=["numpy>=1.23.4", "pandas>=1.5.1"]
+)
