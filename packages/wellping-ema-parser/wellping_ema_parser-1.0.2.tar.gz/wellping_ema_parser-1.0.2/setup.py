92109a968c1904a5e25d9e5aa850862140ffbd5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:37:59 +0000

    Add package wellping-ema-parser/1.0.2/wellping_ema_parser-1.0.2.tar.gz

---
+++ packages/wellping-ema-parser/wellping_ema_parser-1.0.2.tar.gz/wellping_ema_parser-1.0.2/setup.py
@@ -0,0 +1,27 @@
+#!/bin/python
+from setuptools import setup
+import pathlib
+
+HERE = pathlib.Path(__file__).parent
+
+README = (HERE / "README.md").read_text()
+
+setup(
+    name="wellping_ema_parser",
+    version="1.0.2",
+    description="JSON to CSV converter for Stanford Communities Project",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/StanfordSocialNeuroscienceLab/wellping-ema-package",
+    author="Ian Richard Ferguson",
+    author_email="IRF229@nyu.edu",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=["scp_ema", "scp_ema/parser"],
+    include_package_data=True,
+    install_requires=["numpy", "pandas", "tqdm"],
+)
