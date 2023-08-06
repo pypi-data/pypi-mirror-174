6e9974a05ccbd160c9885fa56405c8eb0e84bfbc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:15:39 +0000

    Add package saas-co/1.8/saas_co-1.8-py2.py3-none-any.whl

---
+++ packages/saas-co/saas_co-1.8-py2.py3-none-any.whl/saas_co/paginate.py
@@ -0,0 +1,49 @@
+import boto3
+from jsonpath_ng import jsonpath, parse
+
+#####################################
+
+def fields(data, args):
+    f = args.get('fields',[])
+    if not f: return data
+    d = {}
+    for je in f: 
+        for i in parse(je).find(data):
+            d.update({je:i.value})
+    return d
+
+#####################################
+
+def found(args,d2):
+    f = args.get('filter',[])
+    if not f: return True
+    for key,val in f.items():
+        for i in parse(key).find(d2):
+            if i.value in val:
+                return True
+    return False
+
+#####################################
+
+class paginate():
+    def __init__(self, session, **kargs):
+        self.json = []
+        self.session = session
+        for page in self.session.client(kargs.get('service')).get_paginator(kargs.get('ep')).paginate():
+            self.response = [fields(i.value, kargs) for i in self.jsonpath_expression().find(page) if found(kargs, i.value)]
+        self.df = pd.json_normalize(self.json)
+
+            
+    def __str__(self):
+        return json.dumps(self.json)
+
+#####################################
+
+class ec2_paginate(paginate):
+    def __init__(self, session, **kargs):
+        kargs.update({'ep':self.name()})
+        kargs.update({'service':'ec2'})
+        paginate.__init__(self, session, **kargs)
+
+#####################################
+#####################################
