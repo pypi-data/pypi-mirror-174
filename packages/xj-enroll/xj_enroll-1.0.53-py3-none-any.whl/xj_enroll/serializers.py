3da17d11a39be646b0053619364e81cad79fecb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:56:15 +0000

    Add package xj-enroll/1.0.53/xj_enroll-1.0.53-py3-none-any.whl

---
+++ packages/xj-enroll/xj_enroll-1.0.53-py3-none-any.whl/xj_enroll/serializers.py
@@ -0,0 +1,29 @@
+"""
+Created on 2022-05-19
+@author:刘飞
+@description:报名模块序列化器
+"""
+from rest_framework import serializers
+
+from .models import *
+
+
+class EnrollListSerializer(serializers.ModelSerializer):
+    """
+    报名表序列化器
+    """
+
+    class Meta:
+        model = Enroll
+        fields = '__all__'
+
+
+class EnrollRecordListSerializer(serializers.ModelSerializer):
+    """
+    报名记录列表序列化器
+    """
+    price = serializers.DecimalField(max_digits=10, decimal_places=2)
+
+    class Meta:
+        model = EnrollRecord
+        fields = '__all__'
