4d170c645a3b790411a15b8f284a85a8a302522f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:24:24 +0000

    Add package django-select2/7.11.1/django-select2-7.11.1.tar.gz

---
+++ packages/django-select2/django-select2-7.11.1.tar.gz/django-select2-7.11.1/tests/testapp/settings.py
@@ -0,0 +1,45 @@
+import os.path
+
+import django
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+DEBUG = True
+
+DATABASES = {"default": {"ENGINE": "django.db.backends.sqlite3", "NAME": ":memory:"}}
+
+INSTALLED_APPS = (
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.staticfiles",
+    "django_select2",
+    "tests.testapp",
+)
+
+STATIC_URL = "/static/"
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
+SITE_ID = 1
+ROOT_URLCONF = "tests.testapp.urls"
+
+LANGUAGES = [
+    ("de", "German"),
+    ("en", "English"),
+]
+LANGUAGE_CODE = "en"
+
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "APP_DIRS": True,
+        "DIRS": ["templates"],
+    },
+]
+
+SECRET_KEY = "123456"
+
+if django.VERSION < (4, 0):
+    USE_L10N = True
+USE_I18N = True
+USE_TZ = True
