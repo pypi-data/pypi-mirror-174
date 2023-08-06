9ca3bf892a3cff420cb96421492377c2fe618b8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:16:53 +0000

    Add package magic-config/0.1.8/magic-config-0.1.8.tar.gz

---
+++ packages/magic-config/magic-config-0.1.8.tar.gz/magic-config-0.1.8/build.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+#python setup.py sdist bdist_wheel
+#twine upload --repository-url https://test.pypi.org/legacy/ dist/*
+
+echo "Delete current dist..."
+rm -vrf dist
+rm -vrf magic_config/magic_config.egg-info
+
+python -m build
+
+#python -m twine upload --verbose --repository testpypi dist/*
+python -m twine upload --verbose dist/*
+
