ab7f3632f74db6eea185a77e6b90c60205ff34f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:50 +0000

    Add package python-ffmpeg/1.0.15/python-ffmpeg-1.0.15.tar.gz

---
+++ packages/python-ffmpeg/python-ffmpeg-1.0.15.tar.gz/python-ffmpeg-1.0.15/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+with open('README.md', 'r') as input_file:
+    long_description = input_file.read()
+
+setup(
+    name='python-ffmpeg',
+    version='1.0.15',
+    description='A python interface for FFmpeg using asyncio',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/jonghwanhyeon/python-ffmpeg',
+    author='Jonghwan Hyeon',
+    author_email='hyeon0145@gmail.com',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3 :: Only'
+    ],
+    keywords='ffmpeg',
+    packages=find_packages(),
+    install_requires=['pyee'],
+)
