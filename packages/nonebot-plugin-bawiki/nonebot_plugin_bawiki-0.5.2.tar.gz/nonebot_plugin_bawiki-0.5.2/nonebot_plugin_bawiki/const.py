21f55f137d55b990bac6f6286e256909bdd37471
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:24:53 +0000

    Add package nonebot-plugin-bawiki/0.5.2/nonebot_plugin_bawiki-0.5.2.tar.gz

---
+++ packages/nonebot-plugin-bawiki/nonebot_plugin_bawiki-0.5.2.tar.gz/nonebot_plugin_bawiki-0.5.2/nonebot_plugin_bawiki/const.py
@@ -0,0 +1,11 @@
+from pathlib import Path
+
+SCHALE_URL = "https://lonqie.github.io/SchaleDB/"
+MIRROR_SCHALE_URL = "http://schale.lgc2333.top/"
+
+BAWIKI_DB_URL = "https://bawiki.lgc2333.top/"
+
+RES_PATH = Path(__file__).parent / "res"
+# RES_FONT = RES_PATH / "SourceHanSansSC-Bold-2.otf"
+RES_CALENDER_BANNER = RES_PATH / "calender_banner.png"
+RES_SCHALE_BG = RES_PATH / "schale_bg.jpg"
