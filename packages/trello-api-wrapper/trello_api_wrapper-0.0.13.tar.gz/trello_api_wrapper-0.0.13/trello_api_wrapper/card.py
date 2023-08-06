b25d5a41b7c633648d258c19bbd969861bb4a5c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:23:58 +0000

    Add package trello-api-wrapper/0.0.13/trello_api_wrapper-0.0.13.tar.gz

---
+++ packages/trello-api-wrapper/trello_api_wrapper-0.0.13.tar.gz/trello_api_wrapper-0.0.13/trello_api_wrapper/card.py
@@ -0,0 +1,32 @@
+from typing import Dict, Type
+
+from ..base_class import BaseClass
+from ..trello_requests import get_request, was_successful
+
+
+class Card(BaseClass):
+    """Card class definition. Those are the main components of the project."""
+
+    def __init__(self, trello: Type[BaseClass], card_id: str) -> None:
+        super().__init__(trello.apikey, trello.token)
+        self.id = card_id
+        self.update()
+
+    def update(self) -> None:
+        card = fetch_data(self)
+        self.name = card["name"]
+        self.closed = card["closed"]
+
+
+def fetch_data(card) -> Dict[str, str]:
+    """Loads CardList information."""
+    url = f"https://api.trello.com/1/cards/{card.id}"
+    response = get_request(card, url)
+    if was_successful(response):
+        temp_card = {
+            "name": response["data"]["name"],
+            "closed": response["data"]["closed"],
+        }
+    else:
+        temp_card = {"name": None, "closed": None}
+    return temp_card
