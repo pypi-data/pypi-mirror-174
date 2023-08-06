6126199e404396bb7469862d60616e36f36bd510
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:57:16 +0000

    Add package drawingtime/0.2/drawingtime-0.2.tar.gz

---
+++ packages/drawingtime/drawingtime-0.2.tar.gz/drawingtime-0.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+setup(
+name="drawingtime",
+version="0.2",
+description="this is superset of turtle module in python",
+long_description="",
+url="",
+author="anocoder",
+package=['init.py'],
+install_requires=[]
+)
