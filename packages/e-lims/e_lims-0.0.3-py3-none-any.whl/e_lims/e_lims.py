8c59d10d4b035adf68683a9c25bc8ffb73485d8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:19:50 +0000

    Add package e-lims/0.0.3/e_lims-0.0.3-py3-none-any.whl

---
+++ packages/e-lims/e_lims-0.0.3-py3-none-any.whl/e_lims/e_lims.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""e-lims main."""
+
+
+def hello(name):
+    # type: (str) -> str
+    """Say hello.
+
+    >>> hello('e-lims')
+    'Hello e-lims!'
+    """
+    return f"Hello {name}!"
