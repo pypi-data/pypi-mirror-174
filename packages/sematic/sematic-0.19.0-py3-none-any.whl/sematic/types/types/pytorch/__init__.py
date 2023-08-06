9ee16364c34ffa90f56b72e9d47e897f6b221d9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:00:56 +0000

    Add package sematic/0.19.0/sematic-0.19.0-py3-none-any.whl

---
+++ packages/sematic/sematic-0.19.0-py3-none-any.whl/sematic/types/types/pytorch/__init__.py
@@ -0,0 +1,8 @@
+try:
+    import torch  # noqa: F401
+except ImportError:
+    pass
+else:
+    # Sematic
+    import sematic.types.types.pytorch.dataloader  # noqa: F401
+    import sematic.types.types.pytorch.module  # noqa: F401
