cd2c77a352e4df0ff6a43b30ced498eff86a0022
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:11:21 +0000

    Add package gold-python/0.1.2/gold_python-0.1.2.tar.gz

---
+++ packages/gold-python/gold_python-0.1.2.tar.gz/gold_python-0.1.2/setup.py
@@ -0,0 +1,33 @@
+from distutils.core import setup
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+  name = 'gold_python',
+  packages = ['gold_python'],
+  version = '0.1.2',
+  long_description=long_description,
+  long_description_content_type='text/markdown',
+  license='BSD 3-Clause',
+  description = 'Library for the developement of finite automata',
+  author = 'Nicolas Saavedra Gonzalez',
+  author_email = 'personal@nicolassaavedra.com',
+  url = 'https://github.com/GOLD-Python/GOLD-Python',
+  download_url = 'https://github.com/GOLD-Python/GOLD-Python/archive/refs/tags/v012-BETA.zip',
+  keywords = ['automata', 'gold', 'transducer', 'pushdown', 'deterministic'],
+  install_requires=[
+          'networkx',
+          'anytree',
+          'graphviz'
+      ],
+  classifiers=[
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: BSD License',
+    'Programming Language :: Python :: 3.10',
+    'Programming Language :: Python :: 3.11',
+  ],
+)
