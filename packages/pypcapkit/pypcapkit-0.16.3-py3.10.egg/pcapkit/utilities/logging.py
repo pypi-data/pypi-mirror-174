e6cb7fed59dff4f31bfcfd50855276a1c43a16de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:00:29 +0000

    Add package pypcapkit/0.16.3/pypcapkit-0.16.3-py3.10.egg

---
+++ packages/pypcapkit/pypcapkit-0.16.3-py3.10.egg/pcapkit/utilities/logging.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+"""Logging System
+====================
+
+:mod:`pcapkit.utilities.logging` contains naïve integration
+of the Python logging system, i.e. a :class:`logging.Logger`
+instance as :data:`~pcapkit.utilities.logging.logger`.
+
+"""
+import logging
+import os
+import sys
+
+__all__ = ['logger']
+
+###############################################################################
+# Dev Mode
+###############################################################################
+
+# boolean mappings
+BOOLEAN_STATES = {'1': True, '0': False,
+                  'yes': True, 'no': False,
+                  'true': True, 'false': False,
+                  'on': True, 'off': False}
+
+#: Development mode flag.
+DEVMODE = BOOLEAN_STATES.get(os.environ.get('PCAPKIT_DEVMODE', 'false').casefold(), False)
+
+###############################################################################
+# Sphinx Mode
+###############################################################################
+
+# NOTE: This is a workaround for TYPE_CHECKING in Sphinx.
+SPHINX_TYPE_CHECKING = BOOLEAN_STATES.get(os.environ.get('PCAPKIT_SPHINX', 'false').casefold(), False)
+
+###############################################################################
+# Logger Setup
+###############################################################################
+
+#: logging.Logger: :class:`~logging.Logger` instance named after ``pcapkit``.
+logger = logging.getLogger('pcapkit')
+
+formatter = logging.Formatter(fmt='[%(levelname)s] %(asctime)s - %(message)s',
+                              datefmt='%m/%d/%Y %I:%M:%S %p')
+handler = logging.StreamHandler(sys.stderr)
+if DEVMODE:
+    logger.setLevel(logging.DEBUG)
+else:
+    logger.setLevel(logging.INFO)
+handler.setFormatter(formatter)
+logger.addHandler(handler)
