1ccdf83649ca13d9beadf4c72adc7572c0474172
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:15:59 +0000

    Add package novalabs/1.2.60/novalabs-1.2.60-py3-none-any.whl

---
+++ packages/novalabs/novalabs-1.2.60-py3-none-any.whl/test/clients/test_get_last_price.py
@@ -0,0 +1,58 @@
+from nova.clients.clients import clients
+from decouple import config
+import time
+
+
+def asserts_get_last_price(
+    exchange: str,
+    pair: str,
+):
+
+    client = clients(
+        exchange=exchange,
+        key=config(f"{exchange}TestAPIKey"),
+        secret=config(f"{exchange}TestAPISecret"),
+        testnet=True
+    )
+
+    data = client.get_last_price(
+        pair=pair,
+    )
+
+    print(data)
+
+    assert isinstance(data, dict)
+    assert data['pair'] == pair
+    assert data['latest_price'] > 0
+
+    print(f"Test get_last_price for {exchange.upper()} successful")
+
+
+def test_get_last_price():
+    all_tests = [
+        # {
+        #     'exchange': 'binance',
+        #     'pair': 'BTCUSDT',
+        # },
+        # {
+        #     'exchange': 'bybit',
+        #     'pair': 'BTCUSDT'
+        # },
+        # {
+        #     'exchange': 'ftx',
+        #     'pair': 'ETH-PERP'
+        # },
+        {
+            'exchange': 'kraken',
+            'pair': 'pf_xbtusd'
+        }
+    ]
+
+    for _test in all_tests:
+        asserts_get_last_price(
+            exchange=_test['exchange'],
+            pair=_test['pair']
+        )
+
+
+test_get_last_price()
---
+++ packages/novalabs/novalabs-1.2.60-py3-none-any.whl/test/clients/test_get_order_book.py
@@ -0,0 +1,56 @@
+from nova.clients.clients import clients
+from decouple import config
+
+
+def asserts_get_order_book(exchange: str, pair: str):
+
+    client = clients(
+        exchange=exchange,
+        key=config(f"{exchange}TestAPIKey"),
+        secret=config(f"{exchange}TestAPISecret"),
+        testnet=True
+    )
+
+    data = client.get_order_book(
+        pair=pair,
+    )
+
+    for key, values in data.items():
+        assert key in ['bids', 'asks']
+        assert isinstance(values, list)
+
+        for _value in values:
+
+            assert isinstance(_value['price'], float)
+            assert isinstance(_value['size'], float)
+
+    print(f"Test get_order for {exchange.upper()} successful")
+
+
+def test_get_order_book():
+    all_tests = [
+        # {
+        #     'exchange': 'binance',
+        #     'pair': 'BTCUSDT'
+        # },
+        # {
+        #     'exchange': 'bybit',
+        #     'pair': 'BTCUSDT'
+        # },
+        {
+            'exchange': 'ftx',
+            'pair': 'BTC-PERP'
+        }
+    ]
+
+    for _test in all_tests:
+
+        asserts_get_order_book(
+            exchange=_test['exchange'],
+            pair=_test['pair']
+        )
+
+
+test_get_order_book()
+
+
