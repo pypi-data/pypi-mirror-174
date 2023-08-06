6384e0b37ee68893f27b3893d550e69fd765249c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:09 +0000

    Add package fumi-probability/0.2/fumi-probability-0.2.tar.gz

---
+++ packages/fumi-probability/fumi-probability-0.2.tar.gz/fumi-probability-0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='fumi-probability',
+      version='0.2',
+      description='Gaussian and Binomial distributions',
+      packages=['fumi_probability'],
+      author = 'Fumihiro Nakashima',
+      author_email = 'fuminaka387@gmail.com',
+      zip_safe=False)
