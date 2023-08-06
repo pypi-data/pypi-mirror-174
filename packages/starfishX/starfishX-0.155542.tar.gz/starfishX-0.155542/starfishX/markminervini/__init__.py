dc8fe25ad652594bd913f9b9c60b1b08d939b072
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:56 +0000

    Add package starfishX/0.155542/starfishX-0.155542.tar.gz

---
+++ packages/starfishX/starfishX-0.155542.tar.gz/starfishX-0.155542/starfishX/markminervini/__init__.py
@@ -0,0 +1,6 @@
+from starfishX.trendtemplate import trendMA_YMF
+from starfishX.trendtemplate import trendMA_Year
+from starfishX.trendtemplate import check52WeekHighLow
+from starfishX.trendtemplate import ROC_Basket
+
+__version__ = "markminervini 0.1" #2022-oct-31
