a7b629bd418397c9086faf75fccee31d01ebd3c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:07:04 +0000

    Add package lightly/1.2.34/lightly-1.2.34-py3-none-any.whl

---
+++ packages/lightly/lightly-1.2.34-py3-none-any.whl/lightly/api/__init__.py
@@ -0,0 +1,7 @@
+""" The lightly.api module provides access to the Lightly web-app. """
+
+# Copyright (c) 2020. Lightly AG and its affiliates.
+# All Rights Reserved
+
+from lightly.api.api_workflow_client import ApiWorkflowClient
+from lightly.api.api_workflow_compute_worker import ArtifactNotExist
