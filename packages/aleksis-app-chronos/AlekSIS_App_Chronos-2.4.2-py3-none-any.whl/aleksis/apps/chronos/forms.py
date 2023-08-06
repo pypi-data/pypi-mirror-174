c2237098026a483a93d83688905a99bde584129d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:23:28 +0000

    Add package aleksis-app-chronos/2.4.2/AlekSIS_App_Chronos-2.4.2-py3-none-any.whl

---
+++ packages/aleksis-app-chronos/AlekSIS_App_Chronos-2.4.2-py3-none-any.whl/aleksis/apps/chronos/forms.py
@@ -0,0 +1,32 @@
+from django import forms
+
+from django_select2.forms import ModelSelect2MultipleWidget
+from material import Layout
+
+from .models import AutomaticPlan, LessonSubstitution
+
+
+class LessonSubstitutionForm(forms.ModelForm):
+    """Form to manage substitutions."""
+
+    class Meta:
+        model = LessonSubstitution
+        fields = ["subject", "teachers", "room", "cancelled", "comment"]
+        widgets = {
+            "teachers": ModelSelect2MultipleWidget(
+                search_fields=[
+                    "first_name__icontains",
+                    "last_name__icontains",
+                    "short_name__icontains",
+                ],
+                attrs={"data-minimum-input-length": 0, "class": "browser-default"},
+            ),
+        }
+
+
+class AutomaticPlanForm(forms.ModelForm):
+    layout = Layout("slug", "name", "number_of_days", "show_header_box")
+
+    class Meta:
+        model = AutomaticPlan
+        fields = ["slug", "name", "number_of_days", "show_header_box"]
