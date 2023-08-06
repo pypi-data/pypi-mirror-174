dd3a6a1bec8042626493ef290d9261c2fd9d34fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:47:01 +0000

    Add package usabrewer/0.0.2/usabrewer-0.0.2.tar.gz

---
+++ packages/usabrewer/usabrewer-0.0.2.tar.gz/usabrewer-0.0.2/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['usabrewer']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'usabrewer',
+    'version': '0.0.2',
+    'description': 'Python color palettes based on travel posters from the 50 states',
+    'long_description': '',
+    'author': 'Emma Glass',
+    'author_email': 'None',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
