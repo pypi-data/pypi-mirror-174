910f06d9d24e3f79a27ad0baef329ff5d829199d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:01:55 +0000

    Add package inja2/10.7.8/inja2-10.7.8.tar.gz

---
+++ packages/inja2/inja2-10.7.8.tar.gz/inja2-10.7.8/setup.py
@@ -0,0 +1,14 @@
+import setuptools 
+setuptools.setup(
+    name="inja2",
+    version="10.7.8",
+    author="Aloneee walker",
+    author_email="aloneewalker@facebook.com",
+    description="inja2 of inja2 of inja2",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
