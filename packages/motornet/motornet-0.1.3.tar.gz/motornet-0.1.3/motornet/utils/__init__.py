4a568a70c99ee470a1839c2c44effd67d326d0d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:13:04 +0000

    Add package motornet/0.1.3/motornet-0.1.3.tar.gz

---
+++ packages/motornet/motornet-0.1.3.tar.gz/motornet-0.1.3/motornet/utils/__init__.py
@@ -0,0 +1,7 @@
+"""This package contains various functions and scripts that can be useful to the general user for building, training,
+and analysing models from `MotorNet`.
+"""
+
+from . import plotor
+from . import net_utils
+
