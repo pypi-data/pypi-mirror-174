f338ff6dc10793aa37d121880fdb12e3886af6d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:27:18 +0000

    Add package kitbag/0.0.3/kitbag-0.0.3.tar.gz

---
+++ packages/kitbag/kitbag-0.0.3.tar.gz/kitbag-0.0.3/kitbag/utils/colors.py
@@ -0,0 +1,9 @@
+red = "#dd2a26"
+yellow = "#fff100"
+green = "#0fac4d"
+blue = "#4285f4"
+grey = "#3C4043"
+
+team_colors = {"FC Nordsjaelland": red, "FC Nordsjaelland U19": blue, "FC Nordsjaelland U17":blue, "FC Nordsjaelland U15":blue,
+               "FC Nordsjaelland Women": yellow,  "FC Nordsjaelland Girls U16": yellow, 
+               "FC Nordsjaelland Girls U16": yellow, "RTD Ghana - Senior": green}
---
+++ packages/kitbag/kitbag-0.0.3.tar.gz/kitbag-0.0.3/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+VERSION = "0.0.3"
+DESCRIPTION = "Kitbag with functions, metadata, plots etc. that are used within FCN and RTD"
+
+setup(
+    name="kitbag",
+    version=VERSION,
+    description=DESCRIPTION,
+    packages=find_packages(),
+)
