37dba5d429078489a43c04b1f29562b69bd664ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:16:48 +0000

    Add package PhoenixScanner/1.0.7/PhoenixScanner-1.0.7.tar.gz

---
+++ packages/PhoenixScanner/PhoenixScanner-1.0.7.tar.gz/PhoenixScanner-1.0.7/test/test.py
@@ -0,0 +1,21 @@
+from PhoenixScanner import Phoenix
+
+Scanner = Phoenix("token")
+
+print(Scanner.check(82))
+"Output: {'user_id': 82, 'is_gban': True, 'reason': 'cwsck', 'scanner': 23123}"
+
+print(Scanner.revert(82))
+"Output: 'Deleted'"
+
+print(Scanner.scan(82, "cwsck", 23123))
+"Output: 'Done'"
+
+print(Scanner.scanlist())
+"Output: [3243252, 432552432, 42323242]"
+
+print(Scanner.token_gen())
+'Output: "RED7-m201vbup6qefssf7ssqqbn"'
+
+print(Scanner.token_revoke("RED7-m201vbup6qefssf7ssqqbn"))
+'Output: "Deleted"'
