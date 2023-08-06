13e02f1fe7e0d5eb950ef07a4734a0fa8ffe2624
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:00:29 +0000

    Add package pypcapkit/0.16.3/pypcapkit-0.16.3-cp37-none-any.whl

---
+++ packages/pypcapkit/pypcapkit-0.16.3-cp37-none-any.whl/pcapkit/vendor/l2tp/__init__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# pylint: disable=unused-import
+""":class:`~pcapkit.protocols.link.l2tp.L2TP` Vendor Crawlers
+================================================================
+
+This module contains all vendor crawlers of
+:class:`~pcapkit.protocols.link.l2tp.L2TP` implementations. Available
+enumerations include:
+
+.. list-table::
+
+   * - :class:`L2TP_Type <pcapkit.vendor.l2tp.type.Type>`
+     - L2TP Types
+
+"""
+
+from pcapkit.vendor.l2tp.type import Type as L2TP_Type
+
+__all__ = ['L2TP_Type']
