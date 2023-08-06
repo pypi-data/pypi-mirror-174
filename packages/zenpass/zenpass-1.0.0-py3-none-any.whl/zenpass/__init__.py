a302936c07bd18b72700dd64b5ecf1a2a9519632
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:00:10 +0000

    Add package zenpass/1.0.0/zenpass-1.0.0-py3-none-any.whl

---
+++ packages/zenpass/zenpass-1.0.0-py3-none-any.whl/zenpass/__init__.py
@@ -0,0 +1,7 @@
+"""Generate a strong password which includes alphabets, numbers, special characters"""
+
+__author__ = 'srg'
+__version__ = '1.0.0'
+
+from .__main__ import main
+from .password import PasswordGenerator
---
+++ packages/zenpass/zenpass-1.0.0-py3-none-any.whl/zenpass/__main__.py
@@ -0,0 +1,47 @@
+import argparse
+
+from .password import PasswordGenerator as Pg
+from .exception import ZenpassException
+
+
+def get_argument():
+    parser = argparse.ArgumentParser(prog="zenpass", usage="zenpass [options]")
+    parser.add_argument('-v', '--version', action='version', help='show version number and exit.', version="1.0.0")
+    group = parser.add_argument_group("to customize Password")
+    group.add_argument("-l", "--length", dest="length", type=int, metavar='', help="to set length to the password")
+    group.add_argument("-n", "--ignore", dest="ignore", metavar='',
+                       help="to ignore unwanted characters to the password")
+    group.add_argument("-i", "--include", dest="include", metavar='', help="to include characters to the password")
+    group.add_argument("-o", "--only", dest="only", metavar='', help="to create password only using wanted characters")
+    group.add_argument("-s", "--separator", dest="separator", metavar='', help="the separator character")
+    group.add_argument("-c", "--seplen", dest="separatorlength", type=int, metavar='',
+                       help="the length of characters between separator")
+    group.add_argument("--repeat", dest="repeat", action='store_true', default=False,
+                       help="to repeat the characters in the password (default : %(default)s)")
+    group.add_argument("--separation", dest="separation", default=False, action="store_true",
+                       help="to separate password characters using separator (default : %(default)s)")
+    parser.add_argument_group(group)
+    options = parser.parse_args()
+    return options
+
+
+def generate_password(rep: bool, separation: bool, pass_len=None, wanted=None, ign=None, inc=None, sep=None,
+                      sep_len=None):
+    arg = Pg(length=pass_len, only=wanted, ignore=ign, include=inc, repeat=rep, separator=sep,
+             separator_length=sep_len, separation=separation)
+    password = arg.generate()
+    return password
+
+
+def main():
+    try:
+        options = get_argument()
+        password = generate_password(options.repeat, options.separation, options.length, options.only, options.ignore,
+                                     options.include, options.separator, options.separatorlength)
+        print("\n{}".format(password))
+    except ZenpassException as e:
+        print(e.__str__())
+
+
+if __name__ == "__main__":
+    main()
