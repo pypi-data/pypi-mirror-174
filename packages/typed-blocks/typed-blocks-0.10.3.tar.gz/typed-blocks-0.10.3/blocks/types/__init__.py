456d3731adc5d31dacfda0f1fe36bed120ce66fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:57:39 +0000

    Add package typed-blocks/0.10.3/typed-blocks-0.10.3.tar.gz

---
+++ packages/typed-blocks/typed-blocks-0.10.3.tar.gz/typed-blocks-0.10.3/blocks/types/__init__.py
@@ -0,0 +1,11 @@
+from blocks.types.base import (Event,
+                               EventOrEvents,
+                               Processor,
+                               AsyncProcessor,
+                               AnyProcessor,
+                               Source,
+                               AsyncSource,
+                               AnySource,
+                               Block,
+                               TypeOfProcessor)
+from blocks.types.dto import ParallelEvent
