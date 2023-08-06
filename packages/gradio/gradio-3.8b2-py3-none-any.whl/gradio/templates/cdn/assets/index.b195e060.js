275e2b1cdcc711cf6e782d696778fcbdcd9ce18d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:45 +0000

    Add package gradio/3.8b2/gradio-3.8b2-py3-none-any.whl

---
+++ packages/gradio/gradio-3.8b2-py3-none-any.whl/gradio/templates/cdn/assets/index.b195e060.js
@@ -0,0 +1,2 @@
+import{C as e}from"./Column.d02a3d27.js";import"./index.446045bf.js";import"./styles.ed3b21b5.js";const m=["static"];export{e as Component,m as modes};
+//# sourceMappingURL=index.b195e060.js.map
---
+++ packages/gradio/gradio-3.8b2-py3-none-any.whl/gradio/templates/cdn/assets/index.c4f57c0f.js
@@ -0,0 +1,2 @@
+import{Q as s}from"./index.446045bf.js";const o=["static"];export{s as Component,o as modes};
+//# sourceMappingURL=index.c4f57c0f.js.map
