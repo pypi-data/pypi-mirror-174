481cef6ce4a473e4c8faba096cde3f7c0d27a845
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:18 +0000

    Add package jenfi-pipeline-data-app/0.3.9/jenfi_pipeline_data_app-0.3.9.tar.gz

---
+++ packages/jenfi-pipeline-data-app/jenfi_pipeline_data_app-0.3.9.tar.gz/jenfi_pipeline_data_app-0.3.9/jenfi_pipeline_data_app/app_funcs/_constants.py
@@ -0,0 +1,10 @@
+import os
+
+ROOT_DIR = os.path.abspath(os.curdir)
+RESULT_FILENAME = "output.json"
+PYTHON_ENV = os.getenv("PYTHON_ENV", "development")
+
+STATUS_SUCCESS = "success"
+STATUS_NO_RESULT = "no_result_returned"
+STATUS_NOT_APPLICABLE = "not_appliciable"
+STATUS_INSUFFICIENT_DATA = "insufficient_data"
---
+++ packages/jenfi-pipeline-data-app/jenfi_pipeline_data_app-0.3.9.tar.gz/jenfi_pipeline_data_app-0.3.9/jenfi_pipeline_data_app/app_funcs/_query.py
@@ -0,0 +1,40 @@
+from ..db_cache import DbCache
+from pandas import DataFrame
+
+# Primary use point for Credit
+# Should be able to help take snapshot of data and return the cache as necessary.
+def df_query(self, query_str: str, rebuild_cache: bool=False) -> DataFrame:
+    """
+    Provide a valid psql query_str and returns a Pandas DataFrame. Has caching.
+    """
+    db_cache = self._db_cache()
+
+    return db_cache.df_query(query_str, rebuild_cache)
+
+
+def query_one(self, query_str: str, rebuild_cache: bool=False):
+    """
+    Direct sqlalchmey fetchone(). Returns None or a dict. Has caching.
+    """
+    db_cache = self._db_cache()
+
+    return db_cache.query_one(query_str, rebuild_cache)
+
+
+def query_all(self, query_str: str, rebuild_cache: bool=False):
+    """
+    Direct sqlalchmey fetchall(). Returns an Array. Has caching.
+    """
+
+    db_cache = self._db_cache()
+
+    return db_cache.query_all(query_str, rebuild_cache)
+
+
+def _db_cache(self) -> None:
+    (step_name, run_id) = self._run_data()
+    bucket_name = self.s3_config.S3_DB_QUERY_CACHE_BUCKET
+
+    return DbCache(
+        self.db, self.db_engine, step_name, run_id, self.tmp_dir(), bucket_name
+    )
