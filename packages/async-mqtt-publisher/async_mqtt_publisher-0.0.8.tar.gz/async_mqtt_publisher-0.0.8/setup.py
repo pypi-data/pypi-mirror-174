5f99dcf20216079436c5cf133f656ba609d96a6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:11 +0000

    Add package async-mqtt-publisher/0.0.8/async_mqtt_publisher-0.0.8.tar.gz

---
+++ packages/async-mqtt-publisher/async_mqtt_publisher-0.0.8.tar.gz/async_mqtt_publisher-0.0.8/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='async_mqtt_publisher',
+    description='MQTT publisher based on aiohttp',
+    version='0.0.8',
+    license='MIT',
+    author="Alexey Dergunov",
+    author_email='dergunovalexey2000@gmail.com',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    url='https://github.com/thealexeydergunov/async_mqtt_publisher.git',
+    keywords='MQTT publisher based on aiohttp',
+    install_requires=[
+        'aiohttp',
+    ],
+)
