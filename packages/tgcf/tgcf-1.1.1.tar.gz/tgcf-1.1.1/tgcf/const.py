be56c7668a7d12b01f21fa8e8ed34859344fbc99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:20:42 +0000

    Add package tgcf/1.1.1/tgcf-1.1.1.tar.gz

---
+++ packages/tgcf/tgcf-1.1.1.tar.gz/tgcf-1.1.1/tgcf/const.py
@@ -0,0 +1,15 @@
+"""Declare all global constants."""
+
+COMMANDS = {
+    "start": "Check whether I am alive",
+    "forward": "Set a new forward",
+    "remove": "Remove an existing forward",
+    "help": "Learn usage",
+}
+
+REGISTER_COMMANDS = True
+
+KEEP_LAST_MANY = 10000
+
+CONFIG_FILE_NAME = "tgcf.config.json"
+CONFIG_ENV_VAR_NAME = "TGCF_CONFIG"
