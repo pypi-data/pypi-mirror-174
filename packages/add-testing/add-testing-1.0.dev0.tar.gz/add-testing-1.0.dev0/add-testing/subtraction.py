07038d3a3ba89dc28e1a51c4aaaf62040a23a8fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 09:54:14 +0000

    Add package add-testing/1.0.dev0/add-testing-1.0.dev0.tar.gz

---
+++ packages/add-testing/add-testing-1.0.dev0.tar.gz/add-testing-1.0.dev0/add-testing/subtraction.py
@@ -0,0 +1,12 @@
+def subtraction(a, b):
+    """
+    This function subtracts the two values.
+
+    Args:
+        a: an integer no
+        b: an integer no
+    
+    Returns:
+        subtracted value
+    """
+    return a - b
